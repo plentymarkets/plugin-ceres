fd31243772b9df5b42528895a58742a674e432b6
Ihab Hussein <ihab.hussein@plentymarkets.com>
2016-09-16 09:06:26 +0200

    - logs deleted

--- resources/js/src/app/main.js
+++ resources/js/src/app/main.js
@@ -4,149 +4,142 @@ new Vue({
 
 // Frontend end scripts
 
-(function($, window, document, undefined){
+(function($, window, document, undefined) {
 
-	function CallistoMain(){
-		console.log('callisto main', $);
+    function CallistoMain() {
 
-
-		// Sticky sidebar single item
-		if(window.matchMedia('(min-width: 768px)').matches) {
+        // Sticky sidebar single item
+        if (window.matchMedia('(min-width: 768px)').matches) {
             $(".single-rightside").stick_in_parent({
 
-			});
-			$('.single-rightside')
-			.on('sticky_kit:bottom', function(e) {
-			    $(this).parent().css('position', 'static');
-			})
-			.on('sticky_kit:unbottom', function(e) {
-			    $(this).parent().css('position', 'relative');
-			});
+            });
+
+            $('.single-rightside')
+                .on('sticky_kit:bottom', function(e) {
+                    $(this).parent().css('position', 'static');
+                })
+                .on('sticky_kit:unbottom', function(e) {
+                    $(this).parent().css('position', 'relative');
+                });
         }
 
+        var sync1 = $("#single-big-image");
+        var sync2 = $("#single-carousel");
 
+        sync1.owlCarousel({
+            singleItem: true,
+            slideSpeed: 1000,
+            navigation: true,
+            navigationText: [
+                "<i class='fa fa-chevron-left' aria-hidden='true'></i>",
+                "<i class='fa fa-chevron-right' aria-hidden='true'></i>"
+            ],
+            pagination: false,
+            afterAction: syncPosition,
+            responsiveRefreshRate: 200,
+        });
 
-		var sync1 = $("#single-big-image");
-		var sync2 = $("#single-carousel");
+        sync2.owlCarousel({
+            items: 8,
+            itemsDesktop: [1199, 8],
+            itemsDesktopSmall: [979, 8],
+            itemsTablet: [768, 6],
+            itemsMobile: [479, 4],
+            navigation: true,
+            navigationText: [
+                "<i class='fa fa-chevron-left' aria-hidden='true'></i>",
+                "<i class='fa fa-chevron-right' aria-hidden='true'></i>"
+            ],
+            pagination: false,
+            responsiveRefreshRate: 100,
+            afterInit: function(el) {
+                el.find(".owl-item").eq(0).addClass("synced");
+            }
+        });
 
-		  sync1.owlCarousel({
-		    singleItem : true,
-		    slideSpeed : 1000,
-		    navigation: true,
-		    navigationText: [
-		      "<i class='fa fa-chevron-left' aria-hidden='true'></i>",
-		      "<i class='fa fa-chevron-right' aria-hidden='true'></i>"
-		    ],
-		    pagination:false,
-		    afterAction : syncPosition,
-		    responsiveRefreshRate : 200,
-		  });
+        function syncPosition(el) {
+            var current = this.currentItem;
+            $("#single-carousel")
+                .find(".owl-item")
+                .removeClass("synced")
+                .eq(current)
+                .addClass("synced")
+            if ($("#single-carousel").data("owlCarousel") !== undefined) {
+                center(current)
+            }
+        }
 
-		  sync2.owlCarousel({
-		    items : 8,
-		    itemsDesktop      : [1199,8],
-		    itemsDesktopSmall     : [979,8],
-		    itemsTablet       : [768,6],
-		    itemsMobile       : [479,4],
-		    navigation: true,
-		    navigationText: [
-		      "<i class='fa fa-chevron-left' aria-hidden='true'></i>",
-		      "<i class='fa fa-chevron-right' aria-hidden='true'></i>"
-		    ],
-		    pagination:false,
-		    responsiveRefreshRate : 100,
-		    afterInit : function(el){
-		      el.find(".owl-item").eq(0).addClass("synced");
-		    }
-		  });
+        $("#single-carousel").on("click", ".owl-item", function(e) {
+            e.preventDefault();
+            var number = $(this).data("owlItem");
+            sync1.trigger("owl.goTo", number);
+        });
 
-		  function syncPosition(el){
-		    var current = this.currentItem;
-		    $("#single-carousel")
-		      .find(".owl-item")
-		      .removeClass("synced")
-		      .eq(current)
-		      .addClass("synced")
-		    if($("#single-carousel").data("owlCarousel") !== undefined){
-		      center(current)
-		    }
-		  }
+        function center(number) {
+            var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+            var num = number;
+            var found = false;
+            for (var i in sync2visible) {
+                if (num === sync2visible[i]) {
+                    var found = true;
+                }
+            }
 
-		  $("#single-carousel").on("click", ".owl-item", function(e){
-		    e.preventDefault();
-		    var number = $(this).data("owlItem");
-		    sync1.trigger("owl.goTo",number);
-		  });
+            if (found === false) {
+                if (num > sync2visible[sync2visible.length - 1]) {
+                    sync2.trigger("owl.goTo", num - sync2visible.length + 2)
+                } else {
+                    if (num - 1 === -1) {
+                        num = 0;
+                    }
+                    sync2.trigger("owl.goTo", num);
+                }
+            } else if (num === sync2visible[sync2visible.length - 1]) {
+                sync2.trigger("owl.goTo", sync2visible[1])
+            } else if (num === sync2visible[0]) {
+                sync2.trigger("owl.goTo", num - 1)
+            }
 
-		  function center(number){
-		    var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
-		    var num = number;
-		    var found = false;
-		    for(var i in sync2visible){
-		      if(num === sync2visible[i]){
-		        var found = true;
-		      }
-		    }
+        }
 
-		    if(found===false){
-		      if(num>sync2visible[sync2visible.length-1]){
-		        sync2.trigger("owl.goTo", num - sync2visible.length+2)
-		      }else{
-		        if(num - 1 === -1){
-		          num = 0;
-		        }
-		        sync2.trigger("owl.goTo", num);
-		      }
-		    } else if(num === sync2visible[sync2visible.length-1]){
-		      sync2.trigger("owl.goTo", sync2visible[1])
-		    } else if(num === sync2visible[0]){
-		      sync2.trigger("owl.goTo", num-1)
-		    }
+        var $toggleBasketPreview = $('#toggleBasketPreview, #closeBasketPreview');
 
-		  }
+        $toggleBasketPreview.on('click', function(evt) {
+            evt.preventDefault();
+            $('body').toggleClass('open-right');
+        });
 
+        var $toggleListView = $('.toggle-list-view');
 
+        $toggleListView.on('click', function(evt) {
+            evt.preventDefault();
 
-		var $toggleBasketPreview = $('#toggleBasketPreview, #closeBasketPreview');
+            //toggle it's own state
+            $toggleListView.toggleClass('grid');
 
-		$toggleBasketPreview.on('click', function( evt ){
-			evt.preventDefault();
-			$('body').toggleClass('open-right');
-		});
+            //toggle internal style of thumbs
+            $('.product-list, .cmp-product-thumb').toggleClass('grid');
+        });
 
+        $('#mainNavbarCollapse').collapse('hide');
 
-		var $toggleListView = $('.toggle-list-view');
+        //Add click listener outside the navigation to close it
+        $('#mainNavbarCollapse').on('show.bs.collapse', function() {
+            $('.main').one('click', closeNav);
+        })
 
-		$toggleListView.on('click', function( evt ){
-			console.log('toggle-list-view click');
-			evt.preventDefault();
+        $('#mainNavbarCollapse').on('hide.bs.collapse', function() {
+            $('.main').off('click', closeNav)
+        })
 
-			//toggle it's own state
-			$toggleListView.toggleClass('grid');
+        function closeNav() {
+            $('#mainNavbarCollapse').collapse('hide');
+        }
 
-			//toggle internal style of thumbs
-			$('.product-list, .cmp-product-thumb').toggleClass('grid');
-		});
+    }
 
-		$('#mainNavbarCollapse').collapse('hide');
+    window.CallistoMain = CallistoMain;
 
-		//Add click listener outside the navigation to close it
-		$('#mainNavbarCollapse').on('show.bs.collapse', function () {
-			$('.main').one('click', closeNav);
-		})
-
-		$('#mainNavbarCollapse').on('hide.bs.collapse', function () {
-			$('.main').off('click', closeNav)
-		})
-
-		function closeNav(){
-			$('#mainNavbarCollapse').collapse('hide');
-		}
-
-	}
-
-	window.CallistoMain = CallistoMain;
-
-	new CallistoMain();
+    new CallistoMain();
 
 })(jQuery, window, document);
