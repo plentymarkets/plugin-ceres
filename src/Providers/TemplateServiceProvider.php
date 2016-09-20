e6e8abde8e95c2472f14a6d03480ef8aabb62c7e
Dominik Meyer <dominik.meyer@plentymarkets.com>
2016-09-20 16:25:08 +0200

    UPDATE php7

--- src/Providers/TemplateServiceProvider.php
+++ src/Providers/TemplateServiceProvider.php
@@ -1,4 +1,4 @@
-<?hh // strict
+<?php // strict
 
 namespace Callisto\Providers;
 
@@ -18,81 +18,81 @@ class TemplateServiceProvider extends ServiceProvider
 {
     public function register():void
     {
-
+        
     }
-
+    
     public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config):void
     {
         // Register Twig String Loader to use function: template_from_string
         $twig->addExtension('Twig_Extension_StringLoader');
-
+        
         // provide template to use for content categories
-        $eventDispatcher->listen('tpl.category.content', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.category.content', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("PluginCallisto::Category.CategoryContent");
-        });
+        }, 0);
 
         // provide template to use for item categories
-        $eventDispatcher->listen('tpl.category.item', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.category.item', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("PluginCallisto::Category.Item.ItemCategoryView");
-        });
+        }, 0);
 
         // provide template to use for blog categories
-        $eventDispatcher->listen('tpl.category.blog', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.category.blog', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("PluginCallisto::PageDesign");
-        });
+        }, 0);
 
         // provide template to use for container categories
-        $eventDispatcher->listen('tpl.category.container', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.category.container', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("PluginCallisto::PageDesign");
-        });
+        }, 0);
 
         // provide template to use for single items
-        $eventDispatcher->listen('tpl.item', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.item', function(TemplateContainer $container,  $templateData) {
             $container->setTemplate("PluginCallisto::Item.SingleItem");
-        });
+        }, 0);
 
         // provide template to use for basket
-        $eventDispatcher->listen('tpl.basket', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.basket', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("PluginCallisto::Basket.Basket");
-        });
+        }, 0);
 
         // provide template to use for checkout
-        $eventDispatcher->listen('tpl.checkout', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.checkout', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("PluginCallisto::Checkout.Checkout");
-         });
+        }, 0);
 
         // provide template to use for my-account
-        $eventDispatcher->listen('tpl.my-account', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.my-account', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("PluginCallisto::MyAccount.MyAccount");
-         });
+        }, 0);
 
         // provide template to use for confirmation
-        $eventDispatcher->listen('tpl.confirmation', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.confirmation', function(TemplateContainer $container,  $templateData) {
             $container->setTemplate("PluginCallisto::Confirmation.Confirmation");
-        });
+        }, 0);
 
         // provide template to use for login
-        $eventDispatcher->listen('tpl.login', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.login', function(TemplateContainer $container,  $templateData) {
             $container->setTemplate("PluginCallisto::LoginPage.LoginPage");
-        });
+        }, 0);
 
         // provide template to use for register
-        $eventDispatcher->listen('tpl.register', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.register', function(TemplateContainer $container, $templateData) {
             $container->setTemplate("PluginCallisto::RegisterPage.RegisterPage");
-        });
+        }, 0);
 
         // provide template to use for guest
-        $eventDispatcher->listen('tpl.guest', (TemplateContainer $container, array<string, mixed> $templateData) ==> {
+        $eventDispatcher->listen('tpl.guest', function(TemplateContainer $container,  $templateData) {
             $container->setTemplate("PluginCallisto::Guest.Guest");
-        });
+        }, 0);
 
         // provide mapped category IDs
-        $eventDispatcher->listen('init.categories', (CategoryMap $categoryMap) ==> {
+        $eventDispatcher->listen('init.categories', function(CategoryMap $categoryMap, $config) {
             $categoryMap->setCategoryMap(array (
                 CategoryKey::HOME           => $config->get("PluginCallisto.global.category.home"),
                 CategoryKey::PAGE_NOT_FOUND => $config->get("PluginCallisto.global.category.page_not_found"),
                 CategoryKey::ITEM_NOT_FOUND => $config->get("PluginCallisto.global.category.item_not_found")
             ));
-        });
+        }, 0);
 	}
 }
