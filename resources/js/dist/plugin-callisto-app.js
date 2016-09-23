87abefc596d456aee3c08a0e61f7455c6a41b87a
Jan Mense <janmense@googlemail.com>
2016-09-23 15:34:09 +0200

    sticky header / topbar icon login, register

--- resources/js/dist/plugin-callisto-app.js
+++ resources/js/dist/plugin-callisto-app.js
@@ -4150,6 +4150,16 @@ new Vue({
 
     function CallistoMain() {
 
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 1){  
+                $('.wrapper-main').addClass("sticky");
+              }
+              else{
+                $('.wrapper-main').removeClass("sticky");
+              }
+        });
+
         // Sticky sidebar single item
         if (window.matchMedia('(min-width: 768px)').matches) {
             $(".single-rightside").stick_in_parent({
