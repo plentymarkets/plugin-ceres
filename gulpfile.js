35e13abf4c68cc16cda987c2a2a7f532c5456696
Lukas Matzen <lukas.matzen@plentymarkets.com>
2016-10-04 14:55:56 +0200

    ESLint:
    	- enable auto. fixing

--- gulpfile.js
+++ gulpfile.js
@@ -126,9 +126,9 @@ gulp.task('build:lint', function() {
     return gulp.src(['resources/js/src/**/*.js', '!node_modules/**'])
         .pipe(eslint({
             "configFile": "./.eslintrc.json",
-            "fix": false
+            "fix": true
         }))
-        //.pipe(gulp.dest("resources/js/src/"))
+        .pipe(gulp.dest("resources/js/src/"))
         // eslint.format() outputs the lint results to the console.
         // Alternatively use eslint.formatEach() (see Docs).
         .pipe(eslint.format("table"))
