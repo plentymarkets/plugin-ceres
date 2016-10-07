82efe47b8d86bfcbdb088a58fdb309dfbcd37c72
Lukas Matzen <lukas.matzen@plentymarkets.com>
2016-10-07 11:42:24 +0200

    ESLint:
    	- temp. commented out code that threw errors

--- resources/js/src/app/directives/checkout/PlaceOrder.js
+++ resources/js/src/app/directives/checkout/PlaceOrder.js
@@ -1,22 +1,22 @@
-var ApiService = require("services/ApiService");
+// var ApiService = require("services/ApiService");
 
 Vue.directive("place-order", function()
 {
-    params: ["trigger"],
-
-    /**
-     * TODO
-     */
-    $elem.click(function(event)
-    {
-        event.preventDefault();
-
-        ApiService.post("/rest/order")
-            .done(function(response)
-            {
-                var target = $elem.attr("href") || $elem.parents("form").attr("action");
-
-                window.location.assign(target);
-            });
-    });
+    // params: ["trigger"],
+    //
+    // /**
+    //  * TODO
+    //  */
+    // $elem.click(function(event)
+    // {
+    //     event.preventDefault();
+    //
+    //     ApiService.post("/rest/order")
+    //         .done(function(response)
+    //         {
+    //             var target = $elem.attr("href") || $elem.parents("form").attr("action");
+    //
+    //             window.location.assign(target);
+    //         });
+    // });
 });
