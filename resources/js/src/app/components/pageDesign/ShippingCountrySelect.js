6435239831735e5e3d1459d2d393c9f5d2bdf3dc
cjohannsen <cjohannsen@ChristonsensMBP.localdomain>
2016-11-14 14:06:01 +0100

    shipping_country_select: - replaced getting data via twig with resources

--- resources/js/src/app/components/pageDesign/ShippingCountrySelect.js
+++ resources/js/src/app/components/pageDesign/ShippingCountrySelect.js
@@ -1,14 +1,22 @@
+var ResourceService = require("services/ResourceService");
+
 Vue.component("shipping-country-select", {
 
     template: "#vue-shipping-country-select",
 
-    props: [
-        "shippingCountryList",
-        "currentShippingCountryId"
-    ],
+    data: function()
+    {
+        return {
+            shippingCountryList: [],
+            currentShippingCountryId: ""
+        };
+    },
 
     created: function()
     {
+        ResourceService.bind("activeShippingCountries", this, "shippingCountryList");
+        ResourceService.bind("currentShippingCountryId", this, "currentShippingCountryId");
+
         for (var i in this.shippingCountryList)
         {
             var country = this.shippingCountryList[i];
