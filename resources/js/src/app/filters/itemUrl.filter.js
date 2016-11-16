149ba842ce09df355b20535c66ae35c94d94448b
Felix Dausch <felix.dausch@plentymarkets.com>
2016-11-16 16:29:50 +0100

    FIX itemURL filter for items not having a URLContent

--- resources/js/src/app/filters/itemUrl.filter.js
+++ resources/js/src/app/filters/itemUrl.filter.js
@@ -4,6 +4,10 @@ Vue.filter("itemURL", function(item)
     var urlContent = item.itemDescription.urlContent.split("/");
     var i          = urlContent.length - 1;
 
-    return "/" + urlContent[i] + "/" + item.itemBase.id + "/" + item.variationBase.id;
+    if (urlContent[i].length > 0)
+    {
+        return "/" + urlContent[i] + "/" + item.itemBase.id + "/" + item.variationBase.id;
+    }
+    return "/" + item.itemBase.id + "/" + item.variationBase.id;
 
 });
