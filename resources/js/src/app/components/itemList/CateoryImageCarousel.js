92bc1741efad4e5737d604e2acb09ad25409aa10
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-02-27 15:43:39 +0100

    image_carousel_with_lazyload: - added owl.carousel 2.2.0 - implemented new owl.carousel for CategoryItem (search)

---
+++ resources/js/src/app/components/itemList/CateoryImageCarousel.js
@@ -0,0 +1,32 @@
+Vue.component("category-image-carousel", {
+
+    props: [
+        "imageUrls",
+        "itemUrl",
+        "template"
+    ],
+
+    created: function()
+    {
+        this.$options.template = this.template;
+    },
+
+    ready: function()
+    {
+        if (this.imageUrls && this.imageUrls.length > 0)
+        {
+            $(".owl-carousel").owlCarousel({
+                dots     : false,
+                items    : 1,
+                loop     : this.imageUrls.length > 1,
+                lazyLoad : true,
+                mouseDrag: false,
+                nav      : true,
+                navText  : [
+                    "<i class='fa fa-chevron-left' aria-hidden='true'></i>",
+                    "<i class='fa fa-chevron-right' aria-hidden='true'></i>"
+                ]
+            });
+        }
+    }
+});
