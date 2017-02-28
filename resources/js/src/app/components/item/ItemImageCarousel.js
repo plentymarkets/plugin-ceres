886ebfdc7f0f3f69e958e7f90c2ac3899296224c
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-02-28 18:04:21 +0100

    image_carousel_with_lazyload: - removed old VariationImageList - ItemImageCarousel will now listen for selected variation and update the images - added variation images to SingleItem.fields.json

--- resources/js/src/app/components/item/ItemImageCarousel.js
+++ resources/js/src/app/components/item/ItemImageCarousel.js
@@ -4,8 +4,16 @@ Vue.component("item-image-carousel", {
 
     props: [
         "currentVariation",
+        "imageUrlAccessor",
         "template"
     ],
+
+    data: function()
+    {
+        return {
+            init: false
+        };
+    },
 
     created: function()
     {
@@ -17,27 +25,70 @@ Vue.component("item-image-carousel", {
 
             var self = this;
 
-            $(window).load(function()
+            if (!this.init)
             {
-                $(self.$els.single).owlCarousel({
-                    autoHeight       : true,
-                    items            : 1,
-                    lazyLoad         : true,
-                    loop             : true,
-                    margin           : 10,
-                    nav              : true,
-                    navClass         : [
-                        "owl-single-item-nav left carousel-control",
-                        "owl-single-item-nav right carousel-control"
-                    ],
-                    navContainerClass: "",
-                    navText          : [
-                        "<i class=\"owl-single-item-control fa fa-chevron-left\" aria-hidden=\"true\"></i>",
-                        "<i class=\"owl-single-item-control fa fa-chevron-right\" aria-hidden=\"true\"></i>"
-                    ],
-                    smartSpeed       : 350
+                $(window).load(function()
+                {
+                    self.initCarousel();
+
+                    self.init = true;
                 });
-            });
+            }
+
+            else
+            {
+                setTimeout(function()
+                {
+                    self.reInitialize();
+                }, 1);
+            }
+
         }.bind(this));
+    },
+
+    methods:
+    {
+        getImageCount: function()
+        {
+            var images = this.currentVariation.documents[0].data.images;
+
+            return images.variation.length || images.all.length;
+        },
+
+        reInitialize: function()
+        {
+            var $owl = $(this.$els.single);
+
+            $owl.trigger("destroy.owl.carousel");
+            $owl.html($owl.find(".owl-stage-outer").html()).removeClass("owl-loaded");
+            $owl.find(".owl-item").remove();
+
+            this.initCarousel();
+        },
+
+        initCarousel: function()
+        {
+            var imageCount = this.getImageCount();
+
+            $(this.$els.single).owlCarousel({
+                autoHeight       : true,
+                items            : 1,
+                lazyLoad         : true,
+                loop             : imageCount > 1,
+                margin           : 10,
+                mouseDrag        : imageCount > 1,
+                nav              : imageCount > 1,
+                navClass         : [
+                    "owl-single-item-nav left carousel-control",
+                    "owl-single-item-nav right carousel-control"
+                ],
+                navContainerClass: "",
+                navText          : [
+                    "<i class=\"owl-single-item-control fa fa-chevron-left\" aria-hidden=\"true\"></i>",
+                    "<i class=\"owl-single-item-control fa fa-chevron-right\" aria-hidden=\"true\"></i>"
+                ],
+                smartSpeed       : 350
+            });
+        }
     }
 });
