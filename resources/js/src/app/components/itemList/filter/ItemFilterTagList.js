3aa8725ed300e2adced8f00314a718d34761c36a
Lukas Matzen <lukas.matzen@plentymarkets.com>
2017-03-03 11:28:58 +0100

    ADD FilterTagList component ADD hide filter if no facets

---
+++ resources/js/src/app/components/itemList/filter/ItemFilterTagList.js
@@ -0,0 +1,55 @@
+var ResourceService = require("services/ResourceService");
+
+Vue.component("item-filter-tag-list", {
+
+    props: [
+        "template",
+        "facets",
+        "categoryId"
+    ],
+
+    data: function()
+    {
+        return {
+            facetParams: []
+        };
+    },
+
+    created: function()
+    {
+        this.$options.template = this.template || "#vue-item-filter-tag-list";
+        ResourceService.bind("facetParams", this);
+    },
+
+    methods:
+    {
+        removeTag: function(tagId)
+        {
+            console.log(tagId);
+        }
+    },
+
+    computed:
+    {
+        tagList: function()
+        {
+            var tagList = [];
+
+            if (this.facetParams.length > 0)
+            {
+                for (var facetKey in this.facets)
+                {
+                    for (var facetItemKey in this.facets[facetKey].values)
+                    {
+                        if (this.facetParams.indexOf(this.facets[facetKey].values[facetItemKey].id.toString()) > -1)
+                        {
+                            tagList.push(this.facets[facetKey].values[facetItemKey]);
+                        }
+                    }
+                }
+            }
+
+            return tagList;
+        }
+    }
+});
