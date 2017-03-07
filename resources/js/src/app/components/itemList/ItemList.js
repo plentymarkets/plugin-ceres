9d46329d461ce46da75b6cc61ee77c8d8d2747b9
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2017-03-07 15:10:36 +0100

    improve the filter list state handling

--- resources/js/src/app/components/itemList/ItemList.js
+++ resources/js/src/app/components/itemList/ItemList.js
@@ -27,47 +27,5 @@ Vue.component("item-list", {
         ResourceService.bind("isLoading", this);
 
         ItemListService.setSearchParams(document.location.search);
-
-        this.watchFacetOpeningState();
-    },
-
-    methods:
-    {
-        watchFacetOpeningState: function()
-        {
-            if (document.getElementById("filterCollapse") !== null)
-            {
-                var observer = new MutationObserver(function(mutation)
-                {
-                    if (!document.getElementById("filterCollapse").classList.contains("collapsing"))
-                    {
-                        this.filterListState = document.getElementById("filterCollapse").classList.contains("in");
-                    }
-                }.bind(this));
-
-                var targetToWatch = document.getElementById("filterCollapse");
-
-                observer.observe(targetToWatch, {attributes: true, subtree: true});
-            }
-        }
-    },
-
-    watch:
-    {
-        isLoading: function()
-        {
-            this.watchFacetOpeningState();
-        },
-
-        itemList: function()
-        {
-            if (!$.isEmptyObject(this.itemList) && document.getElementById("filterCollapse") !== null)
-            {
-                if (this.filterListState)
-                {
-                    document.getElementById("filterCollapse").classList.add("in");
-                }
-            }
-        }
     }
 });
