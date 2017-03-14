02a545fc11db0ffe7ba847c6652f179867ddbbe8
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-03-14 16:19:05 +0100

    filter_and_image_carousel: - added translations for sortData in ItemListSorting

--- resources/js/src/app/components/itemList/ItemListSorting.js
+++ resources/js/src/app/components/itemList/ItemListSorting.js
@@ -11,7 +11,34 @@ Vue.component("item-list-sorting", {
     data: function()
     {
         return {
-            selectedSorting: {}
+            selectedSorting: {},
+            dataTranslationMapping:
+            {
+                "item.id_asc"                : "itemId_asc",
+                "item.id_desc"               : "itemId_desc",
+                "texts.name1_asc"            : "itemName_asc",
+                "texts.name1_desc"           : "itemName_desc",
+                "item.position_asc"          : "itemPosition_asc",
+                "item.position_desc"         : "itemPosition_desc",
+                "item.salesPrice.price_asc"  : "itemPrice_asc",
+                "item.salesPrice.price_desc" : "itemPrice_desc",
+                "variation.createdAt_asc"    : "variationCreateTimestamp_asc",
+                "variation.createdAt_desc"   : "variationCreateTimestamp_desc",
+                "variation.id_asc"           : "variationId_asc",
+                "variation.id_desc"          : "variationId_desc",
+                "variation.number_asc"       : "variationCustomNumber_asc",
+                "variation.number_desc"      : "variationCustomNumber_desc",
+                "variation.updatedAt_asc"    : "variationLastUpdateTimestamp_asc",
+                "variation.updatedAt_desc"   : "variationLastUpdateTimestamp_desc",
+                "variation.position_asc"     : "variationPosition_asc",
+                "variation.position_desc"    : "variationPosition_desc",
+                "variation.isActive_asc"     : "variationActive_asc",
+                "variation.isActive_desc"    : "variationActive_desc",
+                "variation.isMain_asc"       : "variationPrimary_asc",
+                "variation.isMain_desc"      : "variationPrimary_desc",
+                "item.manufacturer.name_asc" : "itemProducerName_asc",
+                "item.manufacturer.name_desc": "itemProducerName_desc"
+            }
         };
     },
 
@@ -35,7 +62,7 @@ Vue.component("item-list-sorting", {
                 var sortItem =
                     {
                         value      : data,
-                        displayName: Translations.Template[data]
+                        displayName: Translations.Template[this.dataTranslationMapping[data]]
                     };
 
                 this.sortData[i] = sortItem;
