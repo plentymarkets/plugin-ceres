f0b43045dab026a79b2aa448ee1c6e25c4f23935
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-03-15 16:42:47 +0100

    filter_and_image_carousel: - pagination will now cast itemsPerPage to integer before calculating pageMax

--- resources/js/src/app/components/itemList/Pagination.js
+++ resources/js/src/app/components/itemList/Pagination.js
@@ -52,9 +52,9 @@ Vue.component("pagination", {
                 return this.lastPageMax;
             }
 
-            var pageMax = this.itemList.total / this.itemSearch.itemsPerPage;
+            var pageMax = this.itemList.total / parseInt(this.itemSearch.itemsPerPage);
 
-            if (this.itemList.total % this.itemSearch.itemsPerPage > 0)
+            if (this.itemList.total % parseInt(this.itemSearch.itemsPerPage) > 0)
             {
                 pageMax += 1;
             }
