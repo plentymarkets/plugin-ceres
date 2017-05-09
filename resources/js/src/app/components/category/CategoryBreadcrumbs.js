13d1be49fee64cbb597e4f07330cb62089eb79b8
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-05-09 09:03:30 +0200

    FIX code style automation

--- resources/js/src/app/components/category/CategoryBreadcrumbs.js
+++ resources/js/src/app/components/category/CategoryBreadcrumbs.js
@@ -2,56 +2,55 @@ var CategoryRendererService = require("services/CategoryRendererService");
 var ResourceService = require("services/ResourceService");
 
 Vue.component("category-breadcrumbs",
-{
-
-    props: [
-        "template",
-        "categories",
-        "currentCategoryTree"
-    ],
-
-    data: function()
     {
-        return {
-            breadcrumbs: {}
-        };
-    },
 
-    created: function()
-    {
-        this.$options.template = this.template;
+        props: [
+            "template",
+            "categories",
+            "currentCategoryTree"
+        ],
 
-        this.init();
-    },
-
-    methods:
-    {
-    /**
-     * initialize values
-     */
-        init: function()
+        data: function()
         {
-            this.categories = JSON.parse(this.categories);
-            this.currentCategoryTree = JSON.parse(this.currentCategoryTree);
-
-            ResourceService.bind("breadcrumbs", this);
-            ResourceService.getResource("breadcrumbs").set(this.currentCategoryTree);
+            return {
+                breadcrumbs: {}
+            };
         },
 
-    /**
-     * render items in relation to location
-     * @param currentCategory
-     */
-        renderItems: function(currentCategory)
+        created: function()
         {
-            CategoryRendererService.renderItems(currentCategory);
+            this.$options.template = this.template;
 
-            return false;
+            this.init();
         },
 
-        getBreadcrumbURL: function(breadcrumb)
-        {
-            return CategoryRendererService.getScopeUrl(breadcrumb);
+        methods: {
+            /**
+             * initialize values
+             */
+            init: function()
+            {
+                this.categories = JSON.parse(this.categories);
+                this.currentCategoryTree = JSON.parse(this.currentCategoryTree);
+
+                ResourceService.bind("breadcrumbs", this);
+                ResourceService.getResource("breadcrumbs").set(this.currentCategoryTree);
+            },
+
+            /**
+             * render items in relation to location
+             * @param currentCategory
+             */
+            renderItems: function(currentCategory)
+            {
+                CategoryRendererService.renderItems(currentCategory);
+
+                return false;
+            },
+
+            getBreadcrumbURL: function(breadcrumb)
+            {
+                return CategoryRendererService.getScopeUrl(breadcrumb);
+            }
         }
-    }
-});
+    });
