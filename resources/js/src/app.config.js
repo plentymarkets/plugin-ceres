2b101d477bd41c126ec6c0fd13b355da44ef9517
Lukas Matzen <lukas.matzen@plentymarkets.com>
2017-05-11 15:36:38 +0200

    REMOVE unused config values

--- resources/js/src/app.config.js
+++ resources/js/src/app.config.js
@@ -1,7 +1,2 @@
 Vue.config.delimiters = ["${", "}"];
 Vue.config.unsafeDelimiters = ["{!!", "!!}"];
-
-// Comment these three for local build. TODO dev/production
-// Vue.config.devtools = false;
-// Vue.config.debug = false;
-// Vue.config.silent = true;
