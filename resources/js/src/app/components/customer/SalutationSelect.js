cd8651b50ee9d099c09318e64f30c4cfc554231c
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-05-16 15:26:23 +0200

    ADD input intent for DE address input

---
+++ resources/js/src/app/components/customer/SalutationSelect.js
@@ -0,0 +1,40 @@
+var ResourceService = require("services/ResourceService");
+
+Vue.component("salutation-select", {
+
+    props: [
+        "template"
+    ],
+
+    data: function()
+    {
+        return {
+            localization: {},
+            salutations: {
+                de: ["Herr", "Frau", "Firma", "Familie"],
+                en: ["Mr.", "Ms.", "Company", "Family"]
+            },
+            currentSalutation: {}
+        };
+    },
+
+    /**
+     * Get the shipping countries
+     */
+    created: function()
+    {
+        this.$options.template = this.template;
+
+        ResourceService.bind("localization", this);
+        this.shopLanguage = this.localization.shopLanguage;
+
+        if (this.shopLanguage === "de")
+        {
+            this.currentSalutation = this.salutations.de;
+        }
+        else
+        {
+            this.currentSalutation = this.salutations.en;
+        }
+    }
+});
