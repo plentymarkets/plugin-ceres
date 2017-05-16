45cdc7fd4c2d3f8b5cbf0a8db17a4c0771c708ed
Lukas Matzen <lukas.matzen@plentymarkets.com>
2017-05-16 10:37:19 +0200

    UPDATE build files

--- resources/js/dist/ceres-app.js
+++ resources/js/dist/ceres-app.js
@@ -3590,6 +3590,23 @@ Vue.filter("itemImage", function (item, baseUrl) {
 },{}],63:[function(require,module,exports){
 "use strict";
 
+Vue.filter("itemImages", function (images, accessor) {
+    var imageUrls = [];
+    var imagesAccessor = "all";
+
+    if (images.variation.length) {
+        imagesAccessor = "variation";
+    }
+
+    for (var i in images[imagesAccessor]) {
+        var imageUrl = images[imagesAccessor][i][accessor];
+
+        imageUrls.push(imageUrl);
+    }
+
+    return imageUrls;
+});
+
 },{}],64:[function(require,module,exports){
 "use strict";
 
