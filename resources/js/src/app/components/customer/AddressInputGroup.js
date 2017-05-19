d102d94334cf605884420cf5c441a93807db4124
Manuel Dierkes <manuel.dierkes@plentymarkets.com>
2017-05-19 11:39:31 +0200

    Added option type filter to read option type data

--- resources/js/src/app/components/customer/AddressInputGroup.js
+++ resources/js/src/app/components/customer/AddressInputGroup.js
@@ -4,6 +4,7 @@ Vue.component("address-input-group", {
         "addressData",
         "defaultCountry",
         "addressType",
+        "modalType",
         "template"
     ],
 
@@ -14,6 +15,35 @@ Vue.component("address-input-group", {
             countryLocaleList: ["DE", "GB"],
             localeToShow: ""
         };
+    },
+
+    filters:{
+        optionType:{
+
+            read: function(value, optionType)
+            {
+
+                var data = this.addressData.options;
+
+                if (typeof data === "undefined")
+                {
+                    return value;
+                }
+                else if (this.modalType === "update" && !this.equalOptionValues(value, data, optionType))
+                {
+                    return value;
+                }
+
+                return this.getOptionType(data, optionType);
+
+            },
+
+            write: function(value)
+            {
+                return value;
+            }
+
+        }
     },
 
     /**
@@ -47,6 +77,30 @@ Vue.component("address-input-group", {
             {
                 this.localeToShow = this.defaultCountry;
             }
+        },
+
+        getOptionType: function(data, optionType)
+    {
+            for (var i = 0; i < data.length; i++)
+        {
+                if (optionType === data[i].typeId)
+            {
+                    return data[i].value;
+                }
+            }
+            return "";
+        },
+
+        equalOptionValues: function(newValue, data, optionType)
+    {
+            var oldValue = this.getOptionType(data, optionType);
+
+            if (typeof newValue === "undefined")
+{
+                return oldValue;
+            }
+
+            return oldValue === newValue;
         }
     }
 });
