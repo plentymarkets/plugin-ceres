35e94a46b2316a5626531db3253c4c92ec57411a
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-05-23 15:55:05 +0200

    category_carousel_disable_lazyloading: - CategoryImageCarousel has now default value for disableLazyLoad

--- resources/js/src/app/components/itemList/CategoryImageCarousel.js
+++ resources/js/src/app/components/itemList/CategoryImageCarousel.js
@@ -1,20 +1,21 @@
 Vue.component("category-image-carousel", {
 
-    props: [
-        "imageUrls",
-        "itemUrl",
-        "altText",
-        "showDots",
-        "showNav",
-        "disableLazyload",
-        "template"
-    ],
+    props: {
+        imageUrls      : {type: Array},
+        itemUrl        : {type: String},
+        altText        : {type: String},
+        showDots       : {type: String},
+        showNav        : {type: String},
+        disableLazyLoad: {
+            type   : Boolean,
+            default: false
+        },
+        template       : {type: String}
+    },
 
     created: function()
     {
         this.$options.template = this.template;
-
-        this.disableLazyload = this.disableLazyload || false;
     },
 
     ready: function()
@@ -22,13 +23,13 @@ Vue.component("category-image-carousel", {
         if (this.imageUrls && this.imageUrls.length > 0)
         {
             $(".owl-carousel").owlCarousel({
-                dots     : (this.showDots === "true"),
-                items    : 1,
-                loop     : this.imageUrls.length > 1,
-                lazyLoad : this.disableLazyload,
-                margin   : 10,
-                nav      : (this.showNav === "true"),
-                navText  : [
+                dots    : (this.showDots === "true"),
+                items   : 1,
+                loop    : this.imageUrls.length > 1,
+                lazyLoad: this.disableLazyLoad,
+                margin  : 10,
+                nav     : (this.showNav === "true"),
+                navText : [
                     "<i class='fa fa-chevron-left' aria-hidden='true'></i>",
                     "<i class='fa fa-chevron-right' aria-hidden='true'></i>"
                 ]
