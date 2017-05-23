d4e6254086c02b82e8531a37d9a475122ec856e8
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-05-23 16:03:52 +0200

    UPDATE build files

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -2394,7 +2394,18 @@ Vue.component("variation-select", {
 
 Vue.component("category-image-carousel", {
 
-    props: ["imageUrls", "itemUrl", "altText", "showDots", "showNav", "template"],
+    props: {
+        imageUrls: { type: Array },
+        itemUrl: { type: String },
+        altText: { type: String },
+        showDots: { type: String },
+        showNav: { type: String },
+        disableLazyLoad: {
+            type: Boolean,
+            default: false
+        },
+        template: { type: String }
+    },
 
     created: function created() {
         this.$options.template = this.template;
@@ -2406,7 +2417,7 @@ Vue.component("category-image-carousel", {
                 dots: this.showDots === "true",
                 items: 1,
                 loop: this.imageUrls.length > 1,
-                lazyLoad: true,
+                lazyLoad: this.disableLazyLoad,
                 margin: 10,
                 nav: this.showNav === "true",
                 navText: ["<i class='fa fa-chevron-left' aria-hidden='true'></i>", "<i class='fa fa-chevron-right' aria-hidden='true'></i>"]
