e00590fae312e742a8731b2402923e757b54966a
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-06-06 13:13:17 +0200

    - AddressSelect will now reset error classes on all fields, when reopening a popup

--- resources/js/src/app/components/customer/AddressSelect/AddressSelect.js
+++ resources/js/src/app/components/customer/AddressSelect/AddressSelect.js
@@ -1,6 +1,7 @@
 var ApiService = require("services/ApiService");
-var ModalService = require("services/ModalService");
 var AddressService = require("services/AddressService");
+var ModalService = require("services/ModalService");
+var ValidationService = require("services/ValidationService");
 
 Vue.component("address-select", {
 
@@ -156,6 +157,7 @@ Vue.component("address-select", {
             this.modalType = "create";
             this.addressToEdit = {};
             this.updateHeadline();
+            ValidationService.unmarkAllFields($(this.$els.addressModal));
             this.addressModal.show();
         },
 
@@ -169,6 +171,7 @@ Vue.component("address-select", {
             // Creates a tmp address to prevent unwanted two-way binding
             this.addressToEdit = JSON.parse(JSON.stringify(address));
             this.updateHeadline();
+            ValidationService.unmarkAllFields($(this.$els.addressModal));
             this.addressModal.show();
         },
 
