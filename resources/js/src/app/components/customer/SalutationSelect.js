2834ae64019d8e6da54f1f0519b5163ec7bd5d6e
Manuel Dierkes <manuel.dierkes@plentymarkets.com>
2017-06-08 15:57:49 +0200

    - filtering values by enabled field company

--- resources/js/src/app/components/customer/SalutationSelect.js
+++ resources/js/src/app/components/customer/SalutationSelect.js
@@ -4,16 +4,40 @@ Vue.component("salutation-select", {
 
     props: [
         "template",
-        "addressData"
+        "addressData",
+        "enabledFields"
     ],
 
-    data: function()
-    {
+    data: function () {
         return {
             localization: {},
             salutations: {
-                de: ["Herr", "Frau", "Firma", "Familie"],
-                en: ["Mr.", "Ms.", "Company", "Family"]
+                complete: {
+                    de: [
+                        {value: "Herr", id: 0},
+                        {value: "Frau", id: 1},
+                        {value: "Firma", id: 2},
+                        {value: "Familie", id: 3}
+                    ],
+                    en: [
+                        {value: "Mr.", id: 0},
+                        {value: "Ms.", id: 1},
+                        {value: "Company", id: 2},
+                        {value: "Family", id: 3}
+                    ]
+                },
+                withoutCompany: {
+                    de: [
+                        {value: "Herr", id: 0},
+                        {value: "Frau", id: 1},
+                        {value: "Familie", id: 3}
+                    ],
+                    en: [
+                        {value: "Mr.", id: 0},
+                        {value: "Ms.", id: 1},
+                        {value: "Family", id: 3}
+                    ]
+                }
             },
             currentSalutation: {}
         };
@@ -22,25 +46,43 @@ Vue.component("salutation-select", {
     /**
      * Get the shipping countries
      */
-    created: function()
-    {
+    created: function () {
+
         this.$options.template = this.template;
 
         ResourceService.bind("localization", this);
         this.shopLanguage = this.localization.shopLanguage;
 
-        if (this.shopLanguage === "de")
-        {
-            this.currentSalutation = this.salutations.de;
+        if (this.shopLanguage === "de") {
+            if (this.isCompanyEnabled(this.enabledFields)) {
+                this.currentSalutation = this.salutations.complete.de;
+            }
+            else {
+                this.currentSalutation = this.salutations.withoutCompany.de;
+            }
         }
-        else
-        {
-            this.currentSalutation = this.salutations.en;
+        else if (this.isCompanyEnabled(this.enabledFields)) {
+            this.currentSalutation = this.salutations.complete.en;
+        }
+        else {
+            this.currentSalutation = this.salutations.withoutCompany.en;
         }
     },
 
-    ready: function()
-{
+    ready: function () {
         this.addressData.addressSalutation = 0;
+    },
+
+    methods: {
+        isCompanyEnabled: function (enabledFields) {
+
+            for (var i = 0; i < enabledFields.length; i++) {
+                if (enabledFields[i] === "billing_address.name1" || enabledFields[i] === "delivery_address.name1") {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 });
