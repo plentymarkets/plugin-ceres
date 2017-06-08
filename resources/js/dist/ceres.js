1721f3f4b6e8fb11c82015222ac8befbf956717b
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-06-08 16:13:51 +0200

    UPDATE build files

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -1237,6 +1237,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var ApiService = require("services/ApiService");
 var AddressService = require("services/AddressService");
 var ModalService = require("services/ModalService");
+var ResourceService = require("services/ResourceService");
 
 Vue.component("address-select", {
 
@@ -1250,7 +1251,8 @@ Vue.component("address-select", {
             headline: "",
             addressToEdit: {},
             addressToDelete: {},
-            deleteModal: ""
+            deleteModal: "",
+            localization: {}
         };
     },
 
@@ -1260,6 +1262,7 @@ Vue.component("address-select", {
      */
     created: function created() {
         this.$options.template = this.template;
+        ResourceService.bind("localization", this);
 
         this.addEventListener();
     },
@@ -1362,7 +1365,7 @@ Vue.component("address-select", {
          */
         showInitialAddModal: function showInitialAddModal() {
             this.modalType = "initial";
-            this.addressToEdit = {};
+            this.addressToEdit = { countryId: this.localization.currentShippingCountryId };
             this.updateHeadline();
             this.addressModal.show();
         },
@@ -1373,7 +1376,7 @@ Vue.component("address-select", {
          */
         showAddModal: function showAddModal() {
             this.modalType = "create";
-            this.addressToEdit = {};
+            this.addressToEdit = { countryId: this.localization.currentShippingCountryId };
             this.updateHeadline();
             _ValidationService2.default.unmarkAllFields($(this.$els.addressModal));
             this.addressModal.show();
@@ -1497,7 +1500,7 @@ Vue.component("address-select", {
     }
 });
 
-},{"services/AddressService":68,"services/ApiService":69,"services/ModalService":74,"services/ValidationService":78}],15:[function(require,module,exports){
+},{"services/AddressService":68,"services/ApiService":69,"services/ModalService":74,"services/ResourceService":76,"services/ValidationService":78}],15:[function(require,module,exports){
 "use strict";
 
 var _ValidationService = require("services/ValidationService");
