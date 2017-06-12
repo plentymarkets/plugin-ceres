953430460f2cd9e92f630ba3abe8535151906919
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-06-12 16:33:46 +0200

    - mouseDrag is now disabled for CategoryImageCarousel, when just one image is existing for the item - remove unused css code - CategoryItem’s hover effect will now be correctly now

--- resources/js/src/app/components/itemList/CategoryImageCarousel.js
+++ resources/js/src/app/components/itemList/CategoryImageCarousel.js
@@ -24,13 +24,14 @@ Vue.component("category-image-carousel", {
         if (this.enableCarousel === "true" && this.imageUrls && this.imageUrls.length > 0)
         {
             $(".owl-carousel").owlCarousel({
-                dots    : (this.showDots === "true"),
-                items   : 1,
-                loop    : this.imageUrls.length > 1,
-                lazyLoad: !this.disableLazyLoad,
-                margin  : 10,
-                nav     : (this.showNav === "true"),
-                navText : [
+                dots     : (this.showDots === "true"),
+                items    : 1,
+                mouseDrag: this.imageUrls.length > 1,
+                loop     : this.imageUrls.length > 1,
+                lazyLoad : !this.disableLazyLoad,
+                margin   : 10,
+                nav      : (this.showNav === "true"),
+                navText  : [
                     "<i class='fa fa-chevron-left' aria-hidden='true'></i>",
                     "<i class='fa fa-chevron-right' aria-hidden='true'></i>"
                 ]
