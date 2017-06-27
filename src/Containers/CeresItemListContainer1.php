45a8cf32bf110e3e4a848a5b6fd3c068f5ea9be2
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2017-06-27 18:17:21 +0200

    added first step for config and changed last seen container to generic

--- src/Containers/CeresItemListContainer1.php
+++ src/Containers/CeresItemListContainer1.php
@@ -4,10 +4,10 @@ namespace Ceres\Containers;
 
 use Plenty\Plugin\Templates\Twig;
 
-class CeresLastSeenContainer
+class CeresItemListContainer1
 {
     public function call(Twig $twig):string
     {
-        return $twig->render('Ceres::Containers.ItemLists.LastSeenItemList');
+        return $twig->render('Ceres::Containers.ItemLists.ItemList1');
     }
 }
