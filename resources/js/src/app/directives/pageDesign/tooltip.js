b8524de3e94d034195ac2e29d5c6d8f4a437e57f
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2017-07-20 13:54:06 +0200

    fix tooltip z-index

--- resources/js/src/app/directives/pageDesign/tooltip.js
+++ resources/js/src/app/directives/pageDesign/tooltip.js
@@ -5,7 +5,9 @@ Vue.directive("tooltip", {
         setTimeout(() =>
         {
             $(this.el).tooltip({
-                trigger: "hover"
+                trigger: "hover",
+                // eslint-disable-next-line
+                template: '<div class="tooltip" style="z-index:9999" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
             });
         }, 1);
     }
