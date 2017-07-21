ddb21f52c4e19c3d8b0be8f481fa7e0847ea74bf
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2017-07-21 15:19:35 +0200

    rework tooltip js logic to accept expressions and correct handle unbinding

--- resources/js/src/app/directives/pageDesign/tooltip.js
+++ resources/js/src/app/directives/pageDesign/tooltip.js
@@ -1,14 +1,29 @@
 Vue.directive("tooltip", {
 
-    bind()
+    unbind()
     {
-        setTimeout(() =>
+        $(this.el).tooltip("dispose");
+    },
+
+    update(value)
+    {
+        if (typeof value === "undefined" || value)
         {
-            $(this.el).tooltip({
-                trigger: "hover",
-                // eslint-disable-next-line
-                template: '<div class="tooltip" style="z-index:9999" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
-            });
-        }, 1);
+            setTimeout(() =>
+            {
+                $(this.el).tooltip({
+                    trigger: "hover",
+                    // eslint-disable-next-line
+                    template: '<div class="tooltip" style="z-index:9999" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
+                });
+            }, 1);
+        }
+        else
+        {
+            setTimeout(() =>
+            {
+                $(this.el).tooltip("dispose");
+            }, 1);
+        }
     }
 });
