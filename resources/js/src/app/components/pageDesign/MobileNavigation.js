774c0becb28593a532e4ed8f074efe77e2dda3f8
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-07-26 12:17:36 +0200

    - cleaned up _navigation.scss - cleaned up MobileNavigation.js/.twig - main.js will now open new mobile navigation

--- resources/js/src/app/components/pageDesign/MobileNavigation.js
+++ resources/js/src/app/components/pageDesign/MobileNavigation.js
@@ -8,8 +8,6 @@ Vue.component("mobile-navigation", {
     data()
     {
         return {
-            items: [],
-            activeItem: null,
             dataContainer1: [],
             dataContainer2: [],
             useFirstContainer: false,
@@ -29,78 +27,15 @@ Vue.component("mobile-navigation", {
 
     ready()
     {
-        const el = this.$el;
-
-        $(el).on("open", () =>
-        {
-            $(el).fadeIn();
-        });
-
-        $(el).on("close", () =>
-        {
-            $(el).fadeOut();
-        });
-
-        // $(document).keyup(event =>
-        // {
-        //     if (event.keyCode == 27)
-        //     {
-        //         // escape key
-        //         $(el).trigger("close");
-        //     }
-        // });
-
-        this.addItem("home", "HOME");
-
-        this.addItem("sub-and-link", "sub-and-link", "home", "/foobar");
-        this.addItem("item1", "item1", "sub-and-link", "/item1");
-        this.addItem("item2", "item2", "sub-and-link", "/item2");
-
-        this.addItem("sub-only", "sub-only", "home");
-        this.addItem("subitem1", "subitem1", "sub-only", "/item1");
-        this.addItem("subitem2", "subitem2", "sub-only", "/item2");
-
-        this.addItem("subsub1", "subsub1", "subitem1", "/subsub1");
-        this.addItem("subsub2", "subsub2", "subitem1", "/subsub2");
-
-        this.addItem("link-only", "link-only", "home", "/link");
-
         // REFACTOR!!!
         /*
             - setze das aktuelle auf aktiv
             - trigger menu-activated auf das aktuelle
         */
-        setTimeout(() =>
-        {
-            $(".menu-home").trigger("menu-activated", {back: false});
-            this.activeItem = "home";
-        }, 1000);
         // ./REFACTOR
     },
 
     methods: {
-        // NEW
-        buildBreads()
-        {
-            this.breads = [];
-
-            const breads = [];
-            let root = this.useFirstContainer ? this.dataContainer2[0] : this.dataContainer1[0];
-
-            while (root.parent)
-            {
-                breads.unshift(
-                    {
-                        name: root.parent.name,
-                        layer: root.parent ? root.parent.children : this.categoryTree
-                    });
-
-                root = root.parent;
-            }
-
-            this.breads = breads;
-        },
-
         buildTree(currentArray, parent)
         {
             for (const category of currentArray)
@@ -131,7 +66,7 @@ Vue.component("mobile-navigation", {
             }
         },
 
-        navigateToNew(children, back)
+        navigateTo(children, back)
         {
             back = !!back;
 
@@ -153,96 +88,32 @@ Vue.component("mobile-navigation", {
             this.useFirstContainer = !this.useFirstContainer;
             this.buildBreads();
         },
-        // ./NEW
-        getItemById(id)
+
+        buildBreads()
         {
-            for (const key in this.items)
+            this.breads = [];
+
+            const breads = [];
+            let root = this.useFirstContainer ? this.dataContainer2[0] : this.dataContainer1[0];
+
+            while (root.parent)
             {
-                if (this.items[key].id === id)
-                {
-                    return this.items[key];
-                }
-            }
-            return null;
-        },
+                breads.unshift(
+                    {
+                        name: root.parent.name,
+                        layer: root.parent ? root.parent.children : this.categoryTree
+                    });
 
-        itemHasChildren({id: id})
-        {
-            const items = this.items.filter(item =>
-            {
-                return item.parent === id;
-            });
-
-            return items.length > 0;
-        },
-
-        getBreadcrumbItems(itemId)
-        {
-            const items = [];
-            let tmp = this.getItemById(itemId);
-
-            while (tmp)
-            {
-                items.unshift(tmp);
-                tmp = this.getItemById(tmp.parent);
+                root = root.parent;
             }
 
-            return items;
+            this.breads = breads;
         },
 
-        getGroupedMenus()
+        closeNavigation()
         {
-            const groupedMenus = {};
-
-            for (const key in this.items)
-            {
-				// skip root item
-                if (this.items[key].parent === "-root")
-                {
-                    continue;
-                }
-                if (!groupedMenus[this.items[key].parent])
-                {
-                    groupedMenus[this.items[key].parent] = [];
-                }
-
-                groupedMenus[this.items[key].parent].push(this.items[key]);
-            }
-
-            return groupedMenus;
-        },
-        addItem(id, title, parent, url)
-        {
-            this.items.push({
-                id: id,
-                title: title,
-                parent: parent ? parent : "-root",
-                url: url
-            });
-        },
-
-        directTo(item)
-        {
-            // REFACTOR!!!
-            window.location.href = item.url;
-        },
-
-        navigateTo(item, goBack)
-        {
-            // REFACTOR!!!
-            // if (itemHasNoChildren?)
-            // {
-            //     return;
-            // }
-
-            if (this.activeItem)
-            {
-                $(".menu-" + this.activeItem).trigger("menu-deactivated", {back: !!goBack});
-            }
-
-            $(".menu-" + item.id).trigger("menu-activated", {back: !!goBack});
-
-            this.activeItem = item.id;
+            $(".mobile-navigation").removeClass("open");
+            $("body").removeClass("menu-is-visible");
         }
     },
 
