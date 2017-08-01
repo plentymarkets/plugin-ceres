b97ed47ded3642cb5c4615c06bada8efa3ef4b94
Ihab Hussein <ihab.hussein@plentymarkets.com>
2017-08-01 18:32:33 +0200

    - added wishlist-component

---
+++ resources/js/src/app/components/item/WishList.js
@@ -0,0 +1,48 @@
+const ApiService = require("services/ApiService");
+
+Vue.component("wish-list", {
+
+    props: [
+        "isActive",
+        "variationId",
+        "template"
+    ],
+
+    created()
+    {
+        this.$options.template = this.template;
+    },
+
+    methods:
+    {
+        switchState()
+        {
+            if (this.isActive)
+            {
+                this.removeFromWishList();
+            }
+            else
+            {
+                this.addToWishList();
+            }
+        },
+
+        addToWishList()
+        {
+            ApiService.post("/rest/io/itemWishList", {variationId: this.variationId})
+                .done(function()
+                {
+                    this.isActive = true;
+                }.bind(this));
+        },
+
+        removeFromWishList()
+        {
+            ApiService.delete("/rest/io/itemWishList/" + this.variationId)
+                .done(function()
+                {
+                    this.isActive = false;
+                }.bind(this));
+        }
+    }
+});
