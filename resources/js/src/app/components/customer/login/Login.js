27a20196b76aba3e296dbd4f5d35436fe3d62a1b
Ihab Hussein <ihab.hussein@plentymarkets.com>
2017-08-18 13:30:16 +0200

    - added password reset fields

--- resources/js/src/app/components/customer/login/Login.js
+++ resources/js/src/app/components/customer/login/Login.js
@@ -1,6 +1,6 @@
-var ApiService          = require("services/ApiService");
-var NotificationService = require("services/NotificationService");
-var ModalService        = require("services/ModalService");
+const ApiService          = require("services/ApiService");
+const NotificationService = require("services/NotificationService");
+const ModalService        = require("services/ModalService");
 
 import ValidationService from "services/ValidationService";
 
@@ -13,16 +13,17 @@ Vue.component("login", {
         "template"
     ],
 
-    data: function()
+    data()
     {
         return {
             password: "",
             username: "",
-            isDisabled: false
+            isDisabled: false,
+            isPwdReset: false
         };
     },
 
-    created: function()
+    created()
     {
         this.$options.template = this.template;
     },
@@ -31,52 +32,67 @@ Vue.component("login", {
         /**
          * Open the login modal
          */
-        showLogin: function()
+        showLogin()
         {
             ModalService.findModal(document.getElementById(this.modalElement)).show();
         },
 
-        validateLogin: function()
+        validateLogin()
         {
-            var self = this;
+            if (!this.isPwdReset)
+            {
+                ValidationService.validate($("#login-form-" + this._uid))
+                    .done(() =>
+{
+                        this.sendLogin();
+                    })
+                    .fail(invalidFields =>
+{
+                        ValidationService.markInvalidFields(invalidFields, "error");
+                    });
+            }
+        },
 
-            ValidationService.validate($("#login-form-" + this._uid))
-                .done(function()
-                {
-                    self.sendLogin();
-                })
-                .fail(function(invalidFields)
-                {
-                    ValidationService.markInvalidFields(invalidFields, "error");
-                });
+        validateResetPwd()
+        {
+            if (this.isPwdReset)
+            {
+                ValidationService.validate($("#reset-pwd-form-" + this._uid))
+                    .done(data =>
+{
+                        this.sendResetPwd();
+                    })
+                    .fail(invalidFields =>
+{
+                        ValidationService.markInvalidFields(invalidFields, "error");
+                    });
+            }
         },
 
         /**
          * Send the login data
          */
-        sendLogin: function()
+        sendLogin()
         {
-            var self = this;
-
             this.isDisabled = true;
 
             ApiService.post("/rest/io/customer/login", {email: this.username, password: this.password}, {supressNotifications: true})
-                .done(function(response)
+                .done(response =>
                 {
                     ApiService.setToken(response);
 
-                    if (document.getElementById(self.modalElement) !== null)
+                    if (document.getElementById(this.modalElement) !== null)
                     {
-                        ModalService.findModal(document.getElementById(self.modalElement)).hide();
+                        ModalService.findModal(document.getElementById(this.modalElement)).hide();
                     }
 
                     NotificationService.success(Translations.Template.accLoginSuccessful).closeAfter(10000);
 
-                    if (self.backlink !== null && self.backlink)
+                    if (this.backlink !== null && this.backlink)
                     {
-                        location.assign(self.backlink);
+                        location.assign(this.backlink);
                     }
-                    else if (self.hasToForward)
+                    else if (this.hasToForward)
                     {
                         location.assign(location.origin);
                     }
@@ -85,11 +101,11 @@ Vue.component("login", {
                         location.reload();
                     }
 
-                    self.isDisabled = false;
+                    this.isDisabled = false;
                 })
-                .fail(function(response)
+                .fail(response =>
                 {
-                    self.isDisabled = false;
+                    this.isDisabled = false;
 
                     switch (response.code)
                     {
@@ -100,6 +116,74 @@ Vue.component("login", {
                         return;
                     }
                 });
+        },
+
+        /**
+         *  Reset password
+         */
+        sendResetPwd()
+        {
+            this.isDisabled = true;
+
+            ApiService.post("/rest/io/customer/password_reset", {email: this.username})
+                .done( () =>
+                {
+                    if (document.getElementById(this.modalElement) !== null)
+                    {
+                        ModalService.findModal(document.getElementById(this.modalElement)).hide();
+                    }
+
+                    NotificationService.success("PwdReset").closeAfter(10000);
+
+                    this.isDisabled = false;
+
+                    this.cancelResetPwd();
+
+                })
+                .fail( () =>
+                {
+                    this.isDisabled = false;
+                });
+        },
+
+        showResetPwdView()
+        {
+            const tooltip = "data-toggle=\"tooltip\" data-placement=\"right\" title=\"jknk\"";
+
+            this.isPwdReset = true;
+
+            if (document.getElementById(this.modalElement) !== null)
+            {
+                $(".modal-title").html(Translations.Template.accForgotPassword + " <i class=\"fa fa-question-circle-o\" aria-hidden=\"true\" " + tooltip + "></i>");
+            }
+            else
+            {
+                $(".login-view-title").html(Translations.Template.accForgotPassword + " <i class=\"fa fa-question-circle-o\" aria-hidden=\"true\" " + tooltip + "></i>");
+            }
+
+            $(".login-container").slideUp("fast", function()
+            {
+                $(".reset-pwd-container").slideDown("fast");
+            });
+        },
+
+        cancelResetPwd()
+        {
+            this.isPwdReset = false;
+
+            if (document.getElementById(this.modalElement) !== null)
+            {
+                $(".modal-title").text(Translations.Template.accLogin);
+            }
+            else
+            {
+                $(".login-view-title").text(Translations.Template.accLogin);
+            }
+
+            $(".reset-pwd-container").slideUp("fast", function()
+            {
+                $(".login-container").slideDown("fast");
+            });
         }
     }
 });
