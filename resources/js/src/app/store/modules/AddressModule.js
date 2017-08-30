a34cefdcf2531d68d6e92d1998174dbbf4f5a8d5
Lukas Matzen <lukas.matzen@plentymarkets.com>
2017-08-30 13:23:26 +0200

    ADD actions und mutations to address module

--- resources/js/src/app/store/modules/AddressModule.js
+++ resources/js/src/app/store/modules/AddressModule.js
@@ -10,12 +10,52 @@ const state =
 
 const mutations =
     {
+        setBillingAddressList(state, billingAddressList)
+        {
+            if (Array.isArray(billingAddressList))
+            {
+                state.billingAddressList = billingAddressList;
+            }
+        },
 
+        setBillingAddressId(state, billingAddressId)
+        {
+            if (billingAddressId)
+            {
+                state.billingAddressId = billingAddressId;
+            }
+        },
+
+        setDeliveryAddressList(state, deliveryAddressList)
+        {
+            if (Array.isArray(deliveryAddressList))
+            {
+                state.deliveryAddressList = deliveryAddressList;
+            }
+        },
+
+        setDeliveryAddressId(state, deliveryAddressId)
+        {
+            if (deliveryAddressId)
+            {
+                state.deliveryAddressId = deliveryAddressId;
+            }
+        }
     };
 
 const actions =
     {
+        setBillingAddress({commit}, {id, addressList})
+        {
+            commit("setBillingAddressList", addressList);
+            commit("setBillingAddressId", id);
+        },
 
+        setDeliveryAddress({commit}, {id, addressList})
+        {
+            commit("setDeliveryAddressList", addressList);
+            commit("setDeliveryAddressId", id);
+        }
     };
 
 const getters =
