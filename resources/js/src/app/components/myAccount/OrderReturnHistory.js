0c76058fb86d48aaf8de3d316b57521f7b978a39
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-09-25 16:39:50 +0200

    ADD Pagination to OrderReturnHistory

--- resources/js/src/app/components/myAccount/OrderReturnHistory.js
+++ resources/js/src/app/components/myAccount/OrderReturnHistory.js
@@ -1,3 +1,6 @@
+const ApiService = require("services/ApiService");
+const NotificationService = require("services/NotificationService");
+
 Vue.component("order-return-history", {
 
     props: [
@@ -11,28 +14,47 @@ Vue.component("order-return-history", {
     data()
 	{
         return {
-            page: 1,
-            pageMax: 1,
-            countStart: 0,
-            countEnd: 0
+            waiting: false
         };
     },
 
     created()
 	{
         this.$options.template = this.template;
-
-        console.log(this.returnsList);
     },
 
     ready()
 	{
         this.itemsPerPage = this.itemsPerPage || 10;
-        this.pageMax = Math.ceil(this.returnsList.totalsCount / this.itemsPerPage);
     },
 
     methods:
     {
+        setPage(page)
+        {
+            if (!this.waiting)
+            {
+                this.waiting = true;
+
+                const lastPage = this.returnsList.page;
+
+                this.returnsList.page = page;
+
+                ApiService.get("/rest/io/customer/order/return", {page: page, items: this.itemsPerPage})
+                .done(response =>
+                {
+                    this.waiting = false;
+                    this.returnsList = response;
+                })
+                .fail(response =>
+                {
+                    this.waiting = false;
+                    this.returnsList.page = lastPage;
+                    NotificationService.error(Translations.Template.notFoundOops);
+                });
+            }
+        },
+
         getOriginOrderId(order)
         {
             for (const orderRef of order.orderReferences)
