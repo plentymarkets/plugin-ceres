e31b257040da25cadcc54563a88eeae2aa255f74
cjohannsen <christoph.johannsen@plentymarkets.com>
2017-12-14 15:23:07 +0100

    CHANGE SingleItemModule - new getter "variationGroupedProperties" is used to get pre-grouped variations. Also add getter "variationMissingProperties" to get all unselected properties

--- resources/js/src/app/store/modules/SingleItemModule.js
+++ resources/js/src/app/store/modules/SingleItemModule.js
@@ -101,6 +101,85 @@ const getters =
         variationTotalPrice(state, getters, rootState, rootGetters)
         {
             return getters.variationPropertySurcharge + getters.variationGraduatedPrice;
+        },
+
+        variationGroupedProperties(state)
+        {
+            if (!state || !state.variation.documents)
+            {
+                return [];
+            }
+
+            const orderPropertyList = state.variation.documents[0].data.properties;
+            const groupIds = [... new Set(orderPropertyList.map(property => property.group && property.group.id))];
+            const groups = [];
+
+            for (const id of groupIds)
+            {
+                const groupProperties = orderPropertyList.filter(property =>
+                {
+                    return property.group === id || property.group && property.group.id === id;
+                });
+
+                groups.push({
+                    group: groupProperties[0].group,
+                    properties: groupProperties.map(property => property.property),
+                    touched: false
+                });
+            }
+
+            return groups;
+        },
+
+        variationMissingProperties(state, getters)
+        {
+            if (state && state.variation.documents && state.variation.documents[0].data.properties)
+            {
+                let missingProperties = state.variation.documents[0].data.properties.filter(property =>
+                {
+                    // selection isn't supported yet
+                    return property.property.isShownOnItemPage && property.property.valueType !== "selection" && !property.property.value;
+                });
+
+                if (missingProperties.length)
+                {
+                    let radioInformation = state.variation.documents[0].data.properties.map(property =>
+                    {
+                        if (property.group && property.group.orderPropertyGroupingType === "single" && property.property.valueType === "empty")
+                        {
+                            return {
+                                groupId: property.group.id,
+                                propertyId: property.property.id,
+                                hasValue: !!property.property.value
+                            };
+                        }
+                        return null;
+                    });
+
+                    radioInformation = [... new Set(radioInformation.filter(id => id))];
+
+                    const radioIdsToRemove = [];
+
+                    for (const radioGroupId of [... new Set(radioInformation.map(radio => radio.groupId))])
+                    {
+                        const radioGroupToClean = radioInformation.find(radio => radio.groupId === radioGroupId && radio.hasValue);
+
+                        if (radioGroupToClean)
+                        {
+                            for (const radio of radioInformation.filter(radio => radio.groupId === radioGroupToClean.groupId && !radio.hasValue))
+                            {
+                                radioIdsToRemove.push(radio.propertyId);
+                            }
+                        }
+                    }
+
+                    missingProperties = missingProperties.filter(property => !radioIdsToRemove.includes(property.property.id));
+                }
+
+                return missingProperties;
+            }
+
+            return [];
         }
     };
 
