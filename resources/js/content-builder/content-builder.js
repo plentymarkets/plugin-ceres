40ff1955c756b93042b425cf3f45267ae8d2d348
Viktor Wiebe <vwiebe@uni-kassel.de>
2018-01-19 16:00:37 +0100

    first experiments with combining ceres and gridstack markup

--- resources/js/content-builder/content-builder.js
+++ resources/js/content-builder/content-builder.js
@@ -1,20 +1,68 @@
 $(document).ready(function()
 {
     initCeresForGridstack();
-    initGridstack();
 });
 
 function initCeresForGridstack()
 {
-    
+    // iterate over all body elements
+    $('body').find('*').each(function()
+    {
+       $(this).click(function (event)
+       {
+           //prevent all existing click actions
+           event.preventDefault();
+       })
+    });
+
+
+    // select drag & drop areas
+    $('.mkt-homepage').each(function(i)
+    {
+        // iterate over all sub-elements
+        $(this).find(' > div, > hr').each(function(j)
+        {
+            // create gridstack item markup
+            var gridStackItem = $(  '<div class="grid-stack-item" ' +
+                                    '     data-gs-x="0"' +
+                                    '     data-gs-y="'+ j + '"' + // one element for each row
+                                    '     data-gs-width="1"' +
+                                    '     data-gs-height="1"><div class="grid-stack-item-content"></div>' +
+                                    '</div>');
+
+            // wrap current element with gridstack item markup
+            $(this).wrap(gridStackItem)
+
+            ++j;
+        });
+
+        // add gridstack container class for current drag & drop area
+        $(this).addClass('grid-stack-' + i);
+
+
+        // initialize gridstack for current gridstack container
+        initGridstack(i, $(this).height());
+
+        ++i;
+    });
+
+
 }
 
-
-function initGridstack()
+/**
+ *
+ * @param id for current container
+ * @param height of current container (acutally not really sure about this...)
+ */
+function initGridstack(id, height)
 {
     var options = {
-        cellHeight: 80,
-        verticalMargin: 10
+        verticalMargin: 0 + 'px', // this kinda breaks everything... ?
+        width:1,
+        cellHeight: height + 'px'
     };
-    $('.grid-stack').gridstack(options);
+    console.log(height);
+
+    var selector = '.grid-stack-' + id;
+    $(selector).gridstack(options);
 }
