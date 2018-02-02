79f9d7177447dc27a61b364046f831094a8a650b
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2018-02-02 09:43:33 +0100

    improved naming

--- resources/js/src/app/components/pageDesign/Popper.js
+++ resources/js/src/app/components/pageDesign/Popper.js
@@ -19,11 +19,11 @@ Vue.component("popper", {
         this.$nextTick(() =>
         {
 			// eslint-disable-next-line
-			this.popper = new Popper(this.$refs.popperButton, this.$refs.popperNode, {
+			this.popper = new Popper(this.$refs.trigger, this.$refs.node, {
 				placement: this.placement,
 				modifiers: {
 					arrow: {
-						element: this.$refs.popperArrow
+						element: this.$refs.arrow
 					}
 				}
 			});
