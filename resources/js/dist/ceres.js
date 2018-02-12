0cc8a6b166c63e7fcb6688cf35b93889ea5495ec
BuildMonkey <buildmonkey.plentymarkets.com>
2018-02-12 16:12:38 +0100

    Automatic build

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -14859,7 +14859,11 @@ Vue.component("category-image-carousel", {
             var itemLazyImage = this.$refs.itemLazyImage;
 
             if (itemLazyImage) {
-                itemLazyImage.loadImage();
+                if (itemLazyImage.loadImage) {
+                    itemLazyImage.loadImage();
+                } else if (itemLazyImage[0] && itemLazyImage[0].loadImage) {
+                    itemLazyImage[0].loadImage();
+                }
             }
         }
     }
