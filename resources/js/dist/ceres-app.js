1968cd8d5658df31639742222496905323317cfb
BuildMonkey <buildmonkey@plentymarkets.com>
2018-03-14 14:43:23 +0100

    Automatic build

--- resources/js/dist/ceres-app.js
+++ resources/js/dist/ceres-app.js
@@ -22006,6 +22006,10 @@ var actions = {
                 commit("setBasketItems", basketItems);
                 commit("setIsBasketLoading", false);
                 resolve(basketItems);
+
+                if (window.location.pathname === "/checkout" && !basketItems.length) {
+                    window.location.pathname = "/basket";
+                }
             }).fail(function (error) {
                 commit("setIsBasketLoading", false);
                 reject(error);
