0958353cebfbf92f21af4bff720f30cd3bcf09b1
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2018-03-26 14:31:31 +0200

    added event handling and remove dom element on compile

--- resources/js/content-builder/sass-compiler.js
+++ resources/js/content-builder/sass-compiler.js
@@ -1,33 +1,39 @@
-var sass = new Sass();
+window.addEventListener("compile-sassy", function() {
+    var sass = new Sass();
 
-// HTTP requests are made relative to worker
-var base = '../../../css/';
+    // HTTP requests are made relative to worker
+    var base = '../../../css/';
+    
+    // the directory files should be made available in
+    var directory = '';
+    
+    // the files to load (relative to both base and directory)
+    var files = [
+        'ceres.scss'
+    ];
+    
+    var vars = [
+        '$brand-primary: #808080;'
+    ]
+    
+    function manipulateVars(content)
+    {
+        return vars[0] + content;
+    }
+    
+    // download the files immediately
+    sass.preloadFiles(base, directory, files, function() { 
+        sass.readFile("ceres.scss", function(read) {
+            sass.writeFile("ceres.scss", manipulateVars(read), function(write) {
+                sass.compileFile("ceres.scss", function(compiled) {
+                    console.log("comp: ", compiled);
+                    // set css to head
+                    var cssNode = document.getElementById("css");
+                    document.getElementById("css").parentNode.removeChild(cssNode);
 
-// the directory files should be made available in
-var directory = '';
-
-// the files to load (relative to both base and directory)
-var files = [
-    'ceres.scss'
-];
-
-var vars = [
-    '$brand-primary: #808080;'
-]
-
-function manipulateVars(content)
-{
-    return vars[0] + content;
-}
-
-// download the files immediately
-sass.preloadFiles(base, directory, files, function() { 
-    sass.readFile("ceres.scss", function(read) {
-        sass.writeFile("ceres.scss", manipulateVars(read), function(write) {
-            sass.compileFile("ceres.scss", function(compiled) {
-                console.log("comp: ", compiled);
-                // set css to head
+                    //INSERT NEW STUFF
+                });
             });
         });
     });
-});
+}, false);
