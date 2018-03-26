079ae6439f402d082b398e1b1e92b078c9e13610
Lukas Matzen <lukas.matzen@plentymarkets.com>
2018-03-26 15:50:22 +0200

    UPDATE replace css in head

--- resources/js/content-builder/sass-compiler.js
+++ resources/js/content-builder/sass-compiler.js
@@ -2,14 +2,14 @@ window.addEventListener("compile-sassy", function(event) {
     var sass = new Sass();
 
     // HTTP requests are made relative to worker
-    var base = '../../../css/';
+    var base = "../../../css/";
     
     // the directory files should be made available in
     var directory = '';
     
     // the files to load (relative to both base and directory)
     var files = [
-        'ceres.scss'
+        "ceres.scss"
     ];
     
     function manipulateVars(content)
@@ -18,16 +18,30 @@ window.addEventListener("compile-sassy", function(event) {
     }
     
     // download the files immediately
-    sass.preloadFiles(base, directory, files, function() { 
+    sass.preloadFiles(base, directory, files, function() {
         sass.readFile("ceres.scss", function(read) {
             sass.writeFile("ceres.scss", manipulateVars(read), function(write) {
                 sass.compileFile("ceres.scss", function(compiled) {
-                    console.log("comp: ", compiled);
                     // set css to head
-                    var cssNode = document.getElementById("css");
-                    document.getElementById("css").parentNode.removeChild(cssNode);
+                    var cssNode = document.getElementById("ceres-css");
+                    document.getElementById("ceres-css").parentNode.removeChild(cssNode);
 
-                    //INSERT NEW STUFF
+                    var head = document.head || document.getElementsByTagName("head")[0];
+                    var style = document.createElement("style");
+    
+                    style.type = "text/css";
+                    style.id = "ceres-css";
+    
+                    if (style.styleSheet)
+                    {
+                        style.styleSheet.cssText = compiled.text;
+                    }
+                    else
+                    {
+                        style.appendChild(document.createTextNode(compiled.text));
+                    }
+    
+                    head.appendChild(style);
                 });
             });
         });
