76c11982af6f0165370a915ec1febbf90c491c57
BuildMonkey <buildmonkey@plentymarkets.com>
2018-04-03 17:28:21 +0200

    Automatic build

--- resources/js/dist/ceres-app.js
+++ resources/js/dist/ceres-app.js
@@ -21848,6 +21848,12 @@ var init = function ($, window, document) {
 
                 return false;
             });
+
+            $("#accountMenuList").click(function () {
+                $("#countrySettings").collapse("hide");
+                $("#searchBox").collapse("hide");
+                $("#currencySelect").collapse("hide");
+            });
         });
     }
 
