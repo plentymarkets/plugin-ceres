3c3e0551f32043559106c9654d44b91e7be2073d
cjohannsen <christoph.johannsen@plentymarkets.com>
2018-04-04 17:23:03 +0200

    ADD test widget

---
+++ src/ContentBuilder/Grid/RowWidget.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Ceres\ContentBuilder\Grid;
+
+use Plenty\Modules\ContentBuilder\Contracts\Widget;
+use Plenty\Plugin\Templates\Twig;
+
+class RowWidget implements Widget
+{
+
+    /**
+     * Get the html representation of the widget
+     *
+     * @param int $widgetGridHeight
+     * @param int $widgetGridWidth
+     * @param array $widgetSettings
+     * @return string
+     */
+    public function getPreview(int $widgetGridHeight = 0, int $widgetGridWidth = 0, array $widgetSettings = []): string
+    {
+        $twig = pluginApp(Twig::class);
+
+        return $twig->render(
+            "Ceres::ContentBuilder.Grid.RowWidget",
+            [
+                "widgetSettings" => $widgetSettings
+            ]
+        );
+    }
+
+    /**
+     * Render the widget
+     * @param int $widgetGridPosX
+     * @param int $widgetGridPosY
+     * @param int $widgetGridHeight
+     * @param int $widgetGridWidth
+     * @param array $widgetSettings
+     * @return string
+     */
+    public function render(
+        int $widgetGridPosX = 0,
+        int $widgetGridPosY = 0,
+        int $widgetGridHeight = 0,
+        int $widgetGridWidth = 0,
+        array $widgetSettings = []
+    ): string
+    {
+        return 'number input twig';
+    }
+}
+
+?>
