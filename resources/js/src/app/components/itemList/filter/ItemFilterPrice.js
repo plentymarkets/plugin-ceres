c52459b150b9103366ca2aa415914328015d2d06
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2018-04-04 16:29:07 +0200

    added logic for the button to apply the filter and added correct markup

--- resources/js/src/app/components/itemList/filter/ItemFilterPrice.js
+++ resources/js/src/app/components/itemList/filter/ItemFilterPrice.js
@@ -11,9 +11,26 @@ Vue.component("item-filter-price", {
         }
     },
 
+    data()
+	{
+        return {
+            minPrice: "",
+            maxPrice: ""
+        };
+    },
+
     created()
     {
         this.$options.template = this.template || "#vue-item-filter-price";
+    },
+
+    computed:
+    {
+        isDisabled()
+		{
+            return (this.minPrice === "" && this.maxPrice === "") ||
+					(parseInt(this.minPrice) >= parseInt(this.maxPrice));
+        }
     },
 
     methods:
