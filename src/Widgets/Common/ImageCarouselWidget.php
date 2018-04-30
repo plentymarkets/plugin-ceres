f7e573ef89cd8b2fbc20a9b20d64a679d15463ee
cjohannsen <christoph.johannsen@plentymarkets.com>
2018-04-30 13:56:12 +0200

    CHANGE ImageCarouselWidget - add new data structure

--- src/Widgets/Common/ImageCarouselWidget.php
+++ src/Widgets/Common/ImageCarouselWidget.php
@@ -14,9 +14,13 @@ class ImageCarouselWidget extends BaseWidget
 
         foreach ($widgetSettings as $key => $value)
         {
-            if (preg_match("/^slide\d+$/", $key) && (isset($value["mobile"]["categoryId"]) || isset($value["mobile"]["variationId"]) || isset($value["mobile"]["customImagePath"])))
+            if (preg_match("/^slide\d+$/", $key) && (!empty($value["categoryId"]["mobile"]) || !empty($value["variationId"]["mobile"]) || !empty($value["customImagePath"]["mobile"])))
             {
-                $sliderParams[] = $value["mobile"];
+                $sliderParams[] = [
+                    "categoryId" => $value["categoryId"]["mobile"],
+                    "variationId" => $value["variationId"]["mobile"],
+                    "customImagePath" => $value["customImagePath"]["mobile"]
+                ];
             }
         }
 
