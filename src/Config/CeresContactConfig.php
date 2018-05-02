587651fe5d75045a02f4fc59988d1719beeaf723
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2018-05-02 15:49:46 +0200

    added configs back to php

--- src/Config/CeresContactConfig.php
+++ src/Config/CeresContactConfig.php
@@ -8,6 +8,8 @@ use Plenty\Plugin\ConfigRepository;
 class CeresContactConfig extends PluginConfig
 {
     public $shopMail;
+    public $openingTimes;
+    public $openingTimes_en;
     public $showData;
     public $apiKey;
     public $mapZoom;
@@ -19,6 +21,10 @@ class CeresContactConfig extends PluginConfig
         parent::__construct($configRepository, "Ceres");
 
         $this->shopMail = $this->getTextValue( "contact.shop_mail", "");
+
+        $this->openingTimes = $this->getTextValue("contact.opening_times", "Montag - Freitag, 00:00 - 24:00" );
+
+        $this->openingTimes_en = $this->getTextValue("contact.en.opening_times", "Monday - Friday, 00:00 - 24:00" );
 
         $this->showData = $this->getMultiSelectValue(
             "contact.show_data",
