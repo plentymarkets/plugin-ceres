ddd65c0ab89d694eaf6b46bcc311678c89edc8c0
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2018-05-03 17:14:03 +0200

    changed some functionality

--- src/Migrations/ConfigPropertiesMigration_0_0_1.php
+++ src/Migrations/ConfigPropertiesMigration_0_0_1.php
@@ -15,24 +15,24 @@ class ConfigPropertiesMigration_0_0_1
 {
     private static $translationMap =
     [
-        'cancellationFormMetaDescription'   => ['meta'      => [descriptionCancellationForm_de, descriptionCancellationForm_en]],
-        'cancellationRightsMetaDescription' => ['meta'      => [descriptionCancellationRights_de, descriptionCancellationRights_en]],
-        'contactMetaDescription'            => ['meta'      => [descriptionContact_de, descriptionContact_en]],
-        'homepageMetaDescription'           => ['meta'      => [descriptionHome_de, descriptionHome_en]],
-        'legalDisclosureMetaDescription'    => ['meta'      => [descriptionLegalDisclosure_de, descriptionLegalDisclosure_en]],
-        'privacyPolicyMetaDescription'      => ['meta'      => [descriptionPrivacyPolicy_de, descriptionPrivacyPolicy_en]],
-        'termsAndConditionsMetaDescription' => ['meta'      => [descriptionTermsAndConditions_de, descriptionTermsAndConditions_en]],
-        'contactOpeningTimes'               => ['contact'   => [openingTimes, openingTimes_en]],
-        'footerColumnTitle1'                => ['footer'    => [col1Title, col1Title]],
-        'footerColumnTitle2'                => ['footer'    => [col2Title, col2Title]],
-        'footerColumnTitle3'                => ['footer'    => [col3Title, col3Title]],
-        'footerStoreFeature1'               => ['footer'    => [storeFeature1, storeFeature1]],
-        'footerStoreFeature2'               => ['footer'    => [storeFeature2, storeFeature2]],
-        'footerStoreFeature3'               => ['footer'    => [storeFeature3, storeFeature3]],
-        'headerCompanyName'                 => ['header'    => [companyName, companyName]],
-        'itemList1ListName'                 => ['itemLists' => [list1Name_de, list1Name_en]],
-        'itemList2ListName'                 => ['itemLists' => [list2Name_de, list2Name_en]],
-        'itemList3ListName'                 => ['itemLists' => [list3Name_de, list3Name_en]]
+        'cancellationFormMetaDescription'   => ['meta'      => ['descriptionCancellationForm_de', 'descriptionCancellationForm_en']],
+        'cancellationRightsMetaDescription' => ['meta'      => ['descriptionCancellationRights_de', 'descriptionCancellationRights_en']],
+        'contactMetaDescription'            => ['meta'      => ['descriptionContact_de', 'descriptionContact_en']],
+        'homepageMetaDescription'           => ['meta'      => ['descriptionHome_de', 'descriptionHome_en']],
+        'legalDisclosureMetaDescription'    => ['meta'      => ['descriptionLegalDisclosure_de', 'descriptionLegalDisclosure_en']],
+        'privacyPolicyMetaDescription'      => ['meta'      => ['descriptionPrivacyPolicy_de', 'descriptionPrivacyPolicy_en']],
+        'termsAndConditionsMetaDescription' => ['meta'      => ['descriptionTermsAndConditions_de', 'descriptionTermsAndConditions_en']],
+        'contactOpeningTimes'               => ['contact'   => ['openingTimes', 'openingTimes_en']],
+        'footerColumnTitle1'                => ['footer'    => ['col1Title', 'col1Title']],
+        'footerColumnTitle2'                => ['footer'    => ['col2Title', 'col2Title']],
+        'footerColumnTitle3'                => ['footer'    => ['col3Title', 'col3Title']],
+        'footerStoreFeature1'               => ['footer'    => ['storeFeature1', 'storeFeature1']],
+        'footerStoreFeature2'               => ['footer'    => ['storeFeature2', 'storeFeature2']],
+        'footerStoreFeature3'               => ['footer'    => ['storeFeature3', 'storeFeature3']],
+        'headerCompanyName'                 => ['header'    => ['companyName', 'companyName']],
+        'itemList1ListName'                 => ['itemLists' => ['list1Name_de', 'list1Name_en']],
+        'itemList2ListName'                 => ['itemLists' => ['list2Name_de', 'list2Name_en']],
+        'itemList3ListName'                 => ['itemLists' => ['list3Name_de', 'list3Name_en']]
     ];
 
     /**
@@ -57,7 +57,7 @@ class ConfigPropertiesMigration_0_0_1
         {
             for($i = 0; $i <= 1; $i++)
             {
-                if(strlen($ceresConfig->{key($translation)}->{$translation[key($translation)][$i]}))
+                if(strlen($configRepo->get('Ceres.' . key($translation) . '.' .  $translation[key($translation)][$i])))
                 {
                     $lang = 'de';
 
@@ -69,7 +69,7 @@ class ConfigPropertiesMigration_0_0_1
                     $translationData = [
                         'languageCode' => $lang,
                         'key' => $key,
-                        'value' => $ceresConfig->{key($translation)}->{$translation[key($translation)][$i]},
+                        'value' => $configRepo->get('Ceres.' . key($translation) . '.' . $translation[key($translation)][$i]),
                         'pluginName' => $pluginName,
                         'pluginSetId' => $pluginSetId,
                         'fileName' => $pluginTranslationFile
