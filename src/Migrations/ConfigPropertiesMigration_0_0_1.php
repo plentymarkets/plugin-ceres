2b793c01756e2415ea7f74e7f5e0e163dfc23760
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2018-05-04 11:05:27 +0200

    fixed not allowed methods

--- src/Migrations/ConfigPropertiesMigration_0_0_1.php
+++ src/Migrations/ConfigPropertiesMigration_0_0_1.php
@@ -3,7 +3,6 @@
 namespace Ceres\Migrations;
 
 use Plenty\Modules\Plugin\Contracts\PluginRepositoryContract;
-use Ceres\Config\CeresConfig;
 use Plenty\Modules\PluginMultilingualism\Contracts\PluginTranslationRepositoryContract;
 use Plenty\Plugin\ConfigRepository;
 
@@ -15,24 +14,24 @@ class ConfigPropertiesMigration_0_0_1
 {
     private $translationMap =
     [
-        'cancellationFormMetaDescription'   => ['meta'      => ['descriptionCancellationForm_de', 'descriptionCancellationForm_en']],
-        'cancellationRightsMetaDescription' => ['meta'      => ['descriptionCancellationRights_de', 'descriptionCancellationRights_en']],
-        'contactMetaDescription'            => ['meta'      => ['descriptionContact_de', 'descriptionContact_en']],
-        'homepageMetaDescription'           => ['meta'      => ['descriptionHome_de', 'descriptionHome_en']],
-        'legalDisclosureMetaDescription'    => ['meta'      => ['descriptionLegalDisclosure_de', 'descriptionLegalDisclosure_en']],
-        'privacyPolicyMetaDescription'      => ['meta'      => ['descriptionPrivacyPolicy_de', 'descriptionPrivacyPolicy_en']],
-        'termsAndConditionsMetaDescription' => ['meta'      => ['descriptionTermsAndConditions_de', 'descriptionTermsAndConditions_en']],
-        'contactOpeningTimes'               => ['contact'   => ['openingTimes', 'openingTimes_en']],
-        'footerColumnTitle1'                => ['footer'    => ['col1Title', 'col1Title']],
-        'footerColumnTitle2'                => ['footer'    => ['col2Title', 'col2Title']],
-        'footerColumnTitle3'                => ['footer'    => ['col3Title', 'col3Title']],
-        'footerStoreFeature1'               => ['footer'    => ['storeFeature1', 'storeFeature1']],
-        'footerStoreFeature2'               => ['footer'    => ['storeFeature2', 'storeFeature2']],
-        'footerStoreFeature3'               => ['footer'    => ['storeFeature3', 'storeFeature3']],
-        'headerCompanyName'                 => ['header'    => ['companyName', 'companyName']],
-        'itemList1ListName'                 => ['itemLists' => ['list1Name_de', 'list1Name_en']],
-        'itemList2ListName'                 => ['itemLists' => ['list2Name_de', 'list2Name_en']],
-        'itemList3ListName'                 => ['itemLists' => ['list3Name_de', 'list3Name_en']]
+        'cancellationFormMetaDescription'   => ['meta'       => ['de.description_cancel_form', 'en.description_cancel_form']],
+        'cancellationRightsMetaDescription' => ['meta'       => ['de.description_cancel_rights', 'en.description_cancel_rights']],
+        'contactMetaDescription'            => ['meta'       => ['de.description_contact', 'en.description_contact']],
+        'homepageMetaDescription'           => ['meta'       => ['de.description_home', 'en.description_home']],
+        'legalDisclosureMetaDescription'    => ['meta'       => ['de.description_legal_disclosure', 'en.description_legal_disclosure']],
+        'privacyPolicyMetaDescription'      => ['meta'       => ['de.description_privacy_policy', 'en.description_privacy_policy']],
+        'termsAndConditionsMetaDescription' => ['meta'       => ['de.description_terms_and_conditions', 'en.description_terms_and_conditions']],
+        'contactOpeningTimes'               => ['contact'    => ['opening_times', 'en.opening_times']],
+        'footerColumnTitle1'                => ['footer'     => ['col_1_title', 'col_1_title']],
+        'footerColumnTitle2'                => ['footer'     => ['col_2_title', 'col_2_title']],
+        'footerColumnTitle3'                => ['footer'     => ['col_3_title', 'col_3_title']],
+        'footerStoreFeature1'               => ['footer'     => ['store_feature_1', 'store_feature_1']],
+        'footerStoreFeature2'               => ['footer'     => ['store_feature_2', 'store_feature_2']],
+        'footerStoreFeature3'               => ['footer'     => ['store_feature_3', 'store_feature_3']],
+        'headerCompanyName'                 => ['header'     => ['company_name', 'company_name']],
+        'itemList1ListName'                 => ['item.lists' => ['1.de.list_name', '1.en.list_name']],
+        'itemList2ListName'                 => ['item.lists' => ['2.de.list_name', '2.en.list_name']],
+        'itemList3ListName'                 => ['item.lists' => ['3.de.list_name', '3.en.list_name']]
     ];
 
     /**
@@ -40,9 +39,6 @@ class ConfigPropertiesMigration_0_0_1
      */
     public function run()
     {
-		/** @var CeresConfig $ceresConfig */
-		$ceresConfig = pluginApp(CeresConfig::class);
-
 		/** @var PluginTranslationRepositoryContract $translationRepo */
 		$translationRepo = pluginApp(PluginTranslationRepositoryContract::class);
 
@@ -53,13 +49,13 @@ class ConfigPropertiesMigration_0_0_1
 		$pluginName = 'Ceres';
 		$pluginTranslationFile = 'Template.properties';
 
-        foreach ($this->translationMap as $key => $translation)
+        foreach ($this->translationMap as $translationKey => $translation)
         {
-            foreach($translation as $key2 => $Value)
+            foreach($translation as $configKey => $configValue)
             {
                 for($i = 0; $i <= 1; $i++)
                 {
-                    if(strlen($configRepo->get('Ceres.' . $key2 . '.' .  $translation[$key2][$i])))
+                    if(strlen($configRepo->get('Ceres.' . $configKey . '.' .  $configValue[$i])))
                     {
                         $lang = 'de';
             
@@ -70,8 +66,8 @@ class ConfigPropertiesMigration_0_0_1
             
                         $translationData = [
                             'languageCode' => $lang,
-                            'key' => $key,
-                            'value' => $configRepo->get('Ceres.' . $key2 . '.' .  $translation[$key2][$i]),
+                            'key' => $translationKey,
+                            'value' => $configRepo->get('Ceres.' . $configKey . '.' .  $configValue[$i]),
                             'pluginName' => $pluginName,
                             'pluginSetId' => $pluginSetId,
                             'fileName' => $pluginTranslationFile
