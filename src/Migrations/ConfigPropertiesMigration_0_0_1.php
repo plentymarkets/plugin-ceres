56b9055a0f334cffe95ecd8d0013a6f22e02c617
Dominik Meyer <dominik.meyer@plentymarkets.com>
2018-05-09 13:26:05 +0200

    UPDATE multilingualism migration UPDATE template properties UPDATE config deprecated bool

--- src/Migrations/ConfigPropertiesMigration_0_0_1.php
+++ src/Migrations/ConfigPropertiesMigration_0_0_1.php
@@ -2,9 +2,10 @@
 
 namespace Ceres\Migrations;
 
-use Plenty\Modules\Plugin\Contracts\PluginRepositoryContract;
+use Plenty\Modules\Plugin\PluginSet\Contracts\PluginSetRepositoryContract;
+use Plenty\Modules\Plugin\PluginSet\Models\PluginSet;
+use Plenty\Modules\Plugin\PluginSet\Models\PluginSetEntry;
 use Plenty\Modules\PluginMultilingualism\Contracts\PluginTranslationRepositoryContract;
-use Plenty\Plugin\ConfigRepository;
 
 /**
  * Class ConfigPropertiesMigration_0_0_1
@@ -14,69 +15,139 @@ class ConfigPropertiesMigration_0_0_1
 {
     private $translationMap =
     [
-        'cancellationFormMetaDescription'   => ['meta'       => ['de.description_cancel_form', 'en.description_cancel_form']],
-        'cancellationRightsMetaDescription' => ['meta'       => ['de.description_cancel_rights', 'en.description_cancel_rights']],
-        'contactMetaDescription'            => ['meta'       => ['de.description_contact', 'en.description_contact']],
-        'homepageMetaDescription'           => ['meta'       => ['de.description_home', 'en.description_home']],
-        'legalDisclosureMetaDescription'    => ['meta'       => ['de.description_legal_disclosure', 'en.description_legal_disclosure']],
-        'privacyPolicyMetaDescription'      => ['meta'       => ['de.description_privacy_policy', 'en.description_privacy_policy']],
-        'termsAndConditionsMetaDescription' => ['meta'       => ['de.description_terms_and_conditions', 'en.description_terms_and_conditions']],
-        'contactOpeningTimes'               => ['contact'    => ['opening_times', 'en.opening_times']],
-        'footerColumnTitle1'                => ['footer'     => ['col_1_title', 'col_1_title']],
-        'footerColumnTitle2'                => ['footer'     => ['col_2_title', 'col_2_title']],
-        'footerColumnTitle3'                => ['footer'     => ['col_3_title', 'col_3_title']],
-        'footerStoreFeature1'               => ['footer'     => ['store_feature_1', 'store_feature_1']],
-        'footerStoreFeature2'               => ['footer'     => ['store_feature_2', 'store_feature_2']],
-        'footerStoreFeature3'               => ['footer'     => ['store_feature_3', 'store_feature_3']],
-        'headerCompanyName'                 => ['header'     => ['company_name', 'company_name']],
-        'itemList1ListName'                 => ['item.lists' => ['1.de.list_name', '1.en.list_name']],
-        'itemList2ListName'                 => ['item.lists' => ['2.de.list_name', '2.en.list_name']],
-        'itemList3ListName'                 => ['item.lists' => ['3.de.list_name', '3.en.list_name']]
+        'cancellationFormMetaDescription' => [
+            'de' => 'meta.de.description_cancel_form',
+            'en' => 'meta.en.description_cancel_form'
+        ],
+        'cancellationRightsMetaDescription' => [
+            'de' => 'meta.de.description_cancel_rights',
+            'en' => 'meta.en.description_cancel_rights'
+        ],
+        'contactMetaDescription' => [
+            'de' => 'meta.de.description_contact',
+            'en' => 'meta.en.description_contact'
+        ],
+        'homepageMetaDescription' => [
+            'de' => 'meta.de.description_home',
+            'en' => 'meta.en.description_home'
+        ],
+        'legalDisclosureMetaDescription' => [
+            'de' => 'meta.de.description_legal_disclosure',
+            'en' => 'meta.en.description_legal_disclosure'
+        ],
+        'privacyPolicyMetaDescription' => [
+            'de' => 'meta.de.description_privacy_policy',
+            'en' => 'meta.en.description_privacy_policy'
+        ],
+        'termsAndConditionsMetaDescription' => [
+            'de' => 'meta.de.description_terms_and_conditions',
+            'en' => 'meta.en.description_terms_and_conditions'
+        ],
+        'contactOpeningTimes' => [
+            'de' => 'contact.opening_times',
+            'en' => 'contact.en.opening_times'
+        ],
+        'footerColumnTitle1' => [
+            'de' => 'footer.col_1_title',
+            'en' => 'footer.col_1_title'
+        ],
+        'footerColumnTitle2' => [
+            'de' => 'footer.col_2_title',
+            'en' => 'footer.col_2_title'
+        ],
+        'footerColumnTitle3' => [
+            'de' => 'footer.col_3_title',
+            'en' => 'footer.col_3_title'
+        ],
+        'footerStoreFeature1' => [
+            'de' => 'footer.store_feature_1',
+            'en' => 'footer.store_feature_1'
+        ],
+        'footerStoreFeature2' => [
+            'de' => 'footer.store_feature_2',
+            'en' => 'footer.store_feature_2'
+        ],
+        'footerStoreFeature3' => [
+            'de' => 'footer.store_feature_3',
+            'en' => 'footer.store_feature_3'
+        ],
+        'headerCompanyName' => [
+            'de' => 'header.company_name',
+            'en' => 'header.company_name'
+        ],
+        'itemList1ListName' => [
+            'de' => 'item.lists.1.de.list_name',
+            'en' => 'item.lists.1.en.list_name'
+        ],
+        'itemList2ListName' => [
+            'de' => 'item.lists.2.de.list_name',
+            'en' => 'item.lists.2.en.list_name'
+        ],
+        'itemList3ListName' => [
+            'de' => 'item.lists.3.de.list_name',
+            'en' => 'item.lists.3.en.list_name'
+        ]
     ];
 
-    /**
-     * @throws \ErrorException
-     */
+   
     public function run()
+    {
+        /** @var PluginSetRepositoryContract $pluginSetRepo */
+        $pluginSetRepo = pluginApp(PluginSetRepositoryContract::class);
+        $pluginSets = $pluginSetRepo->list();
+    
+        /** @var PluginSet $pluginSet */
+        foreach($pluginSets as $pluginSet)
+        {
+            foreach ($pluginSet->pluginSetEntries as $pluginSetEntry)
+            {
+                if ($pluginSetEntry instanceof PluginSetEntry && $pluginSetEntry->plugin->name === 'Ceres')
+                {
+                    $pluginSetId = $pluginSetEntry->pluginSetId;
+                    $config      = $pluginSetEntry->configurations()->getResults();
+                    
+                    $this->migrate($pluginSetId, $config);
+                }
+            }
+        }
+    }
+    
+    public function migrate($pluginSetId, $config)
     {
 		/** @var PluginTranslationRepositoryContract $translationRepo */
 		$translationRepo = pluginApp(PluginTranslationRepositoryContract::class);
 
-		/** @var ConfigRepository $configRepo */
-		$configRepo = pluginApp(ConfigRepository::class);
-
-		$pluginSetId = pluginSetId();
 		$pluginName = 'Ceres';
 		$pluginTranslationFile = 'Template.properties';
-
-        foreach ($this->translationMap as $translationKey => $translation)
+		
+		foreach($this->translationMap as $translationKey => $configKeys)
         {
-            foreach($translation as $configKey => $configValue)
+            foreach($configKeys as $lang => $configKey)
             {
-                for($i = 0; $i <= 1; $i++)
+                $configValue = null;
+                foreach($config as $configEntry)
                 {
-                    if(strlen($configRepo->get('Ceres.' . $configKey . '.' .  $configValue[$i])))
+                    if($configEntry->key == $configKey)
                     {
-                        $lang = 'de';
-            
-                        if($i == 1)
-                        {
-                            $lang = 'en';
-                        }
-            
-                        $translationData = [
-                            'languageCode' => $lang,
-                            'key' => $translationKey,
-                            'value' => $configRepo->get('Ceres.' . $configKey . '.' .  $configValue[$i]),
-                            'pluginName' => $pluginName,
-                            'pluginSetId' => $pluginSetId,
-                            'fileName' => $pluginTranslationFile
-                        ];
-            
-                        $translationRepo->updateOrCreateTranslation($translationData);
+                        $configValue = $configEntry->value;
                     }
                 }
+                
+                if(!is_null($configValue) && strlen($configValue))
+                {
+                    $translationData = [
+                        'languageCode' => $lang,
+                        'key'          => $translationKey,
+                        'value'        => $configValue,
+                        'pluginName'   => $pluginName,
+                        'pluginSetId'  => $pluginSetId,
+                        'fileName'     => $pluginTranslationFile
+                    ];
+    
+                    $translationRepo->updateOrCreateTranslation($translationData);
+                }
+                
             }
-		}
+        }
     }
 }
