949ce1553064b507ccac0ff0c3fbcd9df7001dd3
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2018-06-14 12:51:22 +0200

    ADD item bundle component

---
+++ resources/js/src/app/components/item/ItemBundle.js
@@ -0,0 +1,34 @@
+Vue.component("item-bundle", {
+
+    props:
+    {
+        template:
+        {
+            type: String,
+            default: "#vue-item-bundle"
+        },
+        bundleType: String,
+        bundleComponents: Array
+    },
+
+    created()
+    {
+        this.$options.template = this.template;
+    },
+
+    methods:
+    {
+        showItemBundleItems()
+		{
+			// TODO check if bundle type ist set and config value from the split settings is set on NO SPLIT
+            return true;
+        },
+
+        getNoBundleText(item)
+		{
+            item.variation.bundleType = null;
+
+            return item;
+        }
+    }
+});
