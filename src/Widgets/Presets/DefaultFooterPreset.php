8d6e5fe9623e931ca1893bb441c7d4b5fff639ee
cjohannsen <christoph.johannsen@plentymarkets.com>
2018-08-17 13:44:00 +0200

    CHANGE DefaultFooterPreset.php - the grid for the list widget is now created dynamically, based on the user's settings

--- src/Widgets/Presets/DefaultFooterPreset.php
+++ src/Widgets/Presets/DefaultFooterPreset.php
@@ -87,21 +87,34 @@ class DefaultFooterPreset implements ContentPreset
     {
         $numberOfFeatures = $this->config->footer->numberOfFeatures;
 
-        $listGridPreset = $this->preset->createWidget("Ceres::ThreeColumnWidget")
-            ->withSetting("layout", "oneToOne")
-            ->withSetting("appearance", "primary");
+        $listGridPreset = null;
+
+        if ($numberOfFeatures === 2)
+        {
+            $listGridPreset = $this->preset->createWidget("Ceres::TwoColumnWidget")
+                ->withSetting("layout", "oneToOne")
+                ->withSetting("appearance", "primary");
+        }
+        else if ($numberOfFeatures === 3)
+        {
+            $listGridPreset = $this->preset->createWidget("Ceres::ThreeColumnWidget")
+                ->withSetting("layout", "oneToOneToOne")
+                ->withSetting("appearance", "primary");
+        }
+
 
         for ($i = 1; $i <= $numberOfFeatures && $i <= 3; $i++)
         {
             $storeFeatureTranslationKey = "{{ trans('Ceres::Template.footerStoreFeature' . $i) }}";
 
-            $listGridPreset->withChild($this->gridDropzoneNames[$i],
-                $this->preset->createWidget("Ceres::ListWidget")
-                    ->withSetting("icon", "fa-check")
-                    ->withSetting("text1", "Test")->toArray()
-//                    ->withSetting("text1", $storeFeatureTranslationKey)->toArray()
-            );
+            $listWidget = $this->preset->createWidget("Ceres::ListWidget")
+                ->withSetting("icon", "fa-check")
+                ->withSetting("text1", "Test");
 
+            if ($listGridPreset !== null)
+            {
+                $listGridPreset->withChild($this->gridDropzoneNames[$i], $listWidget->toArray());
+            }
         }
     }
 
