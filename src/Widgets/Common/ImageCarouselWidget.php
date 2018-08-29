2ca9ec6c40c7ec632dcef62de9091bd16540ac1d
cjohannsen <christoph.johannsen@plentymarkets.com>
2018-08-29 14:44:46 +0200

    CHANGE ItemListWidget - consider old slider settings

--- src/Widgets/Common/ImageCarouselWidget.php
+++ src/Widgets/Common/ImageCarouselWidget.php
@@ -7,4 +7,37 @@ use Ceres\Widgets\Helper\BaseWidget;
 class ImageCarouselWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.Common.ImageCarouselWidget";
+
+    protected function getTemplateData($widgetSettings, $isPreview)
+    {
+        $sliderParams = [];
+
+        foreach (["slide1", "slide2", "slide3"] as $slideName)
+        {
+            $slide = $widgetSettings[$slideName];
+
+            if (!empty($slide))
+            {
+                $sliderParams[] = [
+                    "categoryId"      => $slide["categoryId"]["mobile"],
+                    "variationId"     => $slide["variationId"]["mobile"],
+                    "customImagePath" => $slide["customImagePath"]["mobile"]
+                ];
+            }
+        }
+
+        if (count($sliderParams) > 0)
+        {
+            return [
+                "sliderParams" => [
+                    "mobile"       => $sliderParams,
+                    "tablet"       => $sliderParams,
+                    "desktop"      => $sliderParams,
+                    "largeDesktop" => $sliderParams
+                ]
+            ];
+        }
+            
+        return null;
+    }
 }
