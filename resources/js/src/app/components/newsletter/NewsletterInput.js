df87bfb30115b7099bb328b207a380b268d8d1e3
Dominik Meyer <dominik.meyer@plentymarkets.com>
2018-09-11 12:51:30 +0200

    ADD newsletter functionality and widget

---
+++ resources/js/src/app/components/newsletter/NewsletterInput.js
@@ -0,0 +1,85 @@
+import TranslationService from "services/TranslationService";
+import ValidationService from "services/ValidationService";
+const ApiService          = require("services/ApiService");
+const NotificationService = require("services/NotificationService");
+
+Vue.component("newsletter-input", {
+    props: {
+        template:
+        {
+            type: String,
+            default: "#vue-newsletter-input"
+        },
+        title:
+        {
+            type: String,
+            default: ""
+        },
+        subTitle:
+        {
+            type: String,
+            default: ""
+        },
+        showNameInputs:
+        {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    data()
+    {
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            isDisabled: false
+        };
+    },
+
+    created()
+    {
+        this.$options.template = this.template;
+    },
+
+    mounted()
+    {
+        this.$nextTick(() =>
+        {
+            this.inputFields = $(".container").find(".input-unit");
+        });
+    },
+
+    methods: {
+        validateData()
+        {
+
+            ValidationService.validate($("#newsletter-input-form"))
+                .done(() =>
+                {
+                    this.save();
+                })
+                .fail(invalidFields =>
+                {
+                    ValidationService.markInvalidFields(invalidFields, "error");
+                });
+        },
+        save()
+        {
+            ApiService.post("/rest/io/customer/newsletter", {email: this.email, firstName: this.firstName, lastName: this.lastName})
+                .done(() =>
+                {
+                    NotificationService.success(
+                        TranslationService.translate("Ceres::Template.resetPwChangePasswordSuccessful")
+                    ).closeAfter(3000);
+
+                })
+                .fail(() =>
+                {
+                    NotificationService.error(
+                        TranslationService.translate("Ceres::Template.resetPwChangePasswordFailed")
+                    ).closeAfter(5000);
+                });
+        }
+    }
+});
---
+++ src/Widgets/Newsletter/NewsletterWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Newsletter;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class NewsletterWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Newsletter.NewsletterWidget";
+}
