79c72a5054c140f036007ed126d9f139e65813d6
Felix Dausch <felix.dausch@plentymarkets.com>
2018-09-11 15:56:10 +0200

    FIX store specials

--- resources/js/src/app/components/itemList/ItemStoreSpecial.js
+++ resources/js/src/app/components/itemList/ItemStoreSpecial.js
@@ -46,14 +46,20 @@ Vue.component("item-store-special", {
     methods: {
         getLabel()
         {
-            if (this.storeSpecial.id === 1 && this.recommendedRetailPrice)
+            if (isNullOrUndefined(this.storeSpecial))
             {
-                const percent = this.getPercentageSale();
-
-                if (parseInt(percent) < 0)
+                if (isNullOrUndefined(this.recommendedRetailPrice))
                 {
-                    return percent + "%";
+                    return "";
                 }
+
+                return this.getPercentageSale();
+
+            }
+
+            if (this.storeSpecial.id === 1 && !isNullOrUndefined(this.recommendedRetailPrice))
+            {
+                return this.getPercentageSale();
             }
 
             return this.storeSpecial.names.name;
@@ -64,7 +70,12 @@ Vue.component("item-store-special", {
             // eslint-disable-next-line
             let percent = (1 - this.variationRetailPrice.unitPrice.value / this.recommendedRetailPrice.price.value ) * -100;
 
-            return percent.toFixed(this.decimalCount).replace(".", App.decimalSeparator);
+            if (percent < 0)
+            {
+                return percent.toFixed(this.decimalCount).replace(".", App.decimalSeparator) + "%";
+            }
+
+            return "";
         }
     }
 });
