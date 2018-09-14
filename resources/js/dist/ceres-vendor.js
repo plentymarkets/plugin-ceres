9d273bf8dce2b8fdc7759fe8b7f2a4c090ade50c
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2018-09-14 11:42:17 +0200

    ADD SPECIFIC BUILD FILES

--- resources/js/dist/ceres-vendor.js
+++ resources/js/dist/ceres-vendor.js
@@ -18227,7 +18227,7 @@ return jQuery;
 })(jQuery, window, document);
 
 /*!
- * Vue.js v2.5.17
+ * Vue.js v2.5.16
  * (c) 2014-2018 Evan You
  * Released under the MIT License.
  */
@@ -23303,7 +23303,7 @@ Object.defineProperty(Vue, 'FunctionalRenderContext', {
   value: FunctionalRenderContext
 });
 
-Vue.version = '2.5.17';
+Vue.version = '2.5.16';
 
 /*  */
 
@@ -30606,9 +30606,6 @@ function now() {
     if (typeof lastDuration !== 'undefined' && lastDuration > 16) {
       // We voluntarily throttle ourselves if we can't manage 60fps
       lastDuration = Math.min(lastDuration - 16, 250);
-
-      // Just in case this is the last event, remember to position just once more
-      pendingTimeout = setTimeout(tick, 250);
       return;
     }
 
@@ -32159,7 +32156,7 @@ return Tether;
       };
       detach = function() {
         detached = true;
-        win.off("touchmove", tick);
+
         win.off("scroll", tick);
         win.off("resize", recalc_and_tick);
         $(document.body).off("sticky_kit:recalc", recalc_and_tick);
@@ -32182,7 +32179,7 @@ return Tether;
           return elm.removeClass(sticky_class);
         }
       };
-      win.on("touchmove", tick);
+
       win.on("scroll", tick);
       win.on("resize", recalc_and_tick);
       $(document.body).on("sticky_kit:recalc", recalc_and_tick);
@@ -41040,9 +41037,9 @@ var Popover = (function ($) {
 
             mom = createUTC([2000, 1]).day(i);
             if (strict && !this._fullWeekdaysParse[i]) {
-                this._fullWeekdaysParse[i] = new RegExp('^' + this.weekdays(mom, '').replace('.', '\\.?') + '$', 'i');
-                this._shortWeekdaysParse[i] = new RegExp('^' + this.weekdaysShort(mom, '').replace('.', '\\.?') + '$', 'i');
-                this._minWeekdaysParse[i] = new RegExp('^' + this.weekdaysMin(mom, '').replace('.', '\\.?') + '$', 'i');
+                this._fullWeekdaysParse[i] = new RegExp('^' + this.weekdays(mom, '').replace('.', '\.?') + '$', 'i');
+                this._shortWeekdaysParse[i] = new RegExp('^' + this.weekdaysShort(mom, '').replace('.', '\.?') + '$', 'i');
+                this._minWeekdaysParse[i] = new RegExp('^' + this.weekdaysMin(mom, '').replace('.', '\.?') + '$', 'i');
             }
             if (!this._weekdaysParse[i]) {
                 regex = '^' + this.weekdays(mom, '') + '|^' + this.weekdaysShort(mom, '') + '|^' + this.weekdaysMin(mom, '');
@@ -41845,7 +41842,7 @@ var Popover = (function ($) {
 
     function preprocessRFC2822(s) {
         // Remove comments and folding whitespace and replace multiple-spaces with a single space
-        return s.replace(/\([^)]*\)|[\n\t]/g, ' ').replace(/(\s\s+)/g, ' ').replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        return s.replace(/\([^)]*\)|[\n\t]/g, ' ').replace(/(\s\s+)/g, ' ').trim();
     }
 
     function checkWeekday(weekdayStr, parsedInput, config) {
@@ -44024,7 +44021,7 @@ var Popover = (function ($) {
     // Side effect imports
 
 
-    hooks.version = '2.22.2';
+    hooks.version = '2.22.1';
 
     setHookCallback(createLocal);
 
