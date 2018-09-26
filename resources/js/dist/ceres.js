e5859042966c4fa59ad45c383d0d786278a8eb57
BuildMonkey <buildmonkey@plentymarkets.com>
2018-09-26 10:43:17 +0200

    Automatic build

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -16056,7 +16056,9 @@ Vue.component("address-input-group", {
 
             this.emitInputEvent("countryId", shippingCountry.id);
 
-            this.togglePickupStation(false);
+            if (this.isPickupStation || this.isPostOffice) {
+                this.togglePickupStation(false);
+            }
         },
         togglePickupStation: function togglePickupStation(showPickupStation) {
             var emitInputs = {
