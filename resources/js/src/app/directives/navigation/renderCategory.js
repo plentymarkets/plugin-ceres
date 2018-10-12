58c72fce4db71d27c9e95cfefc9ad1151bca4fe1
Sebastian Schwarz <sebastian.schwarz@plentymarkets.com>
2018-10-12 12:39:13 +0200

    IMPROVE code style

--- resources/js/src/app/directives/navigation/renderCategory.js
+++ resources/js/src/app/directives/navigation/renderCategory.js
@@ -20,23 +20,19 @@ Vue.directive("render-category",
 
                 if (!App.isCategoryView || currentCategoryType !== el.dataset.categoryType)
                 {
-                    let url;
-
                     // check if touch device and change the ui handling
                     if (document.body.classList.contains("touch"))
                     {
                         if (openCategory && openCategory.contains(event.target) || binding.value.alwaysOpen)
                         {
                             store.dispatch("selectCategory", {categoryId: parseInt(el.dataset.categoryId), withReload: true});
-                            url = store.state.navigation.currentCategory.url;
-                            window.open(url, "_self");
+                            window.open(store.state.navigation.currentCategory.url, "_self");
                         }
                     }
                     else
                     {
                         store.dispatch("selectCategory", {categoryId: parseInt(el.dataset.categoryId), withReload: true});
-                        url = store.state.navigation.currentCategory.url;
-                        window.open(url, "_self");
+                        window.open(store.state.navigation.currentCategory.url, "_self");
                     }
                 }
                 // check if user click the opened category and change the ui handling
