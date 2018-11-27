dce66bc0177837efca136693a2c1a88b4e9a5198
cjohannsen <christoph.johannsen@plentymarkets.com>
2018-11-27 14:31:36 +0100

    CHANGE LiveShopping - remove utc convert

--- resources/js/src/app/components/liveShopping/LiveShoppingDetails.js
+++ resources/js/src/app/components/liveShopping/LiveShoppingDetails.js
@@ -67,8 +67,8 @@ Vue.component("live-shopping-details", {
         {
             const momentNow = moment(Date.now());
 
-            this.momentBegin = moment.unix(this.liveShoppingData.liveShopping.fromTime).utc(true);
-            this.momentEnd = moment.unix(this.liveShoppingData.liveShopping.toTime).utc(true);
+            this.momentBegin = moment.unix(this.liveShoppingData.liveShopping.fromTime);
+            this.momentEnd = moment.unix(this.liveShoppingData.liveShopping.toTime);
             this.hasStarted = this.momentBegin < momentNow;
             this.hasClosed = this.momentEnd < momentNow;
 
