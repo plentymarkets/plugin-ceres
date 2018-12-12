6c28616e0dc2198b5e5d7d9ceb06ccebbd6039f3
Lukas Matzen <lukas.matzen@plentymarkets.com>
2018-12-12 10:48:44 +0100

    UPDATE build files

--- resources/js/dist/ceres-app.js
+++ resources/js/dist/ceres-app.js
@@ -22795,8 +22795,8 @@ Vue.component("item-filter", {
 
     methods: {
         updateFacet: function updateFacet(facetValue) {
-            this.$store.dispatch("selectFacetNew", { facetValue: facetValue, isSelected: this.isSelected(facetValue.id) });
-            // this.$store.dispatch("selectFacet", {facetValue});
+            this.$store.dispatch("selectFacetNew", { facetValue: facetValue });
+            // this.$store.dispatch("sselectFacet", {facetValue});
         },
         isSelected: function isSelected(facetValueId) {
             return this.selectedFacets.findIndex(function (selectedFacet) {
@@ -26853,18 +26853,16 @@ function getUrlParams(urlParams) {
 
     urlParams = urlParams.split("+").join(" ");
 
-    var result = {};
-    var params = (window.location.search.split("?")[1] || "").split("&");
+    var params = {};
+    var re = /[?&]?([^=]+)=([^&]*)/g;
+    var tokens = void 0;
 
-    for (var param in params) {
-        if (params.hasOwnProperty(param)) {
-            var paramParts = params[param].split("=");
-
-            result[paramParts[0]] = decodeURIComponent(paramParts[1] || "");
-        }
+    while (tokens) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+        tokens = re.exec(urlParams);
     }
 
-    return result;
+    return params;
 }
 
 function setUrlParams(urlParams) {
