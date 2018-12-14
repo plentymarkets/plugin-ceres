f4d840f0753f1b36f608188bf44c830224268cef
lkreimann <reimann@unidomo.com>
2018-12-14 09:15:29 +0100

    ADD breakpoints

--- resources/js/src/app/components/containers/ContainerItemList.js
+++ resources/js/src/app/components/containers/ContainerItemList.js
@@ -76,6 +76,9 @@ Vue.component("container-item-list", {
                         items: (this.itemsPerPage > 1) ? 2 : 1
                     },
                     768: {
+                        items: (this.itemsPerPage > 2) ? 3 : ((items.itemsPerPage > 1) ? 2 : 1)
+                    }, 
+                    992: {
                         items: this.itemsPerPage
                     }
                 },
