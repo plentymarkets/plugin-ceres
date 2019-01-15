9a5d725b701af98c4ec62a1cafb51eb66a87e270
Felix Dausch <felix.dausch@plentymarkets.com>
2019-01-15 14:07:20 +0100

    Exclude appearance to global setting

--- tools/collectWidgets.js
+++ tools/collectWidgets.js
@@ -3,19 +3,74 @@ var path = require('path');
 var glob = require('glob');
 
 var WIDGET_FILES = 'resources/views/Widgets/**/*.json';
-var TARGET_FILE = 'contentWidgets.json';
+var GLOB_FILES   = 'resources/views/Widgets/**/*.glob.json';
+var TARGET_FILE  = 'contentWidgets.json';
+var INCLUDE_PATTERN = /^@glob\((\w+)\)$/;
+
+console.log("Collecting widget definitions.");
+
 
 function root(dir) {
     return path.resolve(__dirname, '..', dir);
 }
 
-console.log("Collecting widget definitions.");
-var widgets = glob.sync(root(WIDGET_FILES))
-    .map(function (widgetFile)
+function readJsonFile(filename)
+{
+    try {
+        return JSON.parse(fs.readFileSync(filename, {encoding: 'utf-8'}));
+    }
+    catch( e )
     {
-        return JSON.parse(fs.readFileSync(widgetFile, {encoding: 'utf-8'}))
+        console.error("Cannot read " + filename );
+        console.log(e);
+    }
+}
+
+var globals = {};
+var globFiles = glob.sync(root(GLOB_FILES));
+
+globFiles.forEach(function(globFile)
+{
+    var key = path.basename(globFile, ".glob.json");
+    globals[key] = readJsonFile(globFile);
+});
+
+function injectGlobals(widgetSettings)
+{
+    var match;
+
+    Object.keys(widgetSettings || {}).forEach(function(key)
+    {
+        if ( typeof widgetSettings[key] === "string"
+            && (match = INCLUDE_PATTERN.exec(widgetSettings[key])) !== null )
+        {
+            if ( globals.hasOwnProperty(match[1]) )
+            {
+                widgetSettings[key] = globals[match[1]];
+            }
+        }
+        else if (!!widgetSettings[key].children)
+        {
+            widgetSettings[key].children = injectGlobals(widgetSettings[key].children);
+        }
     });
 
+    return widgetSettings;
+}
+
+
+var widgets = glob.sync(root(WIDGET_FILES))
+    .filter(function (widgetFile)
+    {
+        return globFiles.indexOf(widgetFile) < 0;
+    })
+    .map(function (widgetFile)
+    {
+        var widget = readJsonFile(widgetFile);
+        widget.settings = injectGlobals(widget.settings);
+
+        return widget;
+    });
 
 fs.writeFileSync(root(TARGET_FILE), JSON.stringify(widgets, null, 4), {encoding: 'utf-8'});
 console.log("> Done");
