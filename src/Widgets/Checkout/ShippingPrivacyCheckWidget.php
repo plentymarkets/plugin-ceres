b0d0e5431e62bf27484368e288caf294e5c14dda
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-01-25 16:45:46 +0100

    ADD ShippingPrivacyCheckWidget

---
+++ src/Widgets/Checkout/ShippingPrivacyCheckWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Checkout;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class ShippingPrivacyCheckWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Checkout.ShippingPrivacyCheckWidget";
+}
