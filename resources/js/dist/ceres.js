41d4c1aea1b52b4f59295f6913b69f20f0bd80ae
Steve Tentrop <steve.tentrop@plentymarkets.com>
2019-02-01 15:23:50 +0100

    Revert "Fix/basket add or update basket item"

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -25558,7 +25558,7 @@ Vue.directive("tooltip", {
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-var exceptionMap = exports.exceptionMap = new Map([["0", "errorActionIsNotExecuted"], ["1", "notificationsItemNotAdded"], ["2", "notificationsNotEnoughStockItem"], ["3", "notificationsInvalidResetPasswordUrl"], ["4", "notificationsCheckPassword"], ["5", "notificationsItemBundleSplitted"], ["6", "notificationsItemOutOfStock"], ["7", "newsletterOptOutSuccessMessage"], ["8", "newsletterOptInMessage"], ["9", "notificationsBasketItemsRemoved"], ["10", "notificationsBasketItemsRemovedForLanguage"], ["11", "notificationsNoEmailEntered"], ["110", "errorBasketItemVariationNotFound"], ["111", "errorBasketItemNotEnoughStockForVariation"], ["112", "errorBasketItemMaximumQuantityReachedForItem"], ["113", "errorBasketItemMaximumQuantityReachedForVariation"], ["114", "errorBasketItemMinimumQuantityNotReachedForVariation"], ["301", "notificationRemoveCouponMinimumOrderValueIsNotReached"], ["401", "notificationsCalculateShippingFailed"]]);
+var exceptionMap = exports.exceptionMap = new Map([["1", "notificationsItemNotAdded"], ["2", "notificationsNotEnoughStockItem"], ["3", "notificationsInvalidResetPasswordUrl"], ["4", "notificationsCheckPassword"], ["5", "notificationsItemBundleSplitted"], ["6", "notificationsItemOutOfStock"], ["7", "newsletterOptOutSuccessMessage"], ["8", "newsletterOptInMessage"], ["9", "notificationsBasketItemsRemoved"], ["10", "notificationsBasketItemsRemovedForLanguage"], ["11", "notificationsNoEmailEntered"], ["301", "notificationRemoveCouponMinimumOrderValueIsNotReached"], ["401", "notificationsCalculateShippingFailed"]]);
 
 exports.default = exceptionMap;
 
--- resources/js/src/app/exceptions/ExceptionMap.js
+++ resources/js/src/app/exceptions/ExceptionMap.js
@@ -1,6 +1,5 @@
 export const exceptionMap = new Map(
     [
-        ["0", "errorActionIsNotExecuted"],
         ["1", "notificationsItemNotAdded"],
         ["2", "notificationsNotEnoughStockItem"],
         ["3", "notificationsInvalidResetPasswordUrl"],
@@ -12,11 +11,6 @@ export const exceptionMap = new Map(
         ["9", "notificationsBasketItemsRemoved"],
         ["10", "notificationsBasketItemsRemovedForLanguage"],
         ["11", "notificationsNoEmailEntered"],
-        ["110", "errorBasketItemVariationNotFound"],
-        ["111", "errorBasketItemNotEnoughStockForVariation"],
-        ["112", "errorBasketItemMaximumQuantityReachedForItem"],
-        ["113", "errorBasketItemMaximumQuantityReachedForVariation"],
-        ["114", "errorBasketItemMinimumQuantityNotReachedForVariation"],
         ["301", "notificationRemoveCouponMinimumOrderValueIsNotReached"],
         ["401", "notificationsCalculateShippingFailed"]
     ]
