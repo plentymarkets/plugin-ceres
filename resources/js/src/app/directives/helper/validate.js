70c91cec8deb1cd21655224fcdab984eddaf1973
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-02-08 16:27:35 +0100

    FIX v-validte

--- resources/js/src/app/directives/helper/validate.js
+++ resources/js/src/app/directives/helper/validate.js
@@ -8,7 +8,7 @@ Vue.directive("validate", {
         }
         else
         {
-            el.dataset.validate = binding.argument || null;
+            el.dataset.validate = binding.arg || null;
         }
     }
 });
