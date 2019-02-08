029fe40db58bc453fe8592ee57fc0e0d7e871b48
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2019-02-08 14:46:59 +0100

    initial upload

---
+++ src/Widgets/MyAccount/BankDetailsWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\MyAccount;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class BankDetailsWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.MyAccount.BankDetailsWidget";
+}
