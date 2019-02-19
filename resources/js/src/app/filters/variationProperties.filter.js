3fb165b5a410a67e714e8c6a0d66c0dbf9d478ed
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-02-19 14:33:49 +0100

    REMOVE filter

--- resources/js/src/app/filters/variationProperties.filter.js
+++
@@ -1,6 +0,0 @@
-import { transformVariationProperties } from "../services/VariationPropertyService";
-
-Vue.filter("variationProperties", (variationPropertyGroups, variationProperties, propertyTypes = [], displaySetting) =>
-{
-    return transformVariationProperties(variationPropertyGroups, variationProperties, propertyTypes, displaySetting);
-});
