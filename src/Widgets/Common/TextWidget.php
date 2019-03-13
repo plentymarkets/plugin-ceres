a04989964d9f044676a7b5cf234e430734248d0a
Lukas Matzen <lks.mtzn@gmail.com>
2019-03-13 15:27:33 +0100

    UPDATE TextWidget remove inline editing code from TextWidget.php

--- src/Widgets/Common/TextWidget.php
+++ src/Widgets/Common/TextWidget.php
@@ -7,26 +7,4 @@ use Ceres\Widgets\Helper\BaseWidget;
 class TextWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.Common.TextWidget";
-
-    protected function getTemplateData($widgetSettings, $isPreview)
-    {
-        $isInlineEditable = true;
-        $content = '';
-
-        if ( $widgetSettings['useCustomMarkup']['mobile']
-            || (!array_key_exists('content', $widgetSettings) && strlen($widgetSettings['text']['mobile'])) )
-        {
-            $isInlineEditable = false;
-            $content = $widgetSettings['text']['mobile'];
-        }
-        else
-        {
-            $content = $widgetSettings['content']['mobile'];
-        }
-
-        return [
-            'isInlineEditable' => $isInlineEditable,
-            'content'          => $content
-        ];
-    }
 }
