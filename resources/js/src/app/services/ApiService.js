fa9927a2cc42460d6eb12e8aa7d1ff37b9a37552
Lukas Matzen <lks.mtzn@gmail.com>
2019-03-27 18:04:44 +0100

    FIX ApiService crashing when data is null

--- resources/js/src/app/services/ApiService.js
+++ resources/js/src/app/services/ApiService.js
@@ -1,4 +1,5 @@
 import { normalizeUrl } from "../helper/url";
+import { isDefined } from "../helper/utils";
 
 var NotificationService = require("services/NotificationService");
 var WaitScreenService   = require("services/WaitScreenService");
@@ -100,6 +101,7 @@ module.exports = (function($)
     {
         var deferred = $.Deferred();
 
+        data = isDefined(data) ? data : {};
         url = normalizeUrl(url);
         config = config || {};
         config.dataType = config.dataType || "json";
