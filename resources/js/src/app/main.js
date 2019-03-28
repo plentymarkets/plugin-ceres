87974a4524c25e19f22b531bc866137e6f3372bb
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-03-28 10:15:02 +0100

    CHANGE main.js - remove import of removed file

--- resources/js/src/app/main.js
+++ resources/js/src/app/main.js
@@ -2,8 +2,6 @@ const browserDetect = require("detect-browser");
 const NotificationService = require("services/NotificationService");
 const AutoFocusService = require("services/AutoFocusService");
 
-import "./mixins/index";
-
 // Frontend end scripts
 // eslint-disable-next-line
 var init = (function($, window, document)
