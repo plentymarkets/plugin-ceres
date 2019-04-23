896b629e8e2c9e11f611587defb5ac8a348da250
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2019-04-23 18:46:55 +0200

    added TranslationService import

--- resources/js/src/app/components/customer/ChangeEmailForm.js
+++ resources/js/src/app/components/customer/ChangeEmailForm.js
@@ -1,6 +1,8 @@
 const ApiService          = require("services/ApiService");
 const NotificationService = require("services/NotificationService");
 
+import TranslationService from "services/TranslationService";
+
 Vue.component("change-email-form", {
 
     props: {
