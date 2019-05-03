759b1388e8e57e73250eb8f9ddf89ff763f1580c
BuildMonkey <buildmonkey@plentymarkets.com>
2019-05-03 17:03:18 +0200

    Automatic build

--- resources/js/dist/ceres-app.js
+++ resources/js/dist/ceres-app.js
@@ -24359,7 +24359,7 @@ Vue.component("account-settings", {
       newMail: "",
       newMail2: "",
       accountSettingsClass: "",
-      accountEmailModal: {},
+      accountEmailModal: null,
       accountPasswordModal: {}
     };
   },
@@ -24371,7 +24371,10 @@ Vue.component("account-settings", {
     var _this = this;
 
     this.$nextTick(function () {
-      _this.accountEmailModal = ModalService.findModal(_this.$refs.accountEmailModal);
+      if (_this.$refs.accountEmailModal) {
+        _this.accountEmailModal = ModalService.findModal(_this.$refs.accountEmailModal);
+      }
+
       _this.accountPasswordModal = ModalService.findModal(_this.$refs.accountPasswordModal);
     });
   },
@@ -24476,7 +24479,10 @@ Vue.component("account-settings", {
      * Clear the fields and close the modal
      */
     clearFieldsAndClose: function clearFieldsAndClose() {
-      this.accountEmailModal.hide();
+      if (this.accountEmailModal) {
+        this.accountEmailModal.hide();
+      }
+
       this.accountPasswordModal.hide();
       this.clearFields();
     }
--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -24706,7 +24706,7 @@ Vue.component("account-settings", {
       newMail: "",
       newMail2: "",
       accountSettingsClass: "",
-      accountEmailModal: {},
+      accountEmailModal: null,
       accountPasswordModal: {}
     };
   },
@@ -24718,7 +24718,10 @@ Vue.component("account-settings", {
     var _this = this;
 
     this.$nextTick(function () {
-      _this.accountEmailModal = ModalService.findModal(_this.$refs.accountEmailModal);
+      if (_this.$refs.accountEmailModal) {
+        _this.accountEmailModal = ModalService.findModal(_this.$refs.accountEmailModal);
+      }
+
       _this.accountPasswordModal = ModalService.findModal(_this.$refs.accountPasswordModal);
     });
   },
@@ -24823,7 +24826,10 @@ Vue.component("account-settings", {
      * Clear the fields and close the modal
      */
     clearFieldsAndClose: function clearFieldsAndClose() {
-      this.accountEmailModal.hide();
+      if (this.accountEmailModal) {
+        this.accountEmailModal.hide();
+      }
+
       this.accountPasswordModal.hide();
       this.clearFields();
     }
