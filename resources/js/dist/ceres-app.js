6755a4dec45430841d4bbd5862d6d8fc11f2246a
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-05-13 16:37:07 +0200

    UPDATE build files

--- resources/js/dist/ceres-app.js
+++ resources/js/dist/ceres-app.js
@@ -29216,6 +29216,12 @@ function unmarkAllFields(form) {
 
 function _validateElement(elem) {
   var $elem = (0, _jquery.default)(elem);
+  /** return if the attribute data-validate is not present on the element */
+
+  if (!$elem[0].attributes.hasOwnProperty("data-validate")) {
+    return true;
+  }
+
   var validationKeys = $elem.attr("data-validate").split("|").map(function (i) {
     return i.trim();
   }) || ["text"];
