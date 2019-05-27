e3a1421d81c832cecd14d3d5d7f8d408f0f78f1e
Maximilian Röll <maximilian.roell@plentymarkets.com>
2019-05-27 10:12:15 +0200

    Moved graduated-prices to own widget

---
+++ src/Widgets/Item/GraduatedPriceWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Item;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class GraduatedPriceWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Item.GraduatedPriceWidget";
+}
