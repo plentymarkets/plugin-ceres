f4f425e2068132fe567f064a3a22491ed6c09627
Felix Dausch <felix.dausch@plentymarkets.com>
2019-06-04 17:06:23 +0200

    Resolve merge conflicts

--- src/ShopBuilder/DataFieldProvider/Item/TextsDataFieldProvider.php
+++ src/ShopBuilder/DataFieldProvider/Item/TextsDataFieldProvider.php
@@ -8,26 +8,14 @@ class TextsDataFieldProvider extends DataFieldProvider
 {
     function register()
     {
-<<<<<<< HEAD
-        $this->addField("Ceres::Widget.dataFieldTextsName1", "{{ item_data_field('texts.name1') }}");
-        $this->addField("Ceres::Widget.dataFieldTextsName2", "{{ item_data_field('texts.name2') }}");
-        $this->addField("Ceres::Widget.dataFieldTextsName3", "{{ item_data_field('texts.name3') }}");
-        $this->addField("Ceres::Widget.dataFieldTextsShortDescription", "{{ item_data_field_html('texts.shortDescription') }}");
-        $this->addField("Ceres::Widget.dataFieldTextsDescription", "{{ item_data_field_html('texts.description') }}");
-        $this->addField("Ceres::Widget.dataFieldTextsTechnicalData", "{{ item_data_field_html('texts.technicalData') }}");
-        $this->addField("Ceres::Widget.dataFieldTextsUrlPath", "{{ item_data_field('texts.urlPath') }}");
-        $this->addField("Ceres::Widget.dataFieldTextsMetaDescription", "{{ item_data_field('texts.metaDescription') }}");
-        $this->addField("Ceres::Widget.dataFieldTextsMetaKeywords", "{{ item_data_field('texts.keywords') }}");
-=======
-        $this->addField("name1", "Ceres::Widget.dataFieldTextsName1", "");
-        $this->addField("name2", "Ceres::Widget.dataFieldTextsName2", "");
-        $this->addField("name3", "Ceres::Widget.dataFieldTextsName3", "");
-        $this->addField("shortDescription", "Ceres::Widget.dataFieldTextsShortDescription", "");
-        $this->addField("description", "Ceres::Widget.dataFieldTextsDescription", "");
-        $this->addField("technicalData", "Ceres::Widget.dataFieldTextsTechnicalData", "");
-        $this->addField("urlPath", "Ceres::Widget.dataFieldTextsUrlPath", "");
-        $this->addField("metaDescription", "Ceres::Widget.dataFieldTextsMetaDescription", "");
-        $this->addField("metaKeywords", "Ceres::Widget.dataFieldTextsMetaKeywords", "");
->>>>>>> sb_single_item/data_field_identifier
+        $this->addField("name1", "Ceres::Widget.dataFieldTextsName1", "{{ item_data_field('texts.name1') }}");
+        $this->addField("name2", "Ceres::Widget.dataFieldTextsName2", "{{ item_data_field('texts.name2') }}");
+        $this->addField("name3", "Ceres::Widget.dataFieldTextsName3", "{{ item_data_field('texts.name3') }}");
+        $this->addField("shortDescription", "Ceres::Widget.dataFieldTextsShortDescription", "{{ item_data_field_html('texts.shortDescription') }}");
+        $this->addField("description", "Ceres::Widget.dataFieldTextsDescription", "{{ item_data_field_html('texts.description') }}");
+        $this->addField("technicalData", "Ceres::Widget.dataFieldTextsTechnicalData", "{{ item_data_field_html('texts.technicalData') }}");
+        $this->addField("urlPath", "Ceres::Widget.dataFieldTextsUrlPath", "{{ item_data_field('texts.urlPath') }}");
+        $this->addField("metaDescription", "Ceres::Widget.dataFieldTextsMetaDescription", "{{ item_data_field('texts.metaDescription') }}");
+        $this->addField("metaKeywords", "Ceres::Widget.dataFieldTextsMetaKeywords", "{{ item_data_field('texts.keywords') }}");
     }
 }
--- src/ShopBuilder/DataFieldProvider/Item/UnitDataFieldProvider.php
+++ src/ShopBuilder/DataFieldProvider/Item/UnitDataFieldProvider.php
@@ -8,22 +8,12 @@ class UnitDataFieldProvider extends DataFieldProvider
 {
     function register()
     {
-<<<<<<< HEAD
-        $this->addField("Ceres::Widget.dataFieldUnitsContent", "{{ item_data_field('unit.content') }}");
-        $this->addField("Ceres::Widget.dataFieldUnitsLength", "{{ item_data_field('variation.lengthMM') }}");
-        $this->addField("Ceres::Widget.dataFieldUnitsWidth", "{{ item_data_field('variation.widthMM') }}");
-        $this->addField("Ceres::Widget.dataFieldUnitsHeight", "{{ item_data_field('variation.heightMM') }}");
-        $this->addField("Ceres::Widget.dataFieldUnitsWeight", "{{ item_data_field('variation.weightG') }}");
-        $this->addField("Ceres::Widget.dataFieldUnitsWeightNet", "{{ item_data_field('variation.weightNetG') }}");
-        $this->addField("Ceres::Widget.dataFieldUnitsVPE", "{{ item_data_field('unit.names.name') }}");
-=======
-        $this->addField("content", "Ceres::Widget.dataFieldUnitsContent", "");
-        $this->addField("length", "Ceres::Widget.dataFieldUnitsLength", "");
-        $this->addField("width", "Ceres::Widget.dataFieldUnitsWidth", "");
-        $this->addField("height", "Ceres::Widget.dataFieldUnitsHeight", "");
-        $this->addField("weight", "Ceres::Widget.dataFieldUnitsWeight", "");
-        $this->addField("weightNet", "Ceres::Widget.dataFieldUnitsWeightNet", "");
-        $this->addField("vpe", "Ceres::Widget.dataFieldUnitsVPE", "");
->>>>>>> sb_single_item/data_field_identifier
+        $this->addField("content", "Ceres::Widget.dataFieldUnitsContent", "{{ item_data_field('unit.content') }}");
+        $this->addField("length", "Ceres::Widget.dataFieldUnitsLength", "{{ item_data_field('variation.lengthMM') }}");
+        $this->addField("width", "Ceres::Widget.dataFieldUnitsWidth", "{{ item_data_field('variation.widthMM') }}");
+        $this->addField("height", "Ceres::Widget.dataFieldUnitsHeight", "{{ item_data_field('variation.heightMM') }}");
+        $this->addField("weight", "Ceres::Widget.dataFieldUnitsWeight", "{{ item_data_field('variation.weightG') }}");
+        $this->addField("weightNet", "Ceres::Widget.dataFieldUnitsWeightNet", "{{ item_data_field('variation.weightNetG') }}");
+        $this->addField("vpe", "Ceres::Widget.dataFieldUnitsVPE", "{{ item_data_field('unit.names.name') }}");
     }
 }
