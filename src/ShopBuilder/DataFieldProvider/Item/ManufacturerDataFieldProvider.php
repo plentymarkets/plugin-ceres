0f1d95ff2cb4169ea02101ac9487307220bf4ae2
Lukas Matzen <lks.mtzn@gmail.com>
2019-06-05 12:54:04 +0200

    UPDATE ItemDataFieldPicker - remove twig print brackets from param

--- src/ShopBuilder/DataFieldProvider/Item/ManufacturerDataFieldProvider.php
+++ src/ShopBuilder/DataFieldProvider/Item/ManufacturerDataFieldProvider.php
@@ -8,7 +8,7 @@ class ManufacturerDataFieldProvider extends DataFieldProvider
 {
     function register()
     {
-        $this->addField("name", "Ceres::Widget.dataFieldItemManufacturerName", "{{ item_data_field('item.manufacturer.name') }}");
-        $this->addField("externalName", "Ceres::Widget.dataFieldItemManufacturerExternalName", "{{ item_data_field('item.manufacturer.externalName') }}");
+        $this->addField("name", "Ceres::Widget.dataFieldItemManufacturerName", "item_data_field('item.manufacturer.name')");
+        $this->addField("externalName", "Ceres::Widget.dataFieldItemManufacturerExternalName", "item_data_field('item.manufacturer.externalName')");
     }
 }
