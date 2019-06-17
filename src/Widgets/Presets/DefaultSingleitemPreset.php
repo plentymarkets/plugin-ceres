03bfc9a64053d199faf32b00e19ccee4b93a0ec8
Maximilian Röll <maximilian.roell@plentymarkets.com>
2019-06-17 16:57:50 +0200

    # Added: Default preset for single item page

---
+++ src/Widgets/Presets/DefaultSingleitemPreset.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Ceres\Widgets\Presets;
+
+use Ceres\Config\CeresConfig;
+use Ceres\Widgets\Helper\Factories\PresetWidgetFactory;
+use Ceres\Widgets\Helper\PresetHelper;
+use Plenty\Modules\ShopBuilder\Contracts\ContentPreset;
+
+class DefaultSingleitemPreset implements ContentPreset
+{
+    /** @var PresetHelper */
+    private $preset;
+
+    /** @var CeresConfig */
+    private $ceresConfig;
+
+    /** @var PresetWidgetFactory */
+    private $twoColumnWidget;
+
+    /** @var PresetWidgetFactory */
+    private $stickyContainer;
+
+    /** @var PresetWidgetFactory */
+    private $twoColumnWidgetAddresses;
+
+    /** @var PresetWidgetFactory */
+    private $twoColumnWidgetAccountSettings;
+
+    public function getWidgets()
+    {
+        $this->preset = pluginApp(PresetHelper::class);
+        $this->ceresConfig = pluginApp(CeresConfig::class);
+        $this->twoColumnWidget();
+        $this->createItemImageWidget();
+        $this->createStickyContainer();
+        $this->createManufacturer();
+        $this->createNameHeader();
+        $this->createSeparatorWidget();
+        $this->createItemVariationNumber();
+        $this->createItemPriceWidget();
+        $this->createItemAvailabilityWidget();
+        $this->createAddToBasketWidget();
+        $this->createAddToWishListWiget();
+        $this->createSeparatorWidget();
+        $text = `* {% if services.customer.showNetPrices() %}{{ trans("Ceres::Template.singleItemExclVAT") }}{% else %}{{ trans("Ceres::Template.singleItemInclVAT") }}{% endif %} {{ trans("Ceres::Template.singleItemExclusive") }}
+                <a {% if ceresConfig.global.shippingCostsCategoryId > 0 %} data-toggle="modal" href="#shippingscosts"{% endif %} title="{{ trans("Ceres::Template.singleItemShippingCosts") }}">{{ trans("Ceres::Template.singleItemShippingCosts") }}</a>`;
+        $this->stickyContainer->createChild('second','Ceres::TextWidget')
+            ->withSetting('text',$text)
+            ->withSetting('appearance', 'none');
+            $this->preset->createWidget('Ceres::TextWidget')
+            ->withSetting('appearance, none')
+            ->withSetting('text','<h1 class="text-muted">{{ trans("Ceres::Template.singleItemShippingCosts") }}</h1>');
+        return $this->preset->toArray();
+    }
+
+    private function createStickyContainer()
+    {
+        $this->stickyContainer = $this->twoColumnWidget->createChild('second', 'Ceres::StickyContainerWidget');
+    }
+
+    private function twoColumnWidget()
+    {
+        $this->twoColumnWidget = $this->preset->createWidget('Ceres::TwoColumnWidget')->withSetting('layout', 'sevenToFive');
+    }
+
+    private function createManufacturer()
+    {
+        $this->stickyContainer->createChild('sticky','Ceres::InlineTextWidget')
+            ->withSetting('appearance','none')
+            ->withSetting('spacing.customPadding', true)
+            ->withSetting('spacing.padding.left.value', 0)
+            ->withSetting('spacing.padding.left.unit', null)
+            ->withSetting('spacing.padding.right.value', 0)
+            ->withSetting('spacing.padding.right.unit', null)
+            ->withSetting('text',"{# SHOPBUILDER:DATA_FIELD Ceres\\ShopBuilder\\DataFieldProvider\\Item\\ManufacturerDataFieldProvider::externalName #}{{ item_data_field('item.manufacturer.externalName') }}");
+    }
+    private function createNameHeader()
+    {
+        $this->stickyContainer->createChild('sticky', 'Ceres::InlineTextWidget')
+            ->withSetting('spacing.customPadding', true)
+            ->withSetting('spacing.padding.left.value', 0)
+            ->withSetting('spacing.padding.left.unit', null)
+            ->withSetting('spacing.padding.right.value', 0)
+            ->withSetting('spacing.padding.right.unit', null)
+            ->withSetting('text', "<h1>{# SHOPBUILDER:DATA_FIELD Ceres\\ShopBuilder\\DataFieldProvider\\Item\\TextsDataFieldProvider::name1 #}{{ item_data_field('texts.name1') }}</h1>")
+            ->withSetting('appearance','none');
+    }
+
+    private function createAddToBasketWidget()
+    {
+        $this->stickyContainer->createChild('sticky', 'Ceres::AddToBasketWidget')
+            ->withSetting('buttonSize', 'lg')
+            ->withSetting('spacing.customMargin', true)
+            ->withSetting('spacing.margin.top.value', 4)
+            ->withSetting('spacing.margin.top.unit', null)
+            ->withSetting('spacing.margin.bottom.value', 4)
+            ->withSetting('spacing.margin.bottom.unit', null);
+    }
+
+    private function createItemAvailabilityWidget()
+    {
+        $this->stickyContainer->createChild('sticky', 'Ceres::ItemAvailabilityWidget');
+    }
+
+    private function createAddToWishListWiget()
+    {
+        $this->stickyContainer->createChild('sticky', 'Ceres::AddToWishListWidget');
+    }
+
+    private function createItemPriceWidget()
+    {
+        $this->stickyContainer->createChild('sticky', 'Ceres::ItemPriceWidget')
+            ->withSetting('showCrossPrice', true)
+            ->withSetting('appearance', 'primary');
+    }
+
+    private function createItemVariationNumber()
+    {
+        $text = '';
+        $text .= "<b>{{ trans(\"Ceres::Template.singleItemNumber\") }}</b>";
+        $text .= "{# SHOPBUILDER:DATA_FIELD Ceres\\ShopBuilder\\DataFieldProvider\\Item\\VariationGlobalDataFieldProvider::number #}";
+        $text .= "{{ item_data_field('variation.number') }}";
+
+        $this->stickyContainer->createChild('sticky', 'Ceres::InlineTextWidget')
+            ->withSetting('appearance', 'none')
+            ->withSetting('text', $text)
+            ->withSetting('spacing.customPadding', true)
+            ->withSetting('spacing.padding.left.value', 0)
+            ->withSetting('spacing.padding.left.unit', null)
+            ->withSetting('spacing.padding.right.value', 0)
+            ->withSetting('spacing.padding.right.unit', null);
+    }
+
+    private function createItemImageWidget()
+    {
+        $this->twoColumnWidget->createChild('first', 'Ceres::ItemImageWidget')
+            ->withSetting('appearance', 'primary')
+            ->withSetting('maxQuantity', 10)
+            ->withSetting('imageSize', 'urlMiddle')
+            ->withSetting('showThumbs', true)
+            ->withSetting('showDots', true);
+    }
+
+    private function createSeparatorWidget()
+    {
+        $this->stickyContainer->createChild('sticky', 'Ceres::SeparatorWidget');
+    }
+
+
+}
