5a3b718394e888e6da0c7108b9d837a67650161c
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-06-26 19:18:47 +0200

    REFACTOR ageRestriction filter

--- resources/js/src/app/filters/ageRestriction.filter.js
+++ resources/js/src/app/filters/ageRestriction.filter.js
@@ -2,28 +2,24 @@ import TranslationService from "services/TranslationService";
 
 Vue.filter("ageRestriction", age =>
 {
-    let ageRestrictionText = "";
-
     if (age === 0)
     {
-        ageRestrictionText = TranslationService.translate("Ceres::Template.singleItemAgeRestrictionNone");
+        return TranslationService.translate("Ceres::Template.singleItemAgeRestrictionNone");
     }
     else if (age > 0 && age <= 18)
     {
-        ageRestrictionText = TranslationService.translate("Ceres::Template.singleItemAgeRestriction", { age });
+        return TranslationService.translate("Ceres::Template.singleItemAgeRestriction", { age });
     }
     else if (age === 50)
     {
-        ageRestrictionText = TranslationService.translate("Ceres::Template.singleItemAgeRestrictionNotFlagged");
+        return TranslationService.translate("Ceres::Template.singleItemAgeRestrictionNotFlagged");
     }
     else if (age === 88)
     {
-        ageRestrictionText = TranslationService.translate("Ceres::Template.singleItemAgeRestrictionNotRequired");
+        return TranslationService.translate("Ceres::Template.singleItemAgeRestrictionNotRequired");
     }
     else
     {
-        ageRestrictionText = TranslationService.translate("Ceres::Template.singleItemAgeRestrictionUnknown");
+        return TranslationService.translate("Ceres::Template.singleItemAgeRestrictionUnknown");
     }
-
-    return ageRestrictionText;
 });
