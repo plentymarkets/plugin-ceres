6b7a68ec2a14f112c6d3b83af2702d15c061124b
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-07-08 13:11:24 +0200

    ADD 5 default presets for legal pages

---
+++ src/Widgets/Presets/Legal/DefaultGTCPreset.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ceres\Widgets\Presets\Legal;
+
+use Ceres\Widgets\Helper\Factories\PresetWidgetFactory;
+use Ceres\Widgets\Helper\PresetHelper;
+use Plenty\Modules\ShopBuilder\Contracts\ContentPreset;
+
+class DefaultGTCPreset implements ContentPreset
+{
+    /** @var PresetHelper $preset */
+    private $preset;
+
+    public function getWidgets()
+    {
+        $this->preset = pluginApp(PresetHelper::class);
+
+        $this->createHeadline();
+        $this->createLegalTextsWidget();
+
+        return $this->preset->toArray();
+    }
+
+    private function createHeadline()
+    {
+        $text = '';
+        $text .= '{% autoescape false %}';
+        $text .= '<h1 class="print-header">{{ trans("Ceres::Template.termsAndConditions", {"hyphen": "&shy;"}) }}</h1>';
+        $text .= '{% endautoescape %}';
+
+        $this->preset->createWidget('Ceres::TextWidget')
+                     ->withSetting("text", $text)
+                     ->withSetting("appearance", "none")
+                     ->withSetting("customPadding", true)
+                     ->withSetting("padding.top.value", 3)
+                     ->withSetting("padding.top.unit", null)
+                     ->withSetting("padding.bottom.value", 0)
+                     ->withSetting("padding.bottom.unit", null)
+                     ->withSetting("customMargin", true)
+                     ->withSetting("margin.bottom.value", 0)
+                     ->withSetting("margin.bottom.unit", null);
+    
+        $this->preset->createWidget("Ceres::SeparatorWidget")
+                     ->withSetting("margin.top.value", 5)
+                     ->withSetting("margin.top.unit", null)
+                     ->withSetting("margin.bottom.value", 5)
+                     ->withSetting("margin.bottom.unit", null);
+    }
+
+    private function createLegalTextsWidget()
+    {
+        $this->preset->createWidget("Ceres::LegalTextsWidget")
+        ->withSetting("type", "gtc");
+    }
+}
