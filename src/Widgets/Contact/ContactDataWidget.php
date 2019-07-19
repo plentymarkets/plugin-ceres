481e4d8389eb84989fd90da1d5bcb6aa0f479408
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-07-19 10:47:52 +0200

    ADD ContactDataWidget

---
+++ src/Widgets/Contact/ContactDataWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Contact;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class ContactDataWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Contact.ContactDataWidget";
+}
