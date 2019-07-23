e0231188281a972e84291f2f6dd1b4c6b34c3fe2
BuildMonkey <buildmonkey@plentymarkets.com>
2019-07-23 18:07:35 +0200

    Automatic build

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -41022,7 +41022,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 Vue.component("item-store-special", {
   delimiters: ["${", "}"],
   template: "#vue-item-store-special",
-  props: ["storeSpecial", "recommendedRetailPrice", "variationRetailPrice", "decimalCount", "bundleType"],
+  props: ["storeSpecial", "recommendedRetailPrice", "variationRetailPrice", "specialOfferPrice", "decimalCount", "bundleType"],
   data: function data() {
     return {
       tagClass: "",
@@ -41065,8 +41065,15 @@ Vue.component("item-store-special", {
       return this.labels[this.storeSpecial.id] || this.storeSpecial.names.name;
     },
     getPercentageSale: function getPercentageSale() {
-      // eslint-disable-next-line
-      var percent = (1 - this.variationRetailPrice.unitPrice.value / this.recommendedRetailPrice.unitPrice.value) * -100;
+      var percent;
+
+      if ((0, _utils.isDefined)(this.specialOfferPrice)) {
+        // eslint-disable-next-line
+        percent = (1 - this.specialOfferPrice.unitPrice.value / this.variationRetailPrice.unitPrice.value) * -100;
+      } else {
+        // eslint-disable-next-line
+        percent = (1 - this.variationRetailPrice.unitPrice.value / this.recommendedRetailPrice.unitPrice.value) * -100;
+      }
 
       if (percent < 0) {
         return percent.toFixed(this.decimalCount).replace(".", App.decimalSeparator) + "%";
