9c7fe2479ad23951c90d610067051095310d7aa9
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-07-29 18:16:39 +0200

    Add skeleton files for privacypolicy widget

---
+++ src/Widgets/Form/AcceptPrivacyPolicyWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Contact;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class AcceptPrivacyPolicyWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Form.AcceptPrivacyPolicyWidget";
+}
