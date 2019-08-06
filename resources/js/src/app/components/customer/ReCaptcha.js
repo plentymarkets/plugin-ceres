b77d198553a6f7712ed194a8e409fb542aa5b78e
Felix Dausch <felix.dausch@plentymarkets.com>
2019-08-06 14:11:16 +0200

    FIX contact form

--- resources/js/src/app/components/customer/ReCaptcha.js
+++ resources/js/src/app/components/customer/ReCaptcha.js
@@ -2,12 +2,7 @@ let gRecaptchaApiLoaded;
 
 Vue.component("recaptcha", {
 
-    props: {
-        template: {
-            type: String,
-            default: "#vue-recaptcha"
-        }
-    },
+    template: "<div data-recaptcha></div>",
 
     data()
     {
@@ -29,6 +24,11 @@ Vue.component("recaptcha", {
     {
         createScript()
         {
+            if (!this.apiKey)
+            {
+                return Promise.resolve();
+            }
+
             if (!gRecaptchaApiLoaded)
             {
                 gRecaptchaApiLoaded = new Promise((resolve, reject) =>
@@ -61,21 +61,24 @@ Vue.component("recaptcha", {
 
         initializeV3()
         {
-            grecaptcha.ready(() =>
+            if (window.grecaptcha)
             {
-                if (this.version !== 3)
+                window.grecaptcha.ready(() =>
                 {
-                    this.$el.dataset.recaptcha = grecaptcha.render(
-                        this.$el,
-                        {
-                            sitekey: this.apiKey,
-                            size: this.version === 1 ? "invisible" : "normal",
-                            badge: this.version === 1 ? "bottomright" : null,
-                            callback: this.recaptchaCallback.bind(this)
-                        }
-                    );
-                }
-            });
+                    if (this.version !== 3)
+                    {
+                        this.$el.dataset.recaptcha = window.grecaptcha.render(
+                            this.$el,
+                            {
+                                sitekey: this.apiKey,
+                                size: "invisible",
+                                badge: "bottomright",
+                                callback: this.recaptchaCallback.bind(this)
+                            }
+                        );
+                    }
+                });
+            }
         },
 
         recaptchaCallback(response)
