716858e51a16515e649039079ae1653d0e5668d8
Steve Tentrop <steve.tentrop@plentymarkets.com>
2019-08-12 11:24:55 +0200

    add jsdoc and a console warning

--- resources/js/src/app/filters/arrayFirst.filter.js
+++ resources/js/src/app/filters/arrayFirst.filter.js
@@ -1,4 +1,9 @@
+/**
+ * @deprecated since version 4.2.0
+ * Will be deleted in version 4.3.0
+ */
 Vue.filter("arrayFirst", function(array)
 {
+    console.warn("arrayFirst is a depricated vue filter!");
     return array[0];
 });
--- resources/js/src/app/filters/attachText.filter.js
+++ resources/js/src/app/filters/attachText.filter.js
@@ -1,4 +1,10 @@
+/**
+ * @deprecated since version 4.2.0
+ *
+ * Will be deleted in version 4.3.0
+ */
 Vue.filter("attachText", function(item, text)
 {
+    console.warn("attachText is a depricated vue filter!");
     return text + item;
 });
