4789499bafd82e792a0a66db2de0dc0c36aa2f67
Dominik Meyer <dominik.meyer@plentymarkets.com>
2019-08-21 16:34:35 +0200

    get template data for order data widget

--- src/Widgets/OrderConfirmation/OrderDataWidget.php
+++ src/Widgets/OrderConfirmation/OrderDataWidget.php
@@ -3,8 +3,25 @@
 namespace Ceres\Widgets\OrderConfirmation;
 
 use Ceres\Widgets\Helper\BaseWidget;
+use IO\Services\OrderService;
 
 class OrderDataWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.OrderConfirmation.OrderDataWidget";
+    
+    protected function getTemplateData($widgetSettings, $isPreview)
+    {
+        if($isPreview)
+        {
+            /** @var OrderService $orderService */
+            $orderService = pluginApp(OrderService::class);
+            $order = $orderService->getRandomOrder();
+            
+            return [
+                'data' => $order,
+                'totals' => $order->totals,
+                'showAdditionalPaymentInformation' => true
+            ];
+        }
+    }
 }
