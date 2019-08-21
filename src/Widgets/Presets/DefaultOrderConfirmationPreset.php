566697e5f37c49f95d18f69ecc2a423378d14ae5
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-08-21 19:25:27 +0200

    CHANGE OrderConfirmationPreset - add default arrays

--- src/Widgets/Presets/DefaultOrderConfirmationPreset.php
+++ src/Widgets/Presets/DefaultOrderConfirmationPreset.php
@@ -52,7 +52,7 @@ class DefaultOrderConfirmationPreset implements ContentPreset
     
     private function createHeadline()
     {
-        $this->preset->createWidget('Ceres::InlineTextWidget')
+        $this->preset->createWidget("Ceres::InlineTextWidget")
                      ->withSetting("text", "<h1 class=\"h2\">{{ trans(\"Ceres::Template.orderConfirmationThanks\") }}</h1>")
                      ->withSetting("appearance", "none")
                      ->withSetting("spacing.customPadding", true)
@@ -85,7 +85,7 @@ class DefaultOrderConfirmationPreset implements ContentPreset
     
     private function createOrderDataWidget()
     {
-        $this->twoColumnWidget->createChild('first', 'Ceres::OrderDataWidget')
+        $this->twoColumnWidget->createChild("first", "Ceres::OrderDataWidget")
                               ->withSetting("spacing.customMargin", true)
                               ->withSetting("spacing.margin.top.value", 3)
                               ->withSetting("spacing.margin.top.unit", null)
@@ -99,27 +99,29 @@ class DefaultOrderConfirmationPreset implements ContentPreset
                               ->withSetting("spacing.padding.top.value", 3)
                               ->withSetting("spacing.padding.top.unit", null)
                               ->withSetting("spacing.padding.bottom.value", 3)
-                              ->withSetting("spacing.padding.bottom.unit", null);
+                              ->withSetting("spacing.padding.bottom.unit", null)
+                              ->withSetting("addressFields", ["title", "contactPerson", "name1", "name2", "name3", "name4", "address1", "address2", "address3", "address4", "postalCode", "town", "country"]);
     }
 
     private function createTrackingLinkWidget()
     {
-        $this->threeColumnWidget->createChild('first', 'Ceres::LinkWidget');
+        $this->threeColumnWidget->createChild("first", "Ceres::LinkWidget");
     }
 
     private function createOrderDocumentsWidget()
     {
-        $this->threeColumnWidget->createChild('second', 'Ceres::OrderDocumentsWidget');
+        $this->threeColumnWidget->createChild("second", "Ceres::OrderDocumentsWidget")
+            ->withSetting("customClass","");
     }
 
     private function createRetourLinkWidget()
     {
-        $this->threeColumnWidget->createChild('third', 'Ceres::LinkWidget');
+        $this->threeColumnWidget->createChild("third", "Ceres::LinkWidget");
     }
 
     private function createPurchasedItemsWidget()
     {
-        $this->twoColumnWidget->createChild('second', 'Ceres::PurchasedItemsWidget')
+        $this->twoColumnWidget->createChild("second", "Ceres::PurchasedItemsWidget")
                               ->withSetting("spacing.customMargin", true)
                               ->withSetting("spacing.margin.top.value", 3)
                               ->withSetting("spacing.margin.top.unit", null)
@@ -133,41 +135,42 @@ class DefaultOrderConfirmationPreset implements ContentPreset
 
     private function createOrderTotalsWidget()
     {
-        $this->twoColumnWidget->createChild('second', 'Ceres::OrderTotalsWidget')
+        $this->twoColumnWidget->createChild("second", "Ceres::OrderTotalsWidget")
                               ->withSetting("spacing.customMargin", true)
                               ->withSetting("spacing.margin.top.value", 3)
                               ->withSetting("spacing.margin.top.unit", null)
                               ->withSetting("spacing.margin.left.value", 4)
                               ->withSetting("spacing.margin.left.unit", null)
                               ->withSetting("spacing.margin.right.value", 4)
-                              ->withSetting("spacing.margin.right.unit", null);
+                              ->withSetting("spacing.margin.right.unit", null)
+                              ->withSetting("visibleFields", ["orderValueNet", "orderValueGross", "rebate", "shippingCostsNet", "shippingCostsGross", "totalSumNet", "promotionCoupon", "vats", "totalSumGross", "salesCoupon", "openAmount"]);
     }
 
     private function createHomeLinkWidget()
     {
-        $this->fourColumnWidget->createChild('second', 'Ceres::LinkWidget');
+        $this->fourColumnWidget->createChild("second", "Ceres::LinkWidget");
     }
 
     private function createMyAccountLinkWidget()
     {
-        $this->fourColumnWidget->createChild('third', 'Ceres::LinkWidget');
+        $this->fourColumnWidget->createChild("third", "Ceres::LinkWidget");
     }
 
     
     private function createTwoColumnWidget()
     {
-        $this->twoColumnWidget = $this->preset->createWidget('Ceres::TwoColumnWidget')
-                                              ->withSetting('layout', 'oneToOne');
+        $this->twoColumnWidget = $this->preset->createWidget("Ceres::TwoColumnWidget")
+                                              ->withSetting("layout", "oneToOne");
     }
     
     private function createThreeColumnWidget()
     {
-        $this->threeColumnWidget = $this->twoColumnWidget->createChild('first', 'Ceres::ThreeColumnWidget')
-                                                         ->withSetting('layout', 'oneToOneToOne');
+        $this->threeColumnWidget = $this->twoColumnWidget->createChild("first", "Ceres::ThreeColumnWidget")
+                                                         ->withSetting("layout", "oneToOneToOne");
     }
 
     private function createFourColumnWidget()
     {
-        $this->fourColumnWidget = $this->preset->createWidget('Ceres::FourColumnWidget');
+        $this->fourColumnWidget = $this->preset->createWidget("Ceres::FourColumnWidget");
     }
 }
