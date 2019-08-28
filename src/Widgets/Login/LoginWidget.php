b9e7e2e281733858d82bf4e9603c53f667e94b5c
Lukas Matzen <lks.mtzn@gmail.com>
2019-08-28 16:44:16 +0200

    ADD LoginWidget - file structure

---
+++ src/Widgets/Login/LoginWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Login;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class LoginWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Login.LoginWidget";
+}
