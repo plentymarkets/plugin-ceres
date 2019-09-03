73cf004510c5936eb878430a5f2d10bbefbfbd66
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2019-09-03 18:00:32 +0200

    intermediate status

--- src/Widgets/Presets/DefaultLoginPreset.php
+++ src/Widgets/Presets/DefaultLoginPreset.php
@@ -6,6 +6,7 @@ use Ceres\Config\CeresConfig;
 use Ceres\Widgets\Helper\Factories\PresetWidgetFactory;
 use Ceres\Widgets\Helper\PresetHelper;
 use IO\Extensions\Constants\ShopUrls;
+use IO\Helper\RouteConfig;
 use Plenty\Modules\ShopBuilder\Contracts\ContentPreset;
 use Plenty\Plugin\Translation\Translator;
 
@@ -27,47 +28,40 @@ class DefaultLoginPreset implements ContentPreset
     {
         $this->preset = pluginApp(PresetHelper::class);
         $this->translator = pluginApp(Translator::class);
-        
         $this->shopUrls = pluginApp(ShopUrls::class);
         
-        $this->createThreeColumnWidget();
+        $this->createTwoColumnWidget();
 
+        $this->createTextWidgetLogin();
         $this->createLoginWidget();
-        $this->createSeparatorWidget();
+        $this->createTextWidgetGuest();
         $this->createGuestLoginWidget();
-        $this->createTextWidget();
+        $this->createTextWidgetRegister();
         $this->createRegisterLinkWidget();
         
         return $this->preset->toArray();
     }
     
-    private function createThreeColumnWidget()
+    private function createTwoColumnWidget()
     {
-        $this->structurWidget = $this->preset->createWidget("Ceres::ThreeColumnWidget")
-                                             ->withSetting("layout", "oneToTwoToOne");
+        $this->structurWidget = $this->preset->createWidget("Ceres::TwoColumnWidget")
+                                             ->withSetting("layout", "oneToOne");
     }
 
     private function createLoginWidget()
     {
-        $this->structurWidget->createChild("second", "Ceres::LoginWidget")
-        ->withSetting("customClass","");
+        $this->structurWidget->createChild("first", "Ceres::LoginWidget")
+                             ->withSetting("customClass","")
+                             ->withSetting("spacing.customMargin", true)
+                             ->withSetting("spacing.margin.top.value", 3)
+                             ->withSetting("spacing.margin.top.unit", null);
     }
 
-    private function createSeparatorWidget()
+    private function createTextWidgetLogin()
     {
-        $this->structurWidget->createChild("second", "Ceres::SeparatorWidget")
-        ->withSetting("customClass","");
-    }
-
-    private function createGuestLoginWidget()
-    {
-        $this->structurWidget->createChild("second", "Ceres::GuestLoginWidget");
-    }
-
-    private function createTextWidget()
-    {
-        $this->structurWidget->createChild("second", "Ceres::InlineTextWidget")
-                             ->withSetting("text", "<h1 class=\"h2\">{{ trans(\"Ceres::Template.loginCallToAction\") }}</h1>")
+        $this->structurWidget->createChild("first", "Ceres::InlineTextWidget")
+                             ->withSetting("customClass","h1")
+                             ->withSetting("text", "<div>{{ trans(\"Ceres::Template.login\") }}<div>")
                              ->withSetting("appearance", "none")
                              ->withSetting("spacing.customPadding", true)
                              ->withSetting("spacing.padding.top.value", 0)
@@ -76,18 +70,82 @@ class DefaultLoginPreset implements ContentPreset
                              ->withSetting("spacing.padding.bottom.unit", null)
                              ->withSetting("spacing.padding.left.value", 0)
                              ->withSetting("spacing.padding.left.unit", null)
+                             ->withSetting("spacing.padding.right.value", 0)
+                             ->withSetting("spacing.padding.right.unit", null)
                              ->withSetting("spacing.customMargin", true)
+                             ->withSetting("spacing.margin.top.value", 3)
+                             ->withSetting("spacing.margin.top.unit", null)
+                             ->withSetting("spacing.margin.bottom.value", 0)
+                             ->withSetting("spacing.margin.bottom.unit", null);
+    }
+
+    private function createTextWidgetGuest()
+    {
+        $this->structurWidget->createChild("second", "Ceres::InlineTextWidget")
+                             ->withSetting("customClass","h1")
+                             ->withSetting("text", "<div>{{ trans(\"Ceres::Template.loginOrderAsGuest\") }}<div>")
+                             ->withSetting("appearance", "none")
+                             ->withSetting("spacing.customPadding", true)
+                             ->withSetting("spacing.padding.top.value", 0)
+                             ->withSetting("spacing.padding.top.unit", null)
+                             ->withSetting("spacing.padding.bottom.value", 0)
+                             ->withSetting("spacing.padding.bottom.unit", null)
+                             ->withSetting("spacing.padding.left.value", 0)
+                             ->withSetting("spacing.padding.left.unit", null)
+                             ->withSetting("spacing.padding.right.value", 0)
+                             ->withSetting("spacing.padding.right.unit", null)
+                             ->withSetting("spacing.customMargin", true)
+                             ->withSetting("spacing.margin.top.value", 3)
+                             ->withSetting("spacing.margin.top.unit", null)
+                             ->withSetting("spacing.margin.bottom.value", 0)
+                             ->withSetting("spacing.margin.bottom.unit", null);
+    }
+
+    private function createGuestLoginWidget()
+    {
+        $this->structurWidget->createChild("second", "Ceres::GuestLoginWidget");
+    }
+
+    private function createTextWidgetRegister()
+    {
+        $this->structurWidget->createChild("first", "Ceres::InlineTextWidget")
+                             ->withSetting("customClass","h1")
+                             ->withSetting("text", "<div>{{ trans(\"Ceres::Template.loginCallToAction\") }}</div>")
+                             ->withSetting("appearance", "none")
+                             ->withSetting("spacing.customPadding", true)
+                             ->withSetting("spacing.padding.top.value", 0)
+                             ->withSetting("spacing.padding.top.unit", null)
+                             ->withSetting("spacing.padding.bottom.value", 0)
+                             ->withSetting("spacing.padding.bottom.unit", null)
+                             ->withSetting("spacing.padding.left.value", 0)
+                             ->withSetting("spacing.padding.left.unit", null)
+                             ->withSetting("spacing.padding.right.value", 0)
+                             ->withSetting("spacing.padding.right.unit", null)
+                             ->withSetting("spacing.customMargin", true)
+                             ->withSetting("spacing.margin.top.value", 3)
+                             ->withSetting("spacing.margin.top.unit", null)
                              ->withSetting("spacing.margin.bottom.value", 0)
                              ->withSetting("spacing.margin.bottom.unit", null);
     }
 
     private function createRegisterLinkWidget()
     {
-        $this->structurWidget->createChild("second", "Ceres::LinkWidget")
-                             ->withSetting("customClass","")
-                             ->withSetting("block", "true")
-                             ->withSetting("text", $this->translator->trans("Ceres::Template.loginRegister"))
-                             ->withSetting("url.type", "external")
-                             ->withSetting("url.value", $this->shopUrls->registration);
+        $registerLinkWidget = $this->structurWidget->createChild("first", "Ceres::LinkWidget")
+                                                   ->withSetting("customClass","")
+                                                   ->withSetting("block", "true")
+                                                   ->withSetting("text", $this->translator->trans("Ceres::Template.loginRegister"));
+
+        if ( in_array(RouteConfig::REGISTER, RouteConfig::getEnabledRoutes())
+            && RouteConfig::getCategoryId(RouteConfig::REGISTER) > 0
+            && !$this->shopUrls->equals($this->shopUrls->registration,'/register') )
+        {
+            $registerLinkWidget->withSetting("url.type", "category")
+                                ->withSetting("url.value", RouteConfig::getCategoryId(RouteConfig::REGISTER));
+        }
+        else
+        {
+            $registerLinkWidget->withSetting("url.type", "external")
+                                ->withSetting("url.value", $this->shopUrls->registration);
+        }
     }
 }
