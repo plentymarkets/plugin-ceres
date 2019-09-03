9663d73db3582025b1dbdfd4bb0a473dacb49963
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2019-09-03 15:33:43 +0200

    wip

--- src/Widgets/Presets/DefaultLoginPreset.php
+++ src/Widgets/Presets/DefaultLoginPreset.php
@@ -5,7 +5,9 @@ namespace Ceres\Widgets\Presets;
 use Ceres\Config\CeresConfig;
 use Ceres\Widgets\Helper\Factories\PresetWidgetFactory;
 use Ceres\Widgets\Helper\PresetHelper;
+use IO\Extensions\Constants\ShopUrls;
 use Plenty\Modules\ShopBuilder\Contracts\ContentPreset;
+use Plenty\Plugin\Translation\Translator;
 
 class DefaultLoginPreset implements ContentPreset
 {
@@ -14,16 +16,26 @@ class DefaultLoginPreset implements ContentPreset
 
     /** @var PresetHelper */
     private $structurWidget;
+
+    /** @var ShopUrls */
+    private $shopUrls;
+
+    /** @var Translator */
+    private $translator;
     
     public function getWidgets()
     {
         $this->preset = pluginApp(PresetHelper::class);
+        $this->translator = pluginApp(Translator::class);
+        
+        $this->shopUrls = pluginApp(ShopUrls::class);
         
         $this->createThreeColumnWidget();
 
         $this->createLoginWidget();
         $this->createSeparatorWidget();
         $this->createGuestLoginWidget();
+        $this->createTextWidget();
         $this->createRegisterLinkWidget();
         
         return $this->preset->toArray();
@@ -37,12 +49,14 @@ class DefaultLoginPreset implements ContentPreset
 
     private function createLoginWidget()
     {
-        $this->structurWidget->createChild("second", "Ceres::LoginWidget");
+        $this->structurWidget->createChild("second", "Ceres::LoginWidget")
+        ->withSetting("customClass","");
     }
 
     private function createSeparatorWidget()
     {
-        $this->structurWidget->createChild("second", "Ceres::SeparatorWidget");
+        $this->structurWidget->createChild("second", "Ceres::SeparatorWidget")
+        ->withSetting("customClass","");
     }
 
     private function createGuestLoginWidget()
@@ -50,8 +64,30 @@ class DefaultLoginPreset implements ContentPreset
         $this->structurWidget->createChild("second", "Ceres::GuestLoginWidget");
     }
 
+    private function createTextWidget()
+    {
+        $this->structurWidget->createChild("second", "Ceres::InlineTextWidget")
+                             ->withSetting("text", "<h1 class=\"h2\">{{ trans(\"Ceres::Template.loginCallToAction\") }}</h1>")
+                             ->withSetting("appearance", "none")
+                             ->withSetting("spacing.customPadding", true)
+                             ->withSetting("spacing.padding.top.value", 0)
+                             ->withSetting("spacing.padding.top.unit", null)
+                             ->withSetting("spacing.padding.bottom.value", 0)
+                             ->withSetting("spacing.padding.bottom.unit", null)
+                             ->withSetting("spacing.padding.left.value", 0)
+                             ->withSetting("spacing.padding.left.unit", null)
+                             ->withSetting("spacing.customMargin", true)
+                             ->withSetting("spacing.margin.bottom.value", 0)
+                             ->withSetting("spacing.margin.bottom.unit", null);
+    }
+
     private function createRegisterLinkWidget()
     {
-        $this->structurWidget->createChild("second", "Ceres::LinkWidget");
+        $this->structurWidget->createChild("second", "Ceres::LinkWidget")
+                             ->withSetting("customClass","")
+                             ->withSetting("block", "true")
+                             ->withSetting("text", $this->translator->trans("Ceres::Template.loginRegister"))
+                             ->withSetting("url.type", "external")
+                             ->withSetting("url.value", $this->shopUrls->registration);
     }
 }
