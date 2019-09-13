c32c199c0bf85ad40b4d79179ecfcf7b6aad7166
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-09-13 14:21:40 +0200

    CHANGE WishListWidget - add new design

--- resources/js/src/app/components/wishList/WishListItem.js
+++ resources/js/src/app/components/wishList/WishListItem.js
@@ -1,10 +1,59 @@
+import { isNullOrUndefined } from "../../helper/utils";
+import { transformVariationProperties } from "../../services/VariationPropertyService";
+
 export default {
-    props: {
+    props:
+    {
         template:
         {
             type: String,
             default: "#vue-wish-list-item"
         },
+        imageAccessor:
+        {
+            type: String,
+            default: "urlMiddle"
+        },
         wishListItem: Object
+    },
+
+    computed:
+    {
+        image()
+        {
+            const itemImages = this.$options.filters.itemImages(this.wishListItem.data.images, this.imageAccessor);
+
+            return this.$options.filters.itemImage(itemImages);
+        },
+
+        unitPrice()
+        {
+            if (!isNullOrUndefined(this.wishListItem.data.prices.specialOffer))
+            {
+                return this.wishListItem.data.prices.specialOffer.unitPrice.value;
+            }
+
+            return this.wishListItem.data.prices.default.unitPrice.value;
+        },
+
+        basePrice()
+        {
+            if (!isNullOrUndefined(this.wishListItem.data.prices.specialOffer))
+            {
+                return this.wishListItem.data.prices.specialOffer.basePrice;
+            }
+
+            return this.wishListItem.data.prices.default.basePrice;
+        },
+
+        itemTotalPrice()
+        {
+            return this.wishListItem.data.price;
+        },
+
+        transformedVariationProperties()
+        {
+            return transformVariationProperties(this.wishListItem.data, [], "displayInOrderProcess");
+        }
     }
 };
