255b65ec62a6523e5f6ad9db4c1288070d0ee7ae
Felix Dausch <felix.dausch@plentymarkets.com>
2019-09-16 11:50:40 +0200

    ADD ChangeMailWidget

---
+++ src/Widgets/Customer/ChangeMailWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Customer;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class ChangeMailWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Customer.ChangeMailWidget";
+}
