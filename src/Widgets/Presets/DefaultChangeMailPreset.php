e62188216b5fdfb69dd8a93a9680d67ae12a321f
Felix Dausch <felix.dausch@plentymarkets.com>
2019-09-16 17:41:18 +0200

    Apply requested changes

--- src/Widgets/Presets/DefaultChangeMailPreset.php
+++ src/Widgets/Presets/DefaultChangeMailPreset.php
@@ -5,7 +5,6 @@ namespace Ceres\Widgets\Presets;
 use Ceres\Widgets\Helper\Factories\PresetWidgetFactory;
 use Ceres\Widgets\Helper\PresetHelper;
 use Plenty\Modules\ShopBuilder\Contracts\ContentPreset;
-use Plenty\Plugin\Translation\Translator;
 
 class DefaultChangeMailPreset implements ContentPreset
 {
