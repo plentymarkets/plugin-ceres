58ad617ba0b1b4150beed3282c1efe7e995e8b21
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2019-09-17 11:05:25 +0200

    minor changes

--- src/Widgets/Presets/Legal/DefaultNewsletterUnsubscribePreset.php
+++ src/Widgets/Presets/Legal/DefaultNewsletterUnsubscribePreset.php
@@ -5,19 +5,26 @@ namespace Ceres\Widgets\Presets;
 use Ceres\Widgets\Helper\Factories\PresetWidgetFactory;
 use Ceres\Widgets\Helper\PresetHelper;
 use Plenty\Modules\ShopBuilder\Contracts\ContentPreset;
-use Plenty\Plugin\Translation\Translator;
 
-class NewsletterOptOutPreset implements ContentPreset
+class DefaultNewsletterUnsubscribePreset implements ContentPreset
 {
     public function getWidgets()
     {
         /** @var PresetHelper */
         $preset = pluginApp(PresetHelper::class);
 
-        /** @var Translator */
-        $translator = pluginApp(Translator::class);
+        /** @var PresetWidgetFactory $row */
+        $row = $preset
+            ->createWidget("Ceres::ThreeColumnWidget")
+            ->withSetting("customClass", "")
+            ->withSetting("layout", "oneToTwoToOne");
 
-        $newsletter = $preset->createWidget("Ceres::NewsletterUnsubscribeWidget")
+        $row->createChild("second", "Ceres::InlineTextWidget")
+            ->withSetting("text", '<h1>{{ trans("Ceres::Template.newsletterOptOutTitle") }}</h1><p>{{ trans("Ceres::Template.newsletterOptOutInfoText") }}</p>')
+            ->withSetting("customClass", "")
+            ->withSetting("appearance", "none");
+
+        $row->createChild("second", "Ceres::NewsletterUnsubscribeWidget")
             ->withSetting("customClass", "")
             ->withSetting("appearance", "primary")
             ->withSetting("spacing.customPadding", true)
