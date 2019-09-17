219960ad07e87a6630f63697792aceb56dac5d47
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2019-09-17 11:19:51 +0200

    widget changes

--- src/Widgets/Common/NewsletterUnsubscribeWidget.php
+++ src/Widgets/Common/NewsletterUnsubscribeWidget.php
@@ -7,30 +7,4 @@ use Ceres\Widgets\Helper\BaseWidget;
 class NewsletterUnsubscribeWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.Common.NewsletterUnsubscribeWidget";
-
-    protected function getTemplateData($widgetSettings, $isPreview)
-    {
-        $hintText = "";
-
-        if ( array_key_exists("hintText", $widgetSettings) && !empty($widgetSettings["hintText"]["mobile"]) )
-        {
-            $hintText = $widgetSettings["hintText"]["mobile"];
-        }
-        else
-        {
-            if ( array_key_exists("title", $widgetSettings) && !empty($widgetSettings["title"]["mobile"]) )
-            {
-                $hintText = "<h3>" . $widgetSettings["title"]["mobile"] . "</h3>";
-            }
-
-            if ( array_key_exists("subTitle", $widgetSettings) && !empty($widgetSettings["subTitle"]["mobile"]) )
-            {
-                $hintText .= "<p>" . $widgetSettings["subTitle"]["mobile"] . "</p>";
-            }
-        }
-
-        return [
-            "hintText" => $hintText
-        ];
-    }
 }
