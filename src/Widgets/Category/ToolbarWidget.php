a0f396044fd278b8599b4dbbfab324f54e67a797
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2019-10-18 09:28:31 +0200

    initial commit

---
+++ src/Widgets/Category/ToolbarWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Category;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class ToolbarWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Category.ToolbarWidget";
+}
