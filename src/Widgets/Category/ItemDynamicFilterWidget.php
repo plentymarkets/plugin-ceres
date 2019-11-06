e0e185cfb495a576d53c0462716fe128cca4ceb9
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2019-11-06 16:48:44 +0100

    add preview data for widget

--- src/Widgets/Category/ItemDynamicFilterWidget.php
+++ src/Widgets/Category/ItemDynamicFilterWidget.php
@@ -3,8 +3,18 @@
 namespace Ceres\Widgets\Category;
 
 use Ceres\Widgets\Helper\BaseWidget;
+use IO\Services\ItemSearch\Factories\Faker\FacetFaker;
 
 class ItemDynamicFilterWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.Category.ItemDynamicFilterWidget";
+
+    protected function getPreviewData($widgetSettings)
+    {
+        /** @var FacetFaker $facetFaker */
+        $facetFaker = pluginApp(FacetFaker::class);
+        $facetResult = $facetFaker->fill([]);
+
+        return ['facets' => $facetResult];
+    }
 }
