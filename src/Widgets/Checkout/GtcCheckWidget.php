d2979c8c8dd3668fea7caafffc20695649289e15
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-11-15 19:38:31 +0100

    transform contactwish and gtccheck widgets

--- src/Widgets/Checkout/GtcCheckWidget.php
+++ src/Widgets/Checkout/GtcCheckWidget.php
@@ -3,8 +3,57 @@
 namespace Ceres\Widgets\Checkout;
 
 use Ceres\Widgets\Helper\BaseWidget;
+use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
+use Ceres\Widgets\Helper\WidgetCategories;
+use Ceres\Widgets\Helper\WidgetDataFactory;
+use Ceres\Widgets\Helper\WidgetTypes;
 
 class GtcCheckWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.Checkout.GtcCheckWidget";
+
+    public function getData()
+    {
+        return WidgetDataFactory::make("Ceres::GtcCheckWidget")
+            ->withLabel("Widget.gtcCheckLabel")
+            ->withPreviewImageUrl("/images/widgets/gtc-check.svg")
+            ->withType(WidgetTypes::CHECKOUT)
+            ->withCategory(WidgetCategories::CHECKOUT)
+            ->withPosition(400)
+            ->toArray();
+    }
+
+    public function getSettings()
+    {
+        /** @var WidgetSettingsFactory $settings */
+        $settings = pluginApp(WidgetSettingsFactory::class);
+
+        $settings->createCustomClass();
+        $settings->createAppearance();
+
+        $settings->createCheckbox("hideCheckbox")
+            ->withDefaultValue(false)
+            ->withName("Widget.gtcCheckHideCheckboxLabel");
+        // missing tooltip
+
+        $settings->createCheckbox("isPreselected")
+            ->withCondition("hideCheckbox !== true")
+            ->withDefaultValue(false)
+            ->withName("Widget.gtcCheckIsPreselectedLabel");
+        // missing tooltip
+
+        $settings->createCheckbox("isRequired")
+            ->withCondition("hideCheckbox !== true")
+            ->withDefaultValue(true)
+            ->withName("Widget.gtcCheckIsRequiredLabel");
+        // missing tooltip
+
+        $settings->createCodeEditor()
+            ->withDefaultValue("")
+            ->withName("Widget.gtcCheckCustomTextLabel");
+        // missing tooltip
+
+        $settings->createSpacing(false, true);
+
+    }
 }
