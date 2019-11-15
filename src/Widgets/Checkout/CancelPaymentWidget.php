31dfae5d1db06e98a72b99c7b0b851bcbd7a309e
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-11-15 18:39:36 +0100

    Transform CancelPaymentWidget

--- src/Widgets/Checkout/CancelPaymentWidget.php
+++ src/Widgets/Checkout/CancelPaymentWidget.php
@@ -3,8 +3,50 @@
 namespace Ceres\Widgets\Checkout;
 
 use Ceres\Widgets\Helper\BaseWidget;
+use Ceres\Widgets\Helper\Factories\Settings\ValueListFactory;
+use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
+use Ceres\Widgets\Helper\WidgetCategories;
+use Ceres\Widgets\Helper\WidgetDataFactory;
+use Ceres\Widgets\Helper\WidgetTypes;
 
 class CancelPaymentWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.Checkout.CancelPaymentWidget";
+
+    public function getData()
+    {
+        return WidgetDataFactory::make("Ceres::CancelPaymentWidget")
+            ->withLabel("Widget.cancelPaymentLabel")
+            ->withPreviewImageUrl("/images/widgets/cancel-payment.svg")
+            ->withType(WidgetTypes::CHECKOUT)
+            ->withCategory(WidgetCategories::BASKET)
+            ->withPosition(800)
+            ->toArray();
+    }
+
+    public function getSettings()
+    {
+        /** @var WidgetSettingsFactory $settings */
+        $settings = pluginApp(WidgetSettingsFactory::class);
+
+        $settings->createCustomClass();
+        $settings->createAppearance();
+
+        $select = $settings->createSelect("buttonSize")
+            ->withDefaultValue("")
+            ->withName("Widget.widgetButtonSizeLabel");
+            // TODO: tooltipText cant be added yet.
+
+        /** @var ValueListFactory $listBoxValues */
+        $listBoxValues = pluginApp(ValueListFactory::class);
+        $listBoxValues->addEntry("btn-sm", "Widget.widgetButtonSizeSm")
+            ->addEntry("", "Widget.widgetButtonSizeNormal")
+            ->addEntry("btn-lg", "Widget.widgetButtonSizeLg");
+
+        $select->withListBoxValues($listBoxValues->toArray());
+
+        $settings->createSpacing();
+
+        return $settings->toArray();
+    }
 }
