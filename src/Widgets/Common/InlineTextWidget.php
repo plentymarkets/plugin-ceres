4321a1d4d751dfa79007db39fc08894203154069
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-11-18 14:24:02 +0100

    Transform inlinetext widget

--- src/Widgets/Common/InlineTextWidget.php
+++ src/Widgets/Common/InlineTextWidget.php
@@ -3,8 +3,37 @@
 namespace Ceres\Widgets\Common;
 
 use Ceres\Widgets\Helper\BaseWidget;
+use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
+use Ceres\Widgets\Helper\WidgetCategories;
+use Ceres\Widgets\Helper\WidgetDataFactory;
+use Ceres\Widgets\Helper\WidgetTypes;
 
 class InlineTextWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.Common.InlineTextWidget";
+
+    public function getData()
+    {
+        return WidgetDataFactory::make("Ceres::InlineTextWidget")
+            ->withLabel("Widget.textLabel")
+            ->withPreviewImageUrl("/images/widgets/text-inline.svg")
+            ->withType(WidgetTypes::STATIC)
+            ->withCategory(WidgetCategories::TEXT)
+            ->withPosition(800)
+            ->toArray();
+    }
+
+    public function getSettings()
+    {
+        /** @var WidgetSettingsFactory $settings */
+        $settings = pluginApp(WidgetSettingsFactory::class);
+
+        $settings->createCustomClass();
+        $settings->createAppearance(true)
+            ->withDefaultValue("none");
+
+        $settings->createSpacing();
+
+        $settings->toArray();
+    }
 }
