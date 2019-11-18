6a477892d477b4082c38bb171f8233182006125c
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-11-18 16:33:19 +0100

    Refactor all other widgets of common type, sans button size include

--- src/Widgets/Common/NewsletterUnsubscribeWidget.php
+++ src/Widgets/Common/NewsletterUnsubscribeWidget.php
@@ -3,8 +3,37 @@
 namespace Ceres\Widgets\Common;
 
 use Ceres\Widgets\Helper\BaseWidget;
+use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
+use Ceres\Widgets\Helper\WidgetCategories;
+use Ceres\Widgets\Helper\WidgetDataFactory;
+use Ceres\Widgets\Helper\WidgetTypes;
 
 class NewsletterUnsubscribeWidget extends BaseWidget
 {
     protected $template = "Ceres::Widgets.Common.NewsletterUnsubscribeWidget";
+
+    public function getData()
+    {
+        return WidgetDataFactory::make("Ceres::NewsletterUnsubscribeWidget")
+            ->withLabel("Widget.newsletterUnsubscribeLabel")
+            ->withPreviewImageUrl("/images/widgets/newsletter-unsubscribe.svg")
+            ->withType(WidgetTypes::STATIC)
+            ->withPosition(600)
+            ->toArray();
+    }
+
+    public function getSettings()
+    {
+        /** @var WidgetSettingsFactory $settings */
+        $settings = pluginApp(WidgetSettingsFactory::class);
+
+        $settings->createCustomClass();
+        $settings->createAppearance();
+
+        // BUTTON SIZE
+
+        $settings->createSpacing(false, true);
+
+        return $settings->toArray();
+    }
 }
