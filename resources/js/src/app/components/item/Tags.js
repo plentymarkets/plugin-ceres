951b0c4135a589e7e265dfc50f9cd256b7766b6c
Maximilian Röll <maximilian.roell@plentymarkets.com>
2019-11-20 13:09:14 +0100

    fix tslint error build build files

--- resources/js/src/app/components/item/Tags.js
+++ resources/js/src/app/components/item/Tags.js
@@ -40,6 +40,7 @@ Vue.component("tags", {
                 const green = parseInt(color.substring(2, 4), 16);
                 const blue = parseInt(color.substring(4, 6), 16);
                 const result = (red * 0.299) + (green * 0.587) + (blue * 0.114);
+
                 return (result > 186) ? "text-context-dark" : "text-context-light";
             },
             getStyles(tag)
