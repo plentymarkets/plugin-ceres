e47508e798c3bd3c3640b46c799f64de73f05482
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-11-21 18:27:37 +0100

    RENAME CategoryNavigationWidget to SideNavigationWidget

---
+++ src/Widgets/Category/SideNavigationWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Category;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class SideNavigationWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Category.SideNavigationWidget";
+}
