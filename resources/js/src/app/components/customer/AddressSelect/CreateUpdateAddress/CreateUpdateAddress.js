2de619faa83124cfa51decdadec8bb0249b631d7
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2019-11-22 16:29:50 +0100

    Update CreateUpdateAddress.js

--- resources/js/src/app/components/customer/AddressSelect/CreateUpdateAddress/CreateUpdateAddress.js
+++ resources/js/src/app/components/customer/AddressSelect/CreateUpdateAddress/CreateUpdateAddress.js
@@ -1,4 +1,4 @@
-import {isNullOrUndefined} from "../../../../helper/utils";
+import { isNullOrUndefined } from "../../../../helper/utils";
 
 const NotificationService = require("../../../../services/NotificationService");
 
