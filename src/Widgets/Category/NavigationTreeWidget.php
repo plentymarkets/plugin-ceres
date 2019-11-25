e58c2ec14b5ccf810ab7bc1745fe71c45fe0b146
cjohannsen <christoph.johannsen@plentymarkets.com>
2019-11-25 13:59:44 +0100

    RENAME 'CategoryNavigationWidget' to 'NavigationTreeWidget'; move widget into category 'navigation'

---
+++ src/Widgets/Category/NavigationTreeWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\Category;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class NavigationTreeWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Category.NavigationTreeWidget";
+}
