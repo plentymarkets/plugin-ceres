fc4c7e0acb3867049da2bed67a03406ff089d3d1
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-11-27 15:28:27 +0100

    Remove curl call from php part of widget

--- src/Widgets/Common/GoogleMapsWidget.php
+++ src/Widgets/Common/GoogleMapsWidget.php
@@ -21,41 +21,13 @@ class GoogleMapsWidget extends BaseWidget
             ];
         }
 
-        $address = urlencode($address);
-
-        // google map geocode api url
-        $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key={$apiKey}";
-
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $url
-        ));
-
-        $result_json = curl_exec($curl);
-        $result = json_decode($result_json, true);
-
-        curl_close($curl);
-
-        $lat = isset($result["results"][0]["geometry"]["location"]["lat"]) ? $result["results"][0]["geometry"]["location"]["lat"] : "";
-        $lng = isset($result["results"][0]["geometry"]["location"]["lng"]) ? $result["results"][0]["geometry"]["location"]["lng"] : "";
-        $formatted_address = isset($result["results"][0]["formatted_address"]) ? $result["results"][0]["formatted_address"] : "";
-
-        if ($lat && $lng && $formatted_address)
-        {
-            return [
-                "geocoding_data" => [
-                    "lat" => $lat,
-                    "lng" => $lng,
-                    "address" => $formatted_address,
-                    "apiKey" => $apiKey
-                ]
-            ];
-        }
-
         return [
-            "geocoding_data" => false
+            "geocoding_data" => [
+                "address" => $address,
+                "apiKey" => $apiKey
+            ]
         ];
+
+
     }
 }
