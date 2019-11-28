c067905a3ff75288f5ba376bd00f87a14de51f04
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2019-11-28 11:27:52 +0100

    fix string quotes

--- src/Widgets/Common/GoogleMapsWidget.php
+++ src/Widgets/Common/GoogleMapsWidget.php
@@ -6,25 +6,25 @@ use Ceres\Widgets\Helper\BaseWidget;
 
 class GoogleMapsWidget extends BaseWidget
 {
-    protected $template = "Ceres::Widgets.Common.GoogleMapsWidget";
+    protected $template = 'Ceres::Widgets.Common.GoogleMapsWidget';
 
     protected function getTemplateData($widgetSettings, $isPreview)
     {
-        $address = $widgetSettings["address"]["mobile"];
+        $address = $widgetSettings['address']['mobile'];
 
-        $apiKey = $widgetSettings["apiKey"]["mobile"];
+        $apiKey = $widgetSettings['apiKey']['mobile'];
 
         if (empty($address) || empty($apiKey))
         {
             return [
-                "geocoding_data" => false
+                'geocoding_data' => false
             ];
         }
 
         return [
-            "geocoding_data" => [
-                "address" => $address,
-                "apiKey" => $apiKey
+            'geocoding_data' => [
+                'address' => $address,
+                'apiKey' => $apiKey
             ]
         ];
 
