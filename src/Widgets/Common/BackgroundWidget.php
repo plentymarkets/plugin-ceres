2920f6fadd4c52dee31381a323431ad16f801ee9
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2019-12-10 17:42:09 +0100

    set styling classes via php

--- src/Widgets/Common/BackgroundWidget.php
+++ src/Widgets/Common/BackgroundWidget.php
@@ -40,7 +40,18 @@ class BackgroundWidget extends BaseWidget
         $settings->createCheckbox("fullWidth")
             ->withDefaultValue(true)
             ->withName("Widget.backgroundFullWidthLabel")
-            ->withName("Widget.backgroundFullWidthTooltip");
+            ->withTooltip("Widget.backgroundFullWidthTooltip");
+            
+        $settings->createCheckbox("backgroundFixed")
+            ->withDefaultValue(false)
+            ->withName("Widget.backgroundFixedLabel")
+            ->withTooltip("Widget.backgroundFixedTooltip");
+
+        $settings->createCheckbox("backgroundRepeat")
+            ->withCondition("backgroundSize !== 'bg-cover'")
+            ->withDefaultValue(false)
+            ->withName("Widget.backgroundRepeatLabel")
+            ->withTooltip("Widget.backgroundRepeatTooltip");
 
         $settings->createSelect("backgroundSize")
             ->withDefaultValue("cover")
@@ -48,27 +59,40 @@ class BackgroundWidget extends BaseWidget
             ->withTooltip("Widget.backgroundSizeTooltip")
             ->withListBoxValues(
                 ValueListFactory::make()
-                    ->addEntry("cover", "Widget.backgroundSizeCover")
-                    ->addEntry("contain", "Widget.backgroundSizeContain")
-                    ->addEntry("auto", "Widget.backgroundSizeAuto")
+                    ->addEntry("bg-cover", "Widget.backgroundSizeCover")
+                    ->addEntry("bg-contain", "Widget.backgroundSizeContain")
+                    ->addEntry("bg-auto", "Widget.backgroundSizeAuto")
                     ->toArray()
         );
-
-        $settings->createCheckbox("backgroundFixed")
-            ->withDefaultValue(false)
-            ->withName("Widget.backgroundFixedLabel")
-            ->withTooltip("Widget.backgroundFixedTooltip");
-
-        $settings->createCheckbox("backgroundRepeat")
-            ->withCondition("backgroundSize !== 'cover'")
-            ->withDefaultValue(false)
-            ->withName("Widget.backgroundRepeatLabel")
-            ->withTooltip("Widget.backgroundRepeatTooltip");
 
         $settings->createHeight();
 
         $settings->createSpacing(true, true);
 
         return $settings->toArray();
+    }
+
+    protected function getTemplateData($widgetSettings, $isPreview)
+    {
+        $stylingClasses = "";
+
+        if ( array_key_exists("backgroundFixed", $widgetSettings) && $widgetSettings["backgroundFixed"]["mobile"] == false )
+        {
+            $stylingClasses .= "bg-scroll ";
+        }
+
+        if ( array_key_exists("backgroundRepeat", $widgetSettings) && $widgetSettings["backgroundRepeat"]["mobile"] == true )
+        {
+            $stylingClasses .= "bg-repeat ";
+        }
+
+        if ( array_key_exists("backgroundSize", $widgetSettings) && $widgetSettings["backgroundSize"]["mobile"] )
+        {
+            $stylingClasses .= $widgetSettings["backgroundSize"]["mobile"] . " ";
+        }
+
+        return [
+            'stylingClasses'  => $stylingClasses
+        ];
     }
 }
