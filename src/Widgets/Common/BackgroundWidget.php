30952b0d79042e5e1af866a1709b537f6599d303
Felix Dausch <felix.dausch@plentymarkets.com>
2019-12-12 18:12:06 +0100

    Replace double quotes

--- src/Widgets/Common/BackgroundWidget.php
+++ src/Widgets/Common/BackgroundWidget.php
@@ -11,13 +11,13 @@ use Ceres\Widgets\Helper\WidgetTypes;
 
 class BackgroundWidget extends BaseWidget
 {
-    protected $template = "Ceres::Widgets.Common.BackgroundWidget";
+    protected $template = 'Ceres::Widgets.Common.BackgroundWidget';
 
     public function getData()
     {
-        return WidgetDataFactory::make("Ceres::BackgroundWidget")
-            ->withLabel("Widget.backgroundLabel")
-            ->withPreviewImageUrl("/images/widgets/background.svg")
+        return WidgetDataFactory::make('Ceres::BackgroundWidget')
+            ->withLabel('Widget.backgroundLabel')
+            ->withPreviewImageUrl('/images/widgets/background.svg')
             ->withType(WidgetTypes::STATIC)
             ->withCategory(WidgetCategories::IMAGE)
             ->withPosition(700)
@@ -31,24 +31,24 @@ class BackgroundWidget extends BaseWidget
 
         $settings->createCustomClass();
 
-        $settings->createCheckbox("fullWidth")
+        $settings->createCheckbox('fullWidth')
             ->withDefaultValue(true)
-            ->withName("Widget.backgroundFullWidthLabel")
-            ->withTooltip("Widget.backgroundFullWidthTooltip");
+            ->withName('Widget.backgroundFullWidthLabel')
+            ->withTooltip('Widget.backgroundFullWidthTooltip');
 
         $settings->createColorPalette();
 
-        $settings->createColor("customColor")
-            ->withCondition("colorPalette === 'custom'")
-            ->withDefaultValue("#000000");
+        $settings->createColor('customColor')
+            ->withCondition('colorPalette === "custom"')
+            ->withDefaultValue('#000000');
 
         $this->createImageSettings($settings);
 
-        $settings->createSlider("opacity")
+        $settings->createSlider('opacity')
             ->withDefaultValue(100)
-            ->withName("Widget.backgroundOpacityLabel")
-            ->withOption("inputInterval", 1)
-            ->withOption("inputMax", 100);
+            ->withName('Widget.backgroundOpacityLabel')
+            ->withOption('inputInterval', 1)
+            ->withOption('inputMax', 100);
 
 
         $settings->createHeight();
@@ -63,65 +63,65 @@ class BackgroundWidget extends BaseWidget
      */
     private function createImageSettings($settings)
     {
-        $settings->createSelect("sourceType")
-            ->withDefaultValue("none")
-            ->withName("Widget.backgroundSourceTypeLabel")
-            ->withTooltip("Widget.backgroundSourceTypeTooltip")
+        $settings->createSelect('sourceType')
+            ->withDefaultValue('none')
+            ->withName('Widget.backgroundSourceTypeLabel')
+            ->withTooltip('Widget.backgroundSourceTypeTooltip')
             ->withListBoxValues(
                 ValueListFactory::make()
-                    ->addEntry("none", "Widget.backgroundSourceTypeNone")
-                    ->addEntry("custom-image", "Widget.backgroundSourceTypeCustomImage")
-                    ->addEntry("category-image1", "Widget.backgroundSourceTypeCategoryImage1")
-                    ->addEntry("category-image2", "Widget.backgroundSourceTypeCategoryImage2")
+                    ->addEntry('none', 'Widget.backgroundSourceTypeNone')
+                    ->addEntry('custom-image', 'Widget.backgroundSourceTypeCustomImage')
+                    ->addEntry('category-image1', 'Widget.backgroundSourceTypeCategoryImage1')
+                    ->addEntry('category-image2', 'Widget.backgroundSourceTypeCategoryImage2')
                     ->toArray()
             );
 
-        $settings->createFile("customImagePath")
-            ->withCondition("sourceType === 'custom-image'")
-            ->withName("Widget.backgroundImageSource");
+        $settings->createFile('customImagePath')
+            ->withCondition('sourceType === "custom-image"')
+            ->withName('Widget.backgroundImageSource');
 
-        $settings->createSelect("backgroundSize")
-            ->withCondition("sourceType !== 'none'")
-            ->withDefaultValue("bg-cover")
-            ->withName("Widget.backgroundSizeLabel")
-            ->withTooltip("Widget.backgroundSizeTooltip")
+        $settings->createSelect('backgroundSize')
+            ->withCondition('sourceType !== "none"')
+            ->withDefaultValue('bg-cover')
+            ->withName('Widget.backgroundSizeLabel')
+            ->withTooltip('Widget.backgroundSizeTooltip')
             ->withListBoxValues(
                 ValueListFactory::make()
-                    ->addEntry("bg-cover", "Widget.backgroundSizeCover")
-                    ->addEntry("bg-contain", "Widget.backgroundSizeContain")
-                    ->addEntry("bg-auto", "Widget.backgroundSizeAuto")
+                    ->addEntry('bg-cover', 'Widget.backgroundSizeCover')
+                    ->addEntry('bg-contain', 'Widget.backgroundSizeContain')
+                    ->addEntry('bg-auto', 'Widget.backgroundSizeAuto')
                     ->toArray()
             );
 
-        $settings->createCheckbox("backgroundFixed")
-            ->withCondition("sourceType !== 'none'")
+        $settings->createCheckbox('backgroundFixed')
+            ->withCondition('sourceType !== "none"')
             ->withDefaultValue(false)
-            ->withName("Widget.backgroundFixedLabel")
-            ->withTooltip("Widget.backgroundFixedTooltip");
+            ->withName('Widget.backgroundFixedLabel')
+            ->withTooltip('Widget.backgroundFixedTooltip');
 
-        $settings->createCheckbox("backgroundRepeat")
-            ->withCondition("sourceType !== 'none' && backgroundSize !== 'bg-cover'")
+        $settings->createCheckbox('backgroundRepeat')
+            ->withCondition('sourceType !== "none" && backgroundSize !== "bg-cover"')
             ->withDefaultValue(false)
-            ->withName("Widget.backgroundRepeatLabel")
-            ->withTooltip("Widget.backgroundRepeatTooltip");
+            ->withName('Widget.backgroundRepeatLabel')
+            ->withTooltip('Widget.backgroundRepeatTooltip');
     }
 
     protected function getTemplateData($widgetSettings, $isPreview)
     {
-        $stylingClasses = "";
+        $stylingClasses = '';
 
-        if (array_key_exists("backgroundFixed",
-                $widgetSettings) && $widgetSettings["backgroundFixed"]["mobile"] == false) {
-            $stylingClasses .= "bg-scroll ";
+        if (array_key_exists('backgroundFixed',
+                $widgetSettings) && $widgetSettings['backgroundFixed']['mobile'] == false) {
+            $stylingClasses .= 'bg-scroll ';
         }
 
-        if (array_key_exists("backgroundRepeat",
-                $widgetSettings) && $widgetSettings["backgroundRepeat"]["mobile"] == true && $widgetSettings["backgroundSize"]["mobile"] !== 'bg-cover') {
-            $stylingClasses .= "bg-repeat ";
+        if (array_key_exists('backgroundRepeat',
+                $widgetSettings) && $widgetSettings['backgroundRepeat']['mobile'] == true && $widgetSettings['backgroundSize']['mobile'] !== 'bg-cover') {
+            $stylingClasses .= 'bg-repeat ';
         }
 
-        if (array_key_exists("backgroundSize", $widgetSettings) && $widgetSettings["backgroundSize"]["mobile"]) {
-            $stylingClasses .= $widgetSettings["backgroundSize"]["mobile"];
+        if (array_key_exists('backgroundSize', $widgetSettings) && $widgetSettings['backgroundSize']['mobile']) {
+            $stylingClasses .= $widgetSettings['backgroundSize']['mobile'];
         }
 
         return [
