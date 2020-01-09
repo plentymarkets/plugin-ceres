f58893b426780d486826026fdc38a5711167f258
Felix Dausch <felix.dausch@plentymarkets.com>
2020-01-09 17:52:24 +0100

    ADD stylellint

--- tools/webpack/styles.config.js
+++ tools/webpack/styles.config.js
@@ -1,5 +1,6 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const StylelintPlugin = require("stylelint-webpack-plugin");
 const path = require("path");
 
 module.exports = env =>
@@ -48,6 +49,7 @@ module.exports = env =>
         },
         plugins: [
             new FixStyleOnlyEntriesPlugin(),
+            // new StylelintPlugin(),
             new MiniCssExtractPlugin({
                 filename: "../../css/ceres-[name]" + (env.prod ? ".min" : "") + ".css",
             })
