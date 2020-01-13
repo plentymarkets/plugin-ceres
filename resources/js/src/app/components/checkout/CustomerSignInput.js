af6d680644ced8886dc17822afffd0dccbd2d7d0
cjohannsen <christoph.johannsen@plentymarkets.com>
2020-01-13 11:49:36 +0100

    CHANGE CustomerSignInput - add maxlength to the input

--- resources/js/src/app/components/checkout/CustomerSignInput.js
+++ resources/js/src/app/components/checkout/CustomerSignInput.js
@@ -6,11 +6,20 @@ Vue.component("customer-sign-input", {
 
     template: `
     <div class="input-unit">
-        <input type="text" @change="setCustomerSign($event.srcElement.value)" :id="'customer-sign_' + _uid">
+        <input type="text" @change="setCustomerSign($event.srcElement.value)" :id="'customer-sign_' + _uid" :maxlength="maxLength">
         <label :for="'customer-sign_' + _uid">
             ${ TranslationService.translate("Ceres::Template.checkoutCustomerSign") }
         </label>
     </div>`,
+
+    props:
+    {
+        maxLength:
+        {
+            type: Number,
+            default: 128
+        }
+    },
 
     methods: {
         ...mapMutations([
