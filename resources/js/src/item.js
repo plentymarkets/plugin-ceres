4f58a3f577666d19a3741ac860127b231838e7c1
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2020-01-15 15:37:20 +0100

    UPDATE make GraduatedPrices a SFC

--- resources/js/src/item.js
+++ resources/js/src/item.js
@@ -3,7 +3,8 @@ import "./category";
 
 import AddToWishList from "./app/components/item/AddToWishList.vue";
 Vue.component("add-to-wish-list", AddToWishList);
-import "./app/components/item/GraduatedPrices";
+import GraduatedPrices from "./app/components/item/GraduatedPrices.vue";
+Vue.component("graduated-prices", GraduatedPrices);
 import "./app/components/item/ItemDataTable";
 import "./app/components/item/ItemImageCarousel";
 import "./app/components/item/OrderProperties";
