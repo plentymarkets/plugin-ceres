f4a967beef07e3863f4619e42c9aa6d8097aa3e4
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2020-01-24 16:28:32 +0100

    set correct static path for ceres demo

--- resources/js/dist/ceres-item.js
+++ resources/js/dist/ceres-item.js
@@ -182,7 +182,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://s3-eu-central-1.amazonaws.com/plentymarkets-public-92/jpx0tvae1136/plugin/26/ceres/FOO/BAR";
+/******/ 	__webpack_require__.p = "https://s3-eu-central-1.amazonaws.com/plentymarkets-public-92/jpx0tvae1136/plugin/26/ceres/js/dist/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
