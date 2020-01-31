a32c52bd44d59720ae5c4921f2a9c3f445603792
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-01-31 19:09:14 +0100

    UPDATE item.js dynamic imports

--- resources/js/src/item.js
+++ resources/js/src/item.js
@@ -1,29 +1,12 @@
 // item script extends category script
 import "./category";
 
-Vue.component("add-to-wish-list", function(resolve)
-{
-    require(["./app/components/item/AddToWishList.vue"], resolve);
-});
-Vue.component("graduated-prices", function(resolve)
-{
-    require(["./app/components/item/GraduatedPrices.vue"], resolve);
-});
-Vue.component("item-data-table", function(resolve)
-{
-    require(["./app/components/item/ItemDataTable.vue"], resolve);
-});
-Vue.component("item-image-carousel", function(resolve)
-{
-    require(["./app/components/item/ItemImageCarousel.vue"], resolve);
-});
-Vue.component("variation-select", function(resolve)
-{
-    require(["./app/components/item/VariationSelect.vue"], resolve);
-});
+Vue.component("add-to-wish-list", () => import("./app/components/item/AddToWishList.vue"));
+Vue.component("graduated-prices", () => import("./app/components/item/GraduatedPrices.vue"));
+Vue.component("item-data-table", () => import("./app/components/item/ItemDataTable.vue"));
+Vue.component("item-image-carousel", () => import("./app/components/item/ItemImageCarousel.vue"));
+Vue.component("variation-select", () => import("./app/components/item/VariationSelect.vue"));
+Vue.component("order-property-list", () => import("./app/components/item/OrderPropertyList.vue"));
+
 import SingleItem from "./app/components/item/SingleItem.vue";
 Vue.component("single-item", SingleItem);
-Vue.component("order-property-list", function(resolve)
-{
-    require(["./app/components/item/OrderPropertyList.vue"], resolve);
-});
