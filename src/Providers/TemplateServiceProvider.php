52d42cd8ca3d757326ba7b41a1cfbe763dc180d4
Steve Tentrop <steve.tentrop@plentymarkets.com>
2020-02-06 17:24:00 +0100

    set templates without event

--- src/Providers/TemplateServiceProvider.php
+++ src/Providers/TemplateServiceProvider.php
@@ -126,26 +126,23 @@ class TemplateServiceProvider extends ServiceProvider
             }
         );
 
+        $templateContainer = pluginApp(ResultFieldTemplate::class);
+
+        $templateContainer->setTemplates(
+            [
+                ResultFieldTemplate::TEMPLATE_LIST_ITEM => 'Ceres::ResultFields.ListItem',
+                ResultFieldTemplate::TEMPLATE_SINGLE_ITEM => 'Ceres::ResultFields.SingleItem',
+                ResultFieldTemplate::TEMPLATE_BASKET_ITEM => 'Ceres::ResultFields.BasketItem',
+                ResultFieldTemplate::TEMPLATE_AUTOCOMPLETE_ITEM_LIST => 'Ceres::ResultFields.AutoCompleteListItem',
+                ResultFieldTemplate::TEMPLATE_CATEGORY_TREE => 'Ceres::ResultFields.CategoryTree',
+                ResultFieldTemplate::TEMPLATE_VARIATION_ATTRIBUTE_MAP => 'Ceres::ResultFields.VariationAttributeMap'
+            ]
+        );
+
         $this->listenToIO(
             'ctx.*',
             function (TemplateContainer $templateContainer, $templateData = []) {
                 $this->setTemplateAndContext($templateContainer);
-            }
-        );
-
-        $this->listenToIO(
-            'ResultFields.*',
-            function (ResultFieldTemplate $templateContainer) {
-                $templateContainer->setTemplates(
-                    [
-                        ResultFieldTemplate::TEMPLATE_LIST_ITEM => 'Ceres::ResultFields.ListItem',
-                        ResultFieldTemplate::TEMPLATE_SINGLE_ITEM => 'Ceres::ResultFields.SingleItem',
-                        ResultFieldTemplate::TEMPLATE_BASKET_ITEM => 'Ceres::ResultFields.BasketItem',
-                        ResultFieldTemplate::TEMPLATE_AUTOCOMPLETE_ITEM_LIST => 'Ceres::ResultFields.AutoCompleteListItem',
-                        ResultFieldTemplate::TEMPLATE_CATEGORY_TREE => 'Ceres::ResultFields.CategoryTree',
-                        ResultFieldTemplate::TEMPLATE_VARIATION_ATTRIBUTE_MAP => 'Ceres::ResultFields.VariationAttributeMap'
-                    ]
-                );
             }
         );
 
@@ -328,32 +325,44 @@ class TemplateServiceProvider extends ServiceProvider
         $templateConfigRepo = pluginApp(TemplateConfigRepositoryContract::class);
 
         $templateConfigRepo
-            ->registerConfigValue('global.google_recaptcha_threshold',           $ceresConfig->global->googleRecaptchaThreshold)
-            ->registerConfigValue('sorting.prioritySearch1',                     $ceresConfig->sorting->prioritySearch1)
-            ->registerConfigValue('sorting.prioritySearch2',                     $ceresConfig->sorting->prioritySearch2)
-            ->registerConfigValue('sorting.prioritySearch3',                     $ceresConfig->sorting->priorityCategory3)
-            ->registerConfigValue('sorting.priorityCategory1',                   $ceresConfig->sorting->priorityCategory1)
-            ->registerConfigValue('sorting.priorityCategory2',                   $ceresConfig->sorting->priorityCategory2)
-            ->registerConfigValue('sorting.priorityCategory3',                   $ceresConfig->sorting->priorityCategory3)
-            ->registerConfigValue('item.name',                                   $ceresConfig->item->itemName)
-            ->registerConfigValue('my_account.confirmation_link_expiration',     $ceresConfig->myAccount->confirmationLinkExpiration)
-            ->registerConfigValue('item.show_please_select',                     $ceresConfig->item->showPleaseSelect)
-            ->registerConfigValue('item.show_category_filter',                   $ceresConfig->item->showCategoryFilter)
-            ->registerConfigValue('global.enableOldUrlPattern',                  $ceresConfig->global->enableOldUrlPattern)
-            ->registerConfigValue('item.variation_show_type',                    $ceresConfig->item->variationShowType)
-            ->registerConfigValue('global.google_recaptcha_secret',              $ceresConfig->global->googleRecaptchaSecret)
-            ->registerConfigValue('currency.available_currencies',               $ceresConfig->currency->availableCurrencies)
-            ->registerConfigValue('my_account.order_return_days',                $ceresConfig->myAccount->orderReturnDays)
-            ->registerConfigValue('checkout.show_all_shipping_profiles',         $ceresConfig->checkout->showAllShippingProfiles)
-            ->registerConfigValue('contact.shop_mail',                           $ceresConfig->contact->shopMail)
-            ->registerConfigValue('global.default_contact_class_b2b',            $ceresConfig->global->defaultContactClassB2B)
-            ->registerConfigValue('item.lists.cross_selling_type',               $ceresConfig->itemLists->crossSellingType)
-            ->registerConfigValue('item.lists.cross_selling_sorting',            $ceresConfig->itemLists->crossSellingSorting)
-            ->registerConfigValue('my_account.confirmation_link_login_redirect', $ceresConfig->myAccount->confirmationLinkLoginRedirect)
-            ->registerConfigValue('my_account.change_payment',                   $ceresConfig->myAccount->changePayment)
-            ->registerConfigValue('my_account.order_return_initial_status',      $ceresConfig->myAccount->orderReturnInitialStatus)
-            ->registerConfigValue('global.user_data_hash_max_age',               $ceresConfig->global->userDataHashMaxAge)
-            ->registerConfigValue('language.active_languages',                   $ceresConfig->language->activeLanguages)
-            ->registerConfigValue('basket.split_bundles',                        $ceresConfig->basket->splitBundles);
+            ->registerConfigValue('global.google_recaptcha_threshold', $ceresConfig->global->googleRecaptchaThreshold)
+            ->registerConfigValue('sorting.prioritySearch1', $ceresConfig->sorting->prioritySearch1)
+            ->registerConfigValue('sorting.prioritySearch2', $ceresConfig->sorting->prioritySearch2)
+            ->registerConfigValue('sorting.prioritySearch3', $ceresConfig->sorting->priorityCategory3)
+            ->registerConfigValue('sorting.priorityCategory1', $ceresConfig->sorting->priorityCategory1)
+            ->registerConfigValue('sorting.priorityCategory2', $ceresConfig->sorting->priorityCategory2)
+            ->registerConfigValue('sorting.priorityCategory3', $ceresConfig->sorting->priorityCategory3)
+            ->registerConfigValue('item.name', $ceresConfig->item->itemName)
+            ->registerConfigValue(
+                'my_account.confirmation_link_expiration',
+                $ceresConfig->myAccount->confirmationLinkExpiration
+            )
+            ->registerConfigValue('item.show_please_select', $ceresConfig->item->showPleaseSelect)
+            ->registerConfigValue('item.show_category_filter', $ceresConfig->item->showCategoryFilter)
+            ->registerConfigValue('global.enableOldUrlPattern', $ceresConfig->global->enableOldUrlPattern)
+            ->registerConfigValue('item.variation_show_type', $ceresConfig->item->variationShowType)
+            ->registerConfigValue('global.google_recaptcha_secret', $ceresConfig->global->googleRecaptchaSecret)
+            ->registerConfigValue('currency.available_currencies', $ceresConfig->currency->availableCurrencies)
+            ->registerConfigValue('my_account.order_return_days', $ceresConfig->myAccount->orderReturnDays)
+            ->registerConfigValue(
+                'checkout.show_all_shipping_profiles',
+                $ceresConfig->checkout->showAllShippingProfiles
+            )
+            ->registerConfigValue('contact.shop_mail', $ceresConfig->contact->shopMail)
+            ->registerConfigValue('global.default_contact_class_b2b', $ceresConfig->global->defaultContactClassB2B)
+            ->registerConfigValue('item.lists.cross_selling_type', $ceresConfig->itemLists->crossSellingType)
+            ->registerConfigValue('item.lists.cross_selling_sorting', $ceresConfig->itemLists->crossSellingSorting)
+            ->registerConfigValue(
+                'my_account.confirmation_link_login_redirect',
+                $ceresConfig->myAccount->confirmationLinkLoginRedirect
+            )
+            ->registerConfigValue('my_account.change_payment', $ceresConfig->myAccount->changePayment)
+            ->registerConfigValue(
+                'my_account.order_return_initial_status',
+                $ceresConfig->myAccount->orderReturnInitialStatus
+            )
+            ->registerConfigValue('global.user_data_hash_max_age', $ceresConfig->global->userDataHashMaxAge)
+            ->registerConfigValue('language.active_languages', $ceresConfig->language->activeLanguages)
+            ->registerConfigValue('basket.split_bundles', $ceresConfig->basket->splitBundles);
     }
 }
