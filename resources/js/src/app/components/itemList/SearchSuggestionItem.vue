9acde009b9ec93f5b1d5efa82ebd3767ac8e36e8
cjohannsen <christoph.johannsen@plentymarkets.com>
2020-03-04 16:43:47 +0100

    CHANGE SearchSuggestionItem.vue - fix text ellipsis of item name labels

--- resources/js/src/app/components/itemList/SearchSuggestionItem.vue
+++ resources/js/src/app/components/itemList/SearchSuggestionItem.vue
@@ -14,10 +14,10 @@
                         <img v-if="item.image" :src="item.image">
                     </div>
 
-                    <div class="label text-truncate" :class="{ 'compact': item.beforeLabel && item.afterLabel }">
-                        <p class="small mb-0" v-if="item.beforeLabel">{{ item.beforeLabel }}</p>
-                        <p class="mb-0" v-html="getHighlightedLabel(item.label)"></p>
-                        <p class="small mb-0" v-if="item.afterLabel">{{ item.afterLabel }}</p>
+                    <div class="label overflow-hidden" :class="{ 'compact': item.beforeLabel && item.afterLabel }">
+                        <p class="small mb-0 text-truncate" v-if="item.beforeLabel">{{ item.beforeLabel }}</p>
+                        <p class="mb-0 text-truncate" v-html="getHighlightedLabel(item.label)"></p>
+                        <p class="small mb-0 text-truncate" v-if="item.afterLabel">{{ item.afterLabel }}</p>
                     </div>
 
                     <div class="count" v-if="showCount && item.count > 0">
