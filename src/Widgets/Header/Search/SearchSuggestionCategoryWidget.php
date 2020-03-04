3edf08a5bd54caf019bb37c373a9374c48bb5803
cjohannsen <christoph.johannsen@plentymarkets.com>
2020-03-04 13:18:19 +0100

    CHANGE SearchSuggestionBaseWidget.php - make settings dynamic; also change translations

--- src/Widgets/Header/Search/SearchSuggestionCategoryWidget.php
+++ src/Widgets/Header/Search/SearchSuggestionCategoryWidget.php
@@ -3,8 +3,6 @@
 
 namespace Ceres\Widgets\Header\Search;
 
-use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
-
 class SearchSuggestionCategoryWidget extends SearchSuggestionBaseWidget
 {
     public $suggestionType = 'category';
@@ -19,17 +17,5 @@ class SearchSuggestionCategoryWidget extends SearchSuggestionBaseWidget
 
     public $position = 200;
 
-    public function getSettings()
-    {
-        /** @var WidgetSettingsFactory $settings * */
-        $settings = WidgetSettingsFactory::create(parent::getSettings());
-
-        $settings->withPointer('appearance');
-
-        $settings->createCheckbox('showCount')
-            ->withName('Widget.searchSuggestionShowCountLabel')
-            ->withDefaultValue(false);
-
-        return $settings->toArray();
-    }
+    public $hasCountOption = true;
 }
