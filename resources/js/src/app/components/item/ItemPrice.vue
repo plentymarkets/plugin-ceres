d5db1af827e5e7777f5fd2a9054880876f1049c3
cjohannsen <christoph.johannsen@plentymarkets.com>
2020-03-10 14:00:20 +0100

    ADD ItemPrice.vue

---
+++ resources/js/src/app/components/item/ItemPrice.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+
+        <div class="crossprice" v-if="showRrp && hasRrp">
+            <del class="text-muted small">
+                {{ currentVariation.prices.rrp.unitPrice.formatted | itemCrossPrice }}
+            </del>
+        </div>
+
+        <span class="price h1">
+            <span :content="currentVariation.prices.default.price.value">
+                <template v-if="showDynamicPrice">
+                    {{ $translate("Ceres::Template.dynamicVariationPrice", { price: variationTotalPrice | currency(currentVariation.prices.default.currency) }) }}
+                </template>
+
+                <template v-else>
+                    {{ variationTotalPrice | currency(currentVariation.prices.default.currency) }}
+                </template>
+            </span>
+            <sup>*</sup>
+            <span :content="currentVariation.prices.default.currency"></span>
+        </span>
+
+        <div class="base-price text-muted my-3" v-if="currentVariation.unit">
+            <div>
+                {{ $translate("Ceres::Template.singleItemContent") }}
+                <span>{{ currentVariation.unit.content | numberFormat }} </span>
+                <span>{{ currentVariation.unit.names.name }}</span>
+            </div>
+            <div v-if="currentVariation.variation.mayShowUnitPrice">
+                {{ $translate("Ceres::Template.singleItemUnitPrice") }}
+                <span class="base-price-value">
+                    {{ variationGraduatedPrice.basePrice | specialOffer(currentVariation.prices, "basePrice") }}
+                </span>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+    props:
+    {
+        showRrp:
+        {
+            type: Boolean,
+            default: true
+        }
+    },
+    data()
+    {
+        return {}
+    },
+    computed:
+    {
+        hasRrp()
+        {
+            return !!this.currentVariation.prices.rrp &&
+                this.currentVariation.prices.rrp.unitPrice.value > 0 &&
+                this.currentVariation.prices.rrp.unitPrice.value > this.currentVariation.prices.default.unitPrice.value;
+        },
+
+        ...mapState({
+            currentVariation: state => state.item.variation.documents[0].data,
+        }),
+
+        ...mapGetters([
+            "showDynamicPrice",
+            "variationGraduatedPrice",
+            "variationTotalPrice"
+        ])
+    }
+}
+</script>
