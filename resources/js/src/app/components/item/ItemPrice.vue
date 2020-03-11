29fe4a58c9ec58c80101e16b542a18b746f7a442
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2020-03-11 14:02:57 +0100

    FIX missing comma

--- resources/js/src/app/components/item/ItemPrice.vue
+++ resources/js/src/app/components/item/ItemPrice.vue
@@ -73,7 +73,7 @@ export default {
 
         variationTotalPrice() {
             return this.$store.getters[`${this.itemId}/variationTotalPrice`]
-        }
+        },
 
         ...mapState({
             currentVariation(state) {
