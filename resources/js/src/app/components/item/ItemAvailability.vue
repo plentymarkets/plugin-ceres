6f76829919fecf4e6308ac5cc836571fed874267
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2020-03-12 18:46:19 +0100

    Null check for availability

--- resources/js/src/app/components/item/ItemAvailability.vue
+++ resources/js/src/app/components/item/ItemAvailability.vue
@@ -3,7 +3,7 @@
         :class="classes"
         :style="paddingStyles">
         <span>
-            {{ currentVariation.variation.availability.names.name }}
+            {{ name }}
         </span>
     </span>
 </template>
@@ -27,16 +27,20 @@ export default {
             return this.$store.state.items[this.itemId] && this.$store.state.items[this.itemId].variation.documents[0].data;
         },
 
-        classes()
-        {
+        classes() {
             return [
                 "availability",
                 "badge",
                 "availability-" + this.currentVariation.variation.availability.id,
                 this.paddingClasses
             ];
-        }
+        },
 
+        name() {
+            return this.currentVariation.variation.availability
+            && this.currentVariation.variation.availability.names
+            && this.currentVariation.variation.availability.names.name;
+        }
     }
 }
 </script>
