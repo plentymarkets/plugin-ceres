2bd03087ecbf2be120b4086dff72eed20ecfb152
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-03-13 14:29:28 +0100

    UPDATE BaseItemModule - cleanup

--- resources/js/src/app/store/modules/singleItem/BaseItemModule.js
+++ resources/js/src/app/store/modules/singleItem/BaseItemModule.js
@@ -2,26 +2,18 @@ import ApiService from "../../../services/ApiService";
 import ItemModule from "./ItemModule";
 import VariationSelectModule from "../VariationSelectModule";
 
-const state =
-    {
-    };
-
-const mutations =
-    {
-    };
-
 const actions =
     {
-        initVariation({ state, commit }, variation)
+        initVariation({ commit }, variation)
         {
-            // register a nested module for the main item
             const itemId = variation.documents[0].data.item.id;
 
+            // register a nested module for the main item
             ceresStore.registerModule(["items", itemId], ItemModule);
             ceresStore.registerModule(["items", itemId, "variationSelect"], VariationSelectModule);
             commit(`${itemId}/setVariation`, variation);
 
-            // rest call for sets if set comps set
+            // rest call for sets if set comps are set
             const setComponentIds = variation.documents[0].data.setComponentVariationIds;
 
             if (setComponentIds && setComponentIds.length)
@@ -32,11 +24,13 @@ const actions =
                         for (const component of Object.values(components.documents))
                         {
                             const itemId = component.data.item.id;
-                            const whackData = { documents: [component] };
+                            // extend the structur of the object to match the old objects
+                            const extendedData = { documents: [component] };
 
+                            // register a module for every set item
                             ceresStore.registerModule(["items", itemId], ItemModule);
                             ceresStore.registerModule(["items", itemId, "variationSelect"], VariationSelectModule);
-                            commit(`${itemId}/setVariation`, whackData);
+                            commit(`${itemId}/setVariation`, extendedData);
                             commit(`${itemId}/setPleaseSelectVariationId`, component.data.variation.id);
                         }
                     });
@@ -44,15 +38,7 @@ const actions =
         }
     };
 
-const getters =
-    {
-
-    };
-
 export default
 {
-    state,
-    actions,
-    mutations,
-    getters
+    actions
 };
