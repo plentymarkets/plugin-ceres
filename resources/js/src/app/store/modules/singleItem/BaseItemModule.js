f608ef289ec004eeb0959109593fb15234b0bacb
cjohannsen <christoph.johannsen@plentymarkets.com>
2020-03-16 11:35:31 +0100

    CHANGE BaseItemModule.js - split action "initVariation" into 2 separate actions

--- resources/js/src/app/store/modules/singleItem/BaseItemModule.js
+++ resources/js/src/app/store/modules/singleItem/BaseItemModule.js
@@ -4,37 +4,40 @@ import VariationSelectModule from "../VariationSelectModule";
 
 const actions =
     {
-        initVariation({ commit }, variation)
+        initVariation({ commit, dispatch }, variation)
         {
-            const itemId = variation.documents[0].data.item.id;
-
             // register a nested module for the main item
-            ceresStore.registerModule(["items", itemId], ItemModule);
-            ceresStore.registerModule(["items", itemId, "variationSelect"], VariationSelectModule);
-            commit(`${itemId}/setVariation`, variation);
+            dispatch("registerItem", variation.documents[0]);
 
             // rest call for sets if set comps are set
             const setComponentIds = variation.documents[0].data.setComponentVariationIds;
 
-            if (setComponentIds && setComponentIds.length)
+            if (!App.isShopbuilder && setComponentIds && setComponentIds.length)
             {
                 ApiService.get("/rest/io/variations", { variationIds: setComponentIds, resultFieldTemplate: "SingleItem" })
                     .done(components =>
                     {
-                        for (const component of Object.values(components.documents))
+                        for (const component of components.documents)
                         {
                             const itemId = component.data.item.id;
-                            // extend the structur of the object to match the old objects
-                            const extendedData = { documents: [component] };
 
                             // register a module for every set item
-                            ceresStore.registerModule(["items", itemId], ItemModule);
-                            ceresStore.registerModule(["items", itemId, "variationSelect"], VariationSelectModule);
-                            commit(`${itemId}/setVariation`, extendedData);
-                            commit(`${itemId}/setPleaseSelectVariationId`, component.data.variation.id);
+                            dispatch("registerItem", component);
+                            commit(`${itemId}/setPleaseSelectVariationId`, itemId);
                         }
                     });
             }
+        },
+
+        registerItem({ commit }, item)
+        {
+            const itemId = item.data.item.id;
+            // extend the structur of the object to match the old objects
+            const extendedData = { documents: [item] };
+
+            ceresStore.registerModule(["items", itemId], ItemModule);
+            ceresStore.registerModule(["items", itemId, "variationSelect"], VariationSelectModule);
+            commit(`${itemId}/setVariation`, extendedData);
         }
     };
 
