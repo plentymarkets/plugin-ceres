b2f5f3de65d80423a73ffdfb1bb52705e6c99077
Felix Dausch <felix.dausch@plentymarkets.com>
2020-03-27 13:59:13 +0100

    remove deployment config

--- .travis/push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pwd
-
-git config --global user.email $GITHUB_MAIL
-git config --global user.name $GITHUB_USERNAME
-
-git add .
-git diff-index --quiet HEAD || git commit -m "Automatic build" && git push origin HEAD:$TRAVIS_BRANCH
