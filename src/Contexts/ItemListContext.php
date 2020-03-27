2bec85591a2483be3196d93609204820dac9757c
Steve Tentrop <steve.tentrop@plentymarkets.com>
2020-03-27 08:32:32 +0100

    add new search and merge data

--- src/Contexts/ItemListContext.php
+++ src/Contexts/ItemListContext.php
@@ -5,6 +5,7 @@ namespace Ceres\Contexts;
 use Ceres\Helper\ExternalSearch;
 use Ceres\Helper\SearchOptions;
 use IO\Services\ItemSearchAutocompleteService;
+use Plenty\Modules\Webshop\ItemSearch\SearchPresets\CategoryItemsVariationCount;
 use Plenty\Modules\Webshop\ItemSearch\SearchPresets\Facets;
 use Plenty\Modules\Webshop\ItemSearch\SearchPresets\SearchItems;
 use Plenty\Modules\Webshop\ItemSearch\SearchPresets\VariationList;
@@ -42,11 +43,11 @@ trait ItemListContext
         if (ExternalSearch::hasExternalSearch()) {
             /** @var ExternalSearch $externalSearch */
             $externalSearch = pluginApp(ExternalSearch::class);
-            $externalSearch->page         = $this->currentPage;
+            $externalSearch->page = $this->currentPage;
             $externalSearch->itemsPerPage = $this->itemsPerPage;
             $externalSearch->searchString = $options['query'];
-            $externalSearch->categoryId   = $options['categoryId'];
-            $externalSearch->sorting      = $this->itemSorting;
+            $externalSearch->categoryId = $options['categoryId'];
+            $externalSearch->sorting = $this->itemSorting;
 
             // emit event to perform external search
             ExternalSearch::getExternalResults($externalSearch);
@@ -68,10 +69,10 @@ trait ItemListContext
                 foreach ($resultVariationIds as $variationId) {
                     $this->itemList[] = $searchResults[$variationId]['documents'][0];
                 }
-                $this->pageMax        = ceil($externalSearch->getCountTotal() / $options['itemsPerPage']);
-                $this->itemCountPage  = count($resultVariationIds);
+                $this->pageMax = ceil($externalSearch->getCountTotal() / $options['itemsPerPage']);
+                $this->itemCountPage = count($resultVariationIds);
                 $this->itemCountTotal = $externalSearch->getCountTotal();
-                $this->facets         = [];
+                $this->facets = [];
 
                 return;
             }
@@ -80,7 +81,7 @@ trait ItemListContext
         $searchResults = $itemSearchService->getResults($defaultSearchFactories);
 
         //try to get result for the "did you mean?" search if there is no result for the original search string
-        if($scope === SearchOptions::SCOPE_SEARCH && (int)$searchResults['itemList']['total'] === 0) {
+        if ($scope === SearchOptions::SCOPE_SEARCH && (int)$searchResults['itemList']['total'] === 0) {
             $originalSearchString = $options['query'];
             /** @var ItemSearchAutocompleteService $itemSearchAutocompleteService */
             $itemSearchAutocompleteService = pluginApp(ItemSearchAutocompleteService::class);
@@ -94,18 +95,37 @@ trait ItemListContext
                 $searchResults = $itemSearchService->getResults(
                     [
                         'itemList' => SearchItems::getSearchFactory($options),
-                        'facets'   => Facets::getSearchFactory($options)
+                        'facets' => Facets::getSearchFactory($options)
                     ]
                 );
+            }
+        }
+
+        $itemIds = [];
+        foreach ($searchResults['itemList']['documents'] as $item) {
+            $itemIds[] = $item['data']['item']['id'];
+        }
+        $optionsVariationCount['itemIds'] = array_unique($itemIds);
+        $variationCount = $itemSearchService->getResults(
+            CategoryItemsVariationCount::getSearchFactory($optionsVariationCount)
+        );
+
+        if (isset($variationCount['itemIds']) &&
+            is_array($variationCount['itemIds']) &&
+            isset($searchResults['itemList']['documents']) &&
+            is_array($searchResults['itemList']['documents'])) {
+
+            foreach ($searchResults['itemList']['documents'] as $key => $entry) {
+                $searchResults['itemList']['documents'][$key]['data']['item']['salableVariationCount'] = $variationCount['itemIds'][$entry['data']['item']['id']] ?? 1;
             }
         }
 
         $this->itemCountTotal = $searchResults['itemList']['total'];
         $this->itemCountTotal = $this->itemCountTotal > 10000 ? 10000 : $this->itemCountTotal;
 
-        $this->pageMax       = ceil($this->itemCountTotal / $options['itemsPerPage']);
+        $this->pageMax = ceil($this->itemCountTotal / $options['itemsPerPage']);
         $this->itemCountPage = count($searchResults['itemList']['documents']);
-        $this->itemList      = $searchResults['itemList']['documents'];
-        $this->facets        = $searchResults['facets'];
+        $this->itemList = $searchResults['itemList']['documents'];
+        $this->facets = $searchResults['facets'];
     }
 }
