ec1a2ed7df25a86ebf3a8d772cfceed1d73371fe
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2020-03-31 15:15:05 +0200

    multiply component quantity with it's price

--- resources/js/src/app/store/modules/singleItem/ItemModule.js
+++ resources/js/src/app/store/modules/singleItem/ItemModule.js
@@ -196,7 +196,7 @@ const getters =
             else if (getters.currentItemVariation.item.itemType !== "set"
                 && rootState.items.isItemSet)
             {
-                return state.variation.documents[0].data.prices.set.price.value + getters.variationPropertySurcharge;
+                return (state.variation.documents[0].data.prices.set.price.value + getters.variationPropertySurcharge) * state.variationOrderQuantity;
             }
             else
             {
