a31e0a6f9d778f55b11982340ff7a7eef714bc28
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-03-31 15:20:49 +0200

    ADD order properties to SetItemComponent

--- resources/js/src/app/components/basket/list/BasketSetComponentList.vue
+++ resources/js/src/app/components/basket/list/BasketSetComponentList.vue
@@ -2,7 +2,7 @@
     <div v-if="setComponents.length > 0" class="set-data small">
         <div class="mb-2"><strong>{{ $translate("Ceres::Template.itemSetContent") }}</strong></div>
         <template v-for="setComponent in setComponents">
-            <set-component-item :variation="setComponent.variation.data" :quantity="setComponent.quantity"></set-component-item>
+            <set-component-item :variation="setComponent.variation.data" :quantity="setComponent.quantity" :order-properties="setComponent.basketItemOrderParams"></set-component-item>
         </template>
     </div>
 </template>
--- resources/js/src/app/components/basket/list/SetComponentItem.vue
+++ resources/js/src/app/components/basket/list/SetComponentItem.vue
@@ -31,6 +31,20 @@
                     </div>
                 </template>
             </div>
+
+            <div class="small" v-if="orderProperties && orderProperties.length">
+                <div class="font-weight-bold my-1">{{ $translate("Ceres::Template.basketAdditionalOptions") }}:</div>
+                <ul class="ml-3">
+                    <li v-for="property in orderProperties" :key="property.propertyId" v-show="isPropertyVisible(property.propertyId)">
+                        <span class="d-block text-truncate">
+                            <strong :class="{ 'colon': property.type.length > 0 }">{{ property.name }} ({{ $translate("Ceres::Template.basketIncludeAbbr") }} {{ variation.properties | propertySurcharge(property.propertyId) | currency }})</strong>
+                            <span>
+                                <order-property-value :property="property"></order-property-value>
+                            </span>
+                        </span>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -41,7 +55,8 @@ export default {
     
     props: {
         variation: Object,
-        quantity: Number
+        quantity: Number,
+        orderProperties: Array
     },
 
     computed: {
