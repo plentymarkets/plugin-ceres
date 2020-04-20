e4c4ede1369dd78e18d740defcd6cc3cfd81c923
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2020-04-20 17:40:58 +0200

    Apply sorting to data to achieve correct rendering of itself

--- src/Wizard/ShopWizard/Services/ShopWizardService.php
+++ src/Wizard/ShopWizard/Services/ShopWizardService.php
@@ -212,7 +212,14 @@ class ShopWizardService
             // search fields logic
 
             $itemSearchSettings = $searchSettingsRepo->getSearchSettings()->toArray();
-            foreach($itemSearchSettings['fields'] as $fieldKey => $fieldSettings) {
+
+            $fields = $itemSearchSettings['fields'];
+
+            usort($fields, function($searchValue1, $searchValue2) {
+                return $searchValue1['boost'] < $searchValue2['boost'] ? 1 : -1;
+            });
+
+            foreach($fields as $fieldKey => $fieldSettings) {
                 $fieldKey += 1;
                 $formFieldPrefix = "search_";
                 switch ($fieldKey) {
