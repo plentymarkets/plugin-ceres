02f6daeaca178efb0985e75cb41d62f58c930ae0
Dominik Meyer <dominik.meyer@plentymarkets.com>
2020-04-24 16:09:58 +0200

    FIX non sb order return for guests

--- src/Contexts/OrderReturnContext.php
+++ src/Contexts/OrderReturnContext.php
@@ -7,11 +7,13 @@ use IO\Helper\ContextInterface;
 class OrderReturnContext extends CategoryContext implements ContextInterface
 {
     public $orderData;
-    
+    public $orderAccessKey;
+
     public function init($params)
     {
         parent::init($params);
         
         $this->orderData = $params['orderData'];
+        $this->orderAccessKey = $params['orderAccessKey'];
     }
 }
