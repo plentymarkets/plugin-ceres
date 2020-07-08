52f77abceda941ee2cc48dcacddbcd98a9d5f854
Christoph Johannsen <Christoph.johannsen@plentymarkets.com>
2020-07-08 14:46:45 +0200

    Update Popper.vue
    rename invisible to hidden

--- resources/js/src/app/components/pageDesign/Popper.vue
+++ resources/js/src/app/components/pageDesign/Popper.vue
@@ -99,7 +99,7 @@ export default {
         classNames()
         {
             // in the shopbuilder we need to hide the popper completely, to hide the dropzone
-            const hideClass = App.isShopBuilder ? "d-none" : "invisible";
+            const hideClass = App.isShopBuilder ? "d-none" : "hidden";
 
             return this.popoverClass + (!this.isVisible ? hideClass : "");
         }
