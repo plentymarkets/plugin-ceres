0f0867e415f351ea29b14dab601a328053db5a3d
Maximilian Röll <maximilian.roell@plentymarkets.com>
2020-08-14 14:54:31 +0200

    - Fixed Tag search not showing facets - Fixed sorting

--- src/Contexts/TagSearchContext.php
+++ src/Contexts/TagSearchContext.php
@@ -5,6 +5,7 @@ namespace Ceres\Contexts;
 use Ceres\Helper\SearchOptions;
 use IO\Helper\ContextInterface;
 use Plenty\Modules\Webshop\ItemSearch\SearchPresets\TagItems;
+use Plenty\Modules\Webshop\ItemSearch\SearchPresets\Facets;
 
 class TagSearchContext extends CategoryContext implements ContextInterface
 {
@@ -18,23 +19,24 @@ class TagSearchContext extends CategoryContext implements ContextInterface
     public function init($params)
     {
         parent::init($params);
-
+        $tagId = $this->getParam( 'tagId','' );
+        $tagIds = is_array( $tagId ) ? $tagId : [$tagId];
         $itemListOptions = [
             'page'          => $this->getParam( 'page', 1 ),
             'itemsPerPage'  => $this->getParam( 'itemsPerPage', '' ),
             'sorting'       => $this->getParam( 'sorting', '' ),
             'tagName'       => $this->getParam( 'tagName', '' ),
-            'tagId'         => $this->getParam( 'tagId','' )
+            'tagIds'        => $tagIds,
+            'facets'        => $this->getParam( 'facets', ''),
+            'priceMin'      => $this->request->get('priceMin', 0),
+            'priceMax'      => $this->request->get('priceMax', 0)
         ];
 
         $itemListOptions = SearchOptions::validateItemListOptions($itemListOptions, SearchOptions::SCOPE_SEARCH);
-        $tagId = $itemListOptions['tagId'];
         $this->initItemList(
             [
-                'itemList' =>  TagItems::getSearchFactory([
-                    'tagIds'    => is_array( $tagId ) ? $tagId : [$tagId],
-                    'sorting'   => $this->getParam('sorting','')
-                ])
+                'itemList' => TagItems::getSearchFactory($itemListOptions),
+                'facets'   => Facets::getSearchFactory($itemListOptions)
             ],
             $itemListOptions,
             SearchOptions::SCOPE_SEARCH
