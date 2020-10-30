81fa6a28ed8359afdab531d6c96687f6e6ee1f09
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-10-30 15:39:50 +0100

    FIX UrlService always takes current pathname to attach params to

--- resources/js/src/app/services/UrlService.js
+++ resources/js/src/app/services/UrlService.js
@@ -100,12 +100,7 @@ export function getUrlParams(urlParams)
 
 export function setUrlParams(urlParams, pushState = true)
 {
-    const pathName =
-        isDefined(store.state.navigation.currentCategory) &&
-        isDefined(store.state.navigation.currentCategory.url) ?
-            store.state.navigation.currentCategory.url :
-            window.location.pathname;
-
+    const pathName = window.location.pathname;
     const params = _createQueryString(urlParams);
     const titleElement = document.getElementsByTagName("title")[0];
 
