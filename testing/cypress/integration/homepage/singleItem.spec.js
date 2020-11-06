f0e61c76a8b351867e761c2623632e9cd6b21781
Luisa Haase <luisa.haase@plentymarkets.com>
2020-11-06 17:01:50 +0100

    Add running tests for checking article name and producer

--- testing/cypress/integration/homepage/singleItem.spec.js
+++ testing/cypress/integration/homepage/singleItem.spec.js
@@ -9,18 +9,30 @@ context("Single Item", () =>
 
     it("should check for article name", () =>
     {
-        cy.get(".title-outer").should("exist");
+        cy.get(".item-name").then(($itemName) => {
+            expect($itemName).to.contain("Loungesessel Herkules");
+        });
     });
 
     it("should check for article producer", () =>
     {
-        cy.get(".producertag").should("exist");
+        cy.get(".producertag").then(($producerName) => {
+            expect($producerName).to.contain("A & C Design");
+        });
     });
 
-    it("should check for seperator between article name and article number", () =>
+    it("should check for the item price", () =>
     {
-        cy.get(".widget-separator").should("exist");
+        cy.get(".has-crossprice").then(($crossPrice) => {
+            const crossPrice = text("0,70"); // Als Zahlenformat vergleichen?
+            expect($crossPrice).to.eql(crossPrice);
+        });
     });
 
+
+    it("should check for breadcrumbs", () =>
+    {
+        cy.get(".breadcrumbs").should("exist");
+    });
 });
 
