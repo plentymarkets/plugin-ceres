08ea394803a6cc0cdf227de28969e4fc4f79011d
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-11-10 12:25:00 +0100

    ADD guest test cases

--- testing/cypress/integration/checkout/payment-provider.spec.js
+++ testing/cypress/integration/checkout/payment-provider.spec.js
@@ -34,7 +34,18 @@ context("Homepage", () =>
 
     it("Should change payment providers as guest", () =>
     {
-
+        visitCheckoutAsGuest();
+        cy.get(`[data-id='${PRE_PAYMENT_ID}']`).click();
+        cy.get(`[data-id='${PRE_PAYMENT_ID}']`).find("input").should("have.be.checked");
+        cy.wait(500);
+        cy.get(`[data-id='${INVOICE_ID}']`).click();
+        cy.get(`[data-id='${INVOICE_ID}']`).find("input").should("have.be.checked");
+        cy.wait(500);
+        cy.get(`[data-id='${CASH_ON_DELIVERY}']`).click();
+        cy.get(`[data-id='${CASH_ON_DELIVERY}']`).find("input").should("have.be.checked");
+        cy.wait(500);
+        cy.get(`[data-id='${PAY_PAL_ID}']`).click();
+        cy.get(`[data-id='${PAY_PAL_ID}']`).find("input").should("have.be.checked");
     });
 
     it("Should have every payment provider visible as user", () =>
@@ -48,7 +59,11 @@ context("Homepage", () =>
 
     it("Should have every payment provider visible as guest", () =>
     {
-
+        visitCheckoutAsGuest();
+        cy.get(`[data-id='${INVOICE_ID}']`).should("exist");
+        cy.get(`[data-id='${PRE_PAYMENT_ID}']`).should("exist");
+        cy.get(`[data-id='${CASH_ON_DELIVERY}']`).should("exist");
+        cy.get(`[data-id='${PAY_PAL_ID}']`).should("exist");
     });
 
     it("Should pay with cash on delivery as user", () =>
@@ -62,7 +77,11 @@ context("Homepage", () =>
 
     it("Should pay with cash on delivery as guest", () =>
     {
-
+        visitCheckoutAsGuest();
+        cy.get(`[data-id='${CASH_ON_DELIVERY}']`).click();
+        cy.get(`[data-id='${CASH_ON_DELIVERY}']`).find("input").should("have.be.checked");
+        completeOrder();
+        cy.get(`[id*=payment_name]`).should("contain", "Nachnahme");
     });
 
     it("Should pay with invoice as user", () =>
@@ -76,7 +95,11 @@ context("Homepage", () =>
 
     it("Should pay with invoice as guest", () =>
     {
-
+        visitCheckoutAsGuest();
+        cy.get(`[data-id='${INVOICE_ID}']`).click();
+        cy.get(`[data-id='${INVOICE_ID}']`).find("input").should("have.be.checked");
+        completeOrder();
+        cy.get(`[id*=payment_name]`).should("contain", "Rechnung");
     });
 
     it("Should pay with pre payment as user", () =>
@@ -90,7 +113,11 @@ context("Homepage", () =>
 
     it("Should pay with pre payment as guest", () =>
     {
-
+        visitCheckoutAsGuest();
+        cy.get(`[data-id='${PRE_PAYMENT_ID}']`).click();
+        cy.get(`[data-id='${PRE_PAYMENT_ID}']`).find("input").should("have.be.checked");
+        completeOrder();
+        cy.get(`[id*=payment_name]`).should("contain", "Vorkasse");
     });
 
     // it("Should pay with paypal as user", () =>
@@ -115,10 +142,31 @@ context("Homepage", () =>
         cy.location("pathname").should("eq", "/checkout/");
     }
 
+    function visitCheckoutAsGuest()
+    {
+        cy.loginAsGuest();
+        cy.addBasketItem(1014);
+        cy.visit("/checkout");
+        cy.location("pathname").should("eq", "/checkout/");
+        addAddress();
+    }
+
     function completeOrder()
     {
         cy.get("input[id^=gtc-accept]").click();
         cy.getByTestingAttr("place-order").click();
         cy.location("pathname").should("eq", "/bestellbestaetigung/");
+    }
+
+    function addAddress()
+    {
+        cy.wait(1000);
+        cy.getByTestingAttr("invoice-addresses-name-select-de").find(`input[name="firstName"]`).type("Plenty", { delay: 40 });
+        cy.getByTestingAttr("invoice-addresses-name-select-de").find(`input[name="lastName"]`).type("Test");
+        cy.getByTestingAttr("invoice-addresses-street-select-de").find(`input[name="street"]`).type("Abby Road");
+        cy.getByTestingAttr("invoice-addresses-street-select-de").find(`input[name="housenumber"]`).type("1337");
+        cy.getByTestingAttr("invoice-addresses-zip-select-de").find(`input[name="zip"]`).type("12345");
+        cy.getByTestingAttr("invoice-addresses-town-select-de").find(`input[name="town"]`).type("Kassel");
+        cy.getByTestingAttr("modal-submit").first().click();
     }
 });
