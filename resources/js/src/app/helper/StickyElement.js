6879dbabcaf5fbe866a6a36446632a874cfccb5d
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2020-11-13 12:35:21 +0100

    change timing

--- resources/js/src/app/helper/StickyElement.js
+++ resources/js/src/app/helper/StickyElement.js
@@ -71,7 +71,7 @@ export class StickyElement
                 setTimeout(() =>
                 {
                     clearInterval(animateChange);
-                }, 350);
+                }, 500);
             });
         }
     }
