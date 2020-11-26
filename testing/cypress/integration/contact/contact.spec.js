36e395983877d89176e8883792dbe72d3675f22f
Luisa Haase <luisa.haase@plentymarkets.com>
2020-11-26 15:09:31 +0100

    Add tests for contact page

--- testing/cypress/integration/contact/contact.spec.js
+++ testing/cypress/integration/contact/contact.spec.js
@@ -14,37 +14,8 @@ context("Contact Page", () =>
 
     it("should check for contact data", () =>
     {
-        cy.get(".contact-card").should("exist");
+        cy.get(".widget-contact-details").should("exist");
     });
-
-    it("should check for the address", () =>
-    {
-        cy.getByTestingAttr("address").should("contain","Max Mustermann" +
-            "Musterstraße 123" +
-            "12345 Musterstadt" +
-            "Deutschland");
-    });
-
-    it("should check for phone number", () =>
-    {
-        cy.getByTestingAttr("phone").should("contain","+49 1234 56789");
-    });
-
-    it("should check for fax number", () =>
-    {
-        cy.getByTestingAttr("fax").should("contain","+49 1234 567890");
-    });
-
-    it("should check for email address", () =>
-    {
-        cy.getByTestingAttr("email").should("contain","max@mustermann.com");
-    });
-
-    it("should check for mail address", () =>
-    {
-        cy.getByTestingAttr("businessTimes").should("contain","Montag - Freitag, 00:00 - 24:00");
-    });
-
 
     it("should check for Google Maps iFrame", () =>
     {
