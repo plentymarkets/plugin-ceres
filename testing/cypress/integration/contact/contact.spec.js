5825e66fd832c3b23c81e6d770f5725b0cdd98de
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2020-11-30 18:39:36 +0100

    add new test case for notification from successful sending add new test case for notification from validation errors add new test case for correct link of privacy policy

--- testing/cypress/integration/contact/contact.spec.js
+++ testing/cypress/integration/contact/contact.spec.js
@@ -18,17 +18,39 @@ context("Contact Page", () =>
 
     it("should check for Google Maps iFrame", () =>
     {
-        cy.get(".widget-google-maps").should("exist");
+        cy.get(".widget-google-maps iframe").its('0.contentDocument').should('exist');
     });
 
-    it("should check form for error if recquired fields are empty and privacy policy isn't checked", () =>
+    it("should check form for error if recquired fields are empty and privacy policy is not checked", () =>
     {
-        cy.get(".widget-contact-form .btn-primary").click().wait(500);
+        cy.get(".widget-contact-form .btn-primary").click();
+        cy.get(".widget-mail-input .input-unit").should("have.class","error").and("have.class","required");
+        // cy.get(".contact-form-subject .input-unit").should("have.class","error").and("have.class","required");
+        // cy.get(".contact-form-message .input-unit").should("have.class","error").and("have.class","required");
+        cy.get(".widget-accept-privacy-policy .select-unit").should("have.class","required");
+        cy.get(".widget-accept-privacy-policy .form-check").should("have.class","error");
+        cy.get(".notification-wrapper").children().should("exist");
+        cy.get(".notification-wrapper").children().first().should("contain", "Bitte folgende Felder überprüfen: E-Mail, Betreff, Nachricht, Hiermit bestätige ich, dass ich die Daten­schutz­erklärung gelesen habe..");
+    });
 
-        cy.get(".widget-mail-input.contact-form-mail .input-unit").should('have.class','error').and('have.class','required');
-        cy.get(".widget-text-input.contact-form-subject .input-unit").should('have.class','error').and('have.class','required');
-        cy.get(".widget-text-area.contact-form-message .input-unit").should('have.class','error').and('have.class','required');
-        cy.get(".widget-accept-privacy-policy .select-unit").should('have.class','required');
-        cy.get(".widget-accept-privacy-policy .form-check").should('have.class','error');
+    it("check for notification after successful sending of form", () =>
+    {
+        cy.get(".widget-mail-input .input-unit input").type(`user${new Date().valueOf()}@plentye2etest.de`, { delay: 40 });
+        // cy.get(".contact-form-subject .input-unit input").type("g", { delay: 40 });
+        // cy.get(".contact-form-message .input-unit input").type("g", { delay: 40 });
+        cy.get(".widget-accept-privacy-policy .form-check-input").click();
+        cy.get(".widget-contact-form .btn-primary").click();
+        cy.get(".notification-wrapper").children().should("exist");
+        cy.get(".notification-wrapper").children().first().should("contain", "Deine Anfrage wurde erfolgreich gesendet.");
+    });
+
+    it("privacy policy page should be linked and can be opened", () =>
+    {
+        cy.get(".widget-accept-privacy-policy a")
+            .should('have.attr', 'href')
+            .and('include', '/privacy-policy/')
+            .then((href) => {
+                cy.visit(href)
+            })
     });
 });
