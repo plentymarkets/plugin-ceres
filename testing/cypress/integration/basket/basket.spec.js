67b58822f436214b2b13c5b2ec09fd34720119f0
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-12-01 16:42:37 +0100

    ADD basket spec check the totals

--- testing/cypress/integration/basket/basket.spec.js
+++ testing/cypress/integration/basket/basket.spec.js
@@ -3,7 +3,7 @@ context("Basket", () =>
 {
     const ITEM_PRICE_NET = 0.59;
     const ITEM_PRICE = 0.70;
-    const SHIPPING_COST_NET = 5.23;
+    const SHIPPING_COST_NET = 5.03;
     const SHIPPING_COST = 5.99;
 
     beforeEach(() =>
@@ -17,7 +17,7 @@ context("Basket", () =>
     {
         checkSums();
     });
-    it.only("Should update the basket item quantity", () =>
+    it("Should update the basket item quantity", () =>
     {
         cy.getByTestingAttr("quantity-btn-increase").click().click();
         cy.getByTestingAttr("quantity-btn-decrease").click();
@@ -39,15 +39,15 @@ context("Basket", () =>
 
     function checkSums(itemQuantity = 1)
     {
-        cy.getByTestingAttr("item-sum-net").should("contain", ITEM_PRICE_NET * itemQuantity);
-        cy.getByTestingAttr("item-sum");
-        cy.getByTestingAttr("shipping-amount-net");
-        cy.getByTestingAttr("shipping-amount");
-        cy.getByTestingAttr("promotion-coupon");
-        cy.getByTestingAttr("basket-amount-net");
+        cy.getByTestingAttr("item-sum-net").should("contain", (ITEM_PRICE_NET * itemQuantity).toLocaleString("de"));
+        cy.getByTestingAttr("item-sum").should("contain", (ITEM_PRICE * itemQuantity).toLocaleString("de"));
+        cy.getByTestingAttr("shipping-amount-net").should("contain", SHIPPING_COST_NET.toLocaleString("de"));
+        cy.getByTestingAttr("shipping-amount").should("contain", SHIPPING_COST.toLocaleString("de"));
+        // cy.getByTestingAttr("promotion-coupon");
+        cy.getByTestingAttr("basket-amount-net").should("contain", (SHIPPING_COST_NET + ITEM_PRICE_NET * itemQuantity).toLocaleString("de"));
         cy.getByTestingAttr("vat-amount");
-        cy.getByTestingAttr("basket-amount");
-        cy.getByTestingAttr("sales-coupon");
-        cy.getByTestingAttr("open-amount");
+        cy.getByTestingAttr("basket-amount").should("contain", (SHIPPING_COST + ITEM_PRICE * itemQuantity).toLocaleString("de"));
+        // cy.getByTestingAttr("sales-coupon");
+        // cy.getByTestingAttr("open-amount");
     }
 });
