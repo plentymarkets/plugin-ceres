b58d26e300c639963b28da853c729a98507393ab
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-12-02 18:05:49 +0100

    ADD coupon test cases

--- testing/cypress/integration/basket/coupon.spec.js
+++ testing/cypress/integration/basket/coupon.spec.js
@@ -5,6 +5,7 @@ context("Basket", () =>
     const ITEM_PRICE = 0.70;
     const SHIPPING_COST_NET = 5.03;
     const SHIPPING_COST = 5.99;
+    const COUPON_CODE = "4CDSQS";
 
     beforeEach(() =>
     {
@@ -13,26 +14,50 @@ context("Basket", () =>
         cy.location("pathname").should("eq", "/warenkorb/");
     });
 
-    // TODO move to own spec
     it("Should add coupon", () =>
     {
+        cy.server().route("POST", "/rest/io/coupon").as("redeemCoupon");
+        cy.getByTestingAttr("coupon-input").type(COUPON_CODE, { delay: 30 });
+        cy.getByTestingAttr("coupon-redeem").click();
+        cy.wait("@redeemCoupon");
+        checkTotals(1, true);
+    });
+
+    it("Should remove coupon code", () =>
+    {
+        cy.server().route("POST", "/rest/io/coupon").as("redeemCoupon");
+        cy.server().route("DELETE", "/rest/io/coupon/4CDSQS/").as("deleteCoupon");
+        cy.getByTestingAttr("coupon-input").type(COUPON_CODE, { delay: 30 });
+        cy.getByTestingAttr("coupon-redeem").click();
+        cy.wait("@redeemCoupon");
+        cy.getByTestingAttr("coupon-remove").click();
+        cy.wait("@deleteCoupon");
         checkTotals();
     });
 
-    // should fail on invalid coupon code
-    // should remove coupon
-
-    function checkTotals(itemQuantity = 1)
+    it.only("Should fail on invalid coupon code", () =>
     {
+        cy.getByTestingAttr("coupon-input").type("INVALID", { delay: 30 });
+        cy.getByTestingAttr("coupon-redeem").click();
+        cy.get(".notification-wrapper").children().first().should("have.class", "alert-danger");
+        cy.get(".notification-wrapper").children().first().should("contain", "Der Gutschein konnte nicht eingelöst werden.");
+    });
+
+    function checkTotals(itemQuantity = 1, isFree = false)
+    {
+        const basketAmountNet = isFree ? 0.0 : (SHIPPING_COST_NET + ITEM_PRICE_NET * itemQuantity);
+        const basketAmountRaw = SHIPPING_COST + ITEM_PRICE * itemQuantity;
+        const basketAmount = isFree ? 0.0 : basketAmountRaw;
+
         cy.getByTestingAttr("item-sum-net").should("contain", (ITEM_PRICE_NET * itemQuantity).toLocaleString("de"));
         cy.getByTestingAttr("item-sum").should("contain", (ITEM_PRICE * itemQuantity).toLocaleString("de"));
         cy.getByTestingAttr("shipping-amount-net").should("contain", SHIPPING_COST_NET.toLocaleString("de"));
         cy.getByTestingAttr("shipping-amount").should("contain", SHIPPING_COST.toLocaleString("de"));
-        // cy.getByTestingAttr("promotion-coupon");
-        cy.getByTestingAttr("basket-amount-net").should("contain", (SHIPPING_COST_NET + ITEM_PRICE_NET * itemQuantity).toLocaleString("de"));
-        cy.getByTestingAttr("vat-amount");
-        cy.getByTestingAttr("basket-amount").should("contain", (SHIPPING_COST + ITEM_PRICE * itemQuantity).toLocaleString("de"));
-        // cy.getByTestingAttr("sales-coupon");
-        // cy.getByTestingAttr("open-amount");
+        if (isFree)
+        {
+            cy.getByTestingAttr("promotion-coupon").should("contain", (basketAmountRaw * -1).toLocaleString("de"));
+        }
+        cy.getByTestingAttr("basket-amount-net").should("contain", basketAmountNet.toLocaleString("de"));
+        cy.getByTestingAttr("basket-amount").should("contain", basketAmount.toLocaleString("de"));
     }
 });
