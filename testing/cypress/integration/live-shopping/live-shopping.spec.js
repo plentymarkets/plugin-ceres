84483f867a81f505881a26dd9bae6ab5a0d82e99
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-12-17 13:17:44 +0100

    ADD live shopping test cases and spec

--- testing/cypress/integration/live-shopping/live-shopping.spec.js
+++ testing/cypress/integration/live-shopping/live-shopping.spec.js
@@ -8,22 +8,31 @@ context("Live shopping", () =>
 
     it("Should show the live shopping widgets", () =>
     {
-        cy.location("pathname").should("eq", "/live-shopping");
-        // checks if the logo exists
-        cy.get(".brand-wrapper").should("exist");
+        cy.location("pathname").should("eq", "/live-shopping/");
+        cy.get(".widget-live-shopping").should("have.length", 2);
     });
 
-    // Should display currently active live shopping item
     it("Should show the live shopping widgets", () =>
     {
-        cy.get(".live-shopping-countdown-heading").first().should("contains", "Angebot endet in:");
+        cy.get(".live-shopping-countdown-heading").first().should("contain", "Angebot endet in:");
     });
-    // Should display the correct live shopping price
-    // Should display rebate
-    // Should display next live shopping item
+
+    it("Should display the correct live shopping price", () =>
+    {
+        cy.get(".live-shopping-price").first().should("contain", "0,60");
+    });
+    it("Should display rebate", () =>
+    {
+        cy.get(".live-shopping-price").first().should("contain", "0,60");
+    });
+
     it("Should display next live shopping item", () =>
     {
-        cy.get(".live-shopping-countdown-heading").eq(1).should("contains", "Angebot startet in:");
+        cy.get(".live-shopping-countdown-heading").eq(1).should("contain", "Angebot startet in:");
     });
-    // Should link to the correct variation
+
+    it("Should link to the correct variation", () =>
+    {
+
+    });
 });
