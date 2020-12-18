12c1b8daea8ab4cc05631bc2ea6bd98f3488d7e5
Felix Dausch <felix.dausch@plentymarkets.com>
2020-12-18 13:38:26 +0100

    FIX item lists

--- src/Containers/CeresItemListContainer3.php
+++ src/Containers/CeresItemListContainer3.php
@@ -11,33 +11,6 @@ class CeresItemListContainer3
 {
     public function call(Twig $twig, $arg):string
     {
-        $tagList = [];
-        
-        /** @var CeresConfig $ceresConfig */
-        $ceresConfig = pluginApp(CeresConfig::class);
-    
-        $listType = $ceresConfig->itemLists->list3Type;
-    
-        if($listType == 'tag_list')
-        {
-            /** @var ItemSearchService $itemSearchService */
-            $itemSearchService = pluginApp( ItemSearchService::class );
-        
-            $itemSearchOptions = [
-                'tagIds' => explode(',', $ceresConfig->itemLists->list3TagIds),
-                'sorting' => $ceresConfig->itemLists->tagSorting
-            ];
-        
-            $result = $itemSearchService->getResults([
-                                                         'tagItems' => TagItems::getSearchFactory( $itemSearchOptions )
-                                                     ]);
-        
-            if(count($result['tagItems']))
-            {
-                $tagList = $result['tagItems']['documents'];
-            }
-        }
-        
         return $twig->render('Ceres::Containers.ItemLists.ItemList3', ["item" => $arg[0], "itemList" => $tagList]);
     }
 }
