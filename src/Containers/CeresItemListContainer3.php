6ee08b036c1843a1856b7718da21d893b15579c9
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2020-12-21 13:47:48 +0100

    remove obsolete itemList entry

--- src/Containers/CeresItemListContainer3.php
+++ src/Containers/CeresItemListContainer3.php
@@ -11,6 +11,6 @@ class CeresItemListContainer3
 {
     public function call(Twig $twig, $arg):string
     {
-        return $twig->render('Ceres::Containers.ItemLists.ItemList3', ["item" => $arg[0], "itemList" => $tagList]);
+        return $twig->render('Ceres::Containers.ItemLists.ItemList3', ["item" => $arg[0]]);
     }
 }
