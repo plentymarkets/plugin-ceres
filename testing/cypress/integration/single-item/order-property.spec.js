13cb9ab195808f6d67dfe647313a7d6803b68af8
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2020-12-31 13:45:06 +0100

    should display order properties should go to next order property page

--- testing/cypress/integration/single-item/order-property.spec.js
+++ testing/cypress/integration/single-item/order-property.spec.js
@@ -8,12 +8,32 @@ context("Single Item", () =>
 
     it("should display order properties", () =>
     {
-
+        cy.getByTestingAttr("order-property-previous-slide").should("be.visible");
+        cy.getByTestingAttr("order-property-next-slide").should("be.visible");
+        cy.getByTestingAttr("order-property-input-radio").should("be.visible");
     });
 
     it("should go to next order property page", () =>
     {
+        cy.getByTestingAttr("order-property-next-slide").click();
+        cy.getByTestingAttr("order-property-input-checkbox").should("be.visible").should("have.attr", "value").and("include", "8");
 
+        cy.getByTestingAttr("order-property-next-slide").click();
+        cy.getByTestingAttr("order-property-input-checkbox").eq(3).should("be.visible").should("have.attr", "value").and("include", "4");
+
+        cy.getByTestingAttr("order-property-next-slide").click();
+        cy.get(".input-unit.file-input.order-property-input").eq(0).should("be.visible");
+        cy.getByTestingAttr("order-property-selection").eq(0).should("be.visible");
+        cy.getByTestingAttr("order-property-selection-option").eq(0).should("be.visible");
+        cy.getByTestingAttr("order-property-input-text").eq(0).should("be.visible");
+        cy.getByTestingAttr("order-property-input-int").eq(0).should("be.visible");
+
+        cy.getByTestingAttr("order-property-next-slide").click();
+        cy.get(".input-unit.file-input.order-property-input").eq(1).should("be.visible");
+        cy.getByTestingAttr("order-property-selection").eq(1).should("be.visible");
+        cy.getByTestingAttr("order-property-selection-option").eq(1).should("be.visible");
+        cy.getByTestingAttr("order-property-input-text").eq(1).should("be.visible");
+        cy.getByTestingAttr("order-property-input-int").eq(1).should("be.visible");
     });
 
     it("should go to previous order property page", () =>
