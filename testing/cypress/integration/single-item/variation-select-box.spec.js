cf49a92df73ab9f4bec89208f6aec295b839f273
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2020-12-31 03:39:46 +0100

    add more tests for variation select with boxes and selects

--- testing/cypress/integration/single-item/variation-select-box.spec.js
+++ testing/cypress/integration/single-item/variation-select-box.spec.js
@@ -44,6 +44,7 @@ context("Variation Select - Box", () =>
         cy.get(".v-s-boxes > div").eq(0).should("contain", "Basic").should("have.class", "active");
         cy.get(".v-s-boxes > div").eq(1).should("contain", "Advanced");
         cy.get(".v-s-boxes > div").eq(2).should("contain", "Professional");
+        cy.get(".v-s-boxes > div").eq(3).should("not.exist");
         isSaleable();
 
         cy.get(".v-s-boxes > div").eq(1).click();
@@ -63,15 +64,82 @@ context("Variation Select - Box", () =>
     /*
     Artikel-ID 143: Zwei Attribute - Dropdown/Box - Nicht kaufbar angezeigt - Nicht verfügbar angezeigt
 
-    1118 - 1128 angezeigt1
+    1118 - 1128 angezeigt
     1119  (schwarz, basic) nicht kaufbar
     (rot, basic) nicht verfügbare Variante, die Variante mit den Attributen gibt es nicht
     */
 
-    it.only("should show unbuyable variation for item 142", () =>
+    it("should show all possible attributes for 143", () =>
     {
-        cy.visit("/variantenauswahl/zwei-attribute-dropdown-box-nicht-gruppiert_143_1122/");
+        cy.visit("/variantenauswahl/zwei-attribute-dropdown-box-nicht-gruppiert_143_1123/");
+
+        cy.get(".variation-select .text-muted").should("contain", "Ausstattung - Box - Nicht gruppiert:");
+        cy.get(".variation-select .text-muted + b").should("contain", "Advanced");
+
+        cy.get(".v-s-boxes > div").eq(0).should("contain", "Advanced").should("have.class", "active");
+        cy.get(".v-s-boxes > div").eq(1).should("contain", "Professional");
+        cy.get(".v-s-boxes > div").eq(2).should("contain", "Basic");
+        cy.get(".v-s-boxes > div").eq(3).should("not.exist");
+
+        cy.get(".variation-select label").should("contain", "Farbe - Dropdown - Nicht Gruppiert");
+
+        cy.get(".custom-select").find("option[value='5']").should("contain", "rot");
+        cy.get(".custom-select").find("option[value='6']").should("contain", "schwarz");
+        cy.get(".custom-select").find("option[value='7']").should("contain", "lila");
+        cy.get(".custom-select").find("option[value='8']").should("contain", "blau");
+        cy.get(".custom-select").find("option").eq(4).should("not.exist");
+    });
+
+    it("should show all possible variations for variation 1119", () =>
+    {
+        cy.visit("/variantenauswahl/zwei-attribute-dropdown-box-nicht-gruppiert_143_1119/");
+
+        cy.get(".variation-select .text-muted").should("contain", "Ausstattung - Box - Nicht gruppiert:");
+        cy.get(".variation-select .text-muted + b").should("contain", "Basic");
+
+        cy.get(".custom-select").find("option[value='5']").should("contain", "rot (nicht verfügbar)");
+        cy.get(".custom-select").find("option[value='6']").should("contain", "schwarz (Ausverkauft)");
+        cy.get(".custom-select").find("option[value='7']").should("contain", "lila");
+        cy.get(".custom-select").find("option[value='8']").should("contain", "blau");
+        cy.get(".custom-select").find("option").eq(4).should("not.exist");
+    });
+
+    it("should not be possible to buy variation 1118", () =>
+    {
+        cy.visit("/variantenauswahl/zwei-attribute-dropdown-box-nicht-gruppiert_143_1118/");
+
+        cy.get(".v-s-boxes > div").eq(2).should("contain", "Basic").should("have.class", "active");
+        isNotSaleable();
+
+        cy.get(".v-s-boxes > div").eq(0).click();
+        cy.get(".v-s-boxes > div").eq(2).should("have.class", "invalid")
+            .should("have.attr", "data-original-title")
+            .and("include", "Ausverkauft");
+        isSaleable();
+    });
+
+    it.only("should not be possible to buy variation with color black and basic box", () =>
+    {
+        cy.visit("/variantenauswahl/zwei-attribute-dropdown-box-nicht-gruppiert_143_1118/");
+
+        cy.get(".v-s-boxes > div").eq(2).should("contain", "Basic").should("have.class", "active");
+        isNotSaleable();
+
+        cy.get(".custom-select").select("rot (nicht verfügbar)");
+        cy.location("pathname").should("eq", "/variantenauswahl/zwei-attribute-dropdown-box-nicht-gruppiert_143/");
+        cy.get(".notification-wrapper").children().first()
+            .should("have.class", "alert-warning")
+            .should("contain", "Ausstattung - Box - Nicht gruppiert nicht verfügbar.");
+        cy.get(".v-s-boxes > div").eq(2).should("contain", "Basic")
+            .should("have.class", "invalid")
+            .should("have.attr", "data-original-title")
+            .and("include", "<b>Farbe - Dropdown - Nicht Gruppiert</b> in der Auswahl nicht verfügbar.");
+        cy.get(".variation-select .text-muted").should("contain", "Ausstattung - Box - Nicht gruppiert:");
+        cy.get(".variation-select .text-muted + b").should("contain", "Keine Auswahl");
+        isNotSaleable();
     }); 
+
+
 
     function isSaleable()
     {
