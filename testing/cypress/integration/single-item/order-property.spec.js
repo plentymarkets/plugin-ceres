bccc1b0142170d37017c1aa5c44c4c1057c72846
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-12-31 15:31:06 +0100

    ADD order property - first wip test cases

--- testing/cypress/integration/single-item/order-property.spec.js
+++ testing/cypress/integration/single-item/order-property.spec.js
@@ -38,47 +38,89 @@ context("Single Item", () =>
 
     it("should go to previous order property page", () =>
     {
-
+        cy.getByTestingAttr("order-property-next-slide").click().click();
+        cy.getByTestingAttr("order-property-previous-slide").click();
+        cy.getByTestingAttr("order-property-input-checkbox").should("be.visible").should("have.attr", "value").and("include", "8");
     });
 
     it("should select single choice", () =>
     {
+        cy.getByTestingAttr("order-property-input-radio").first().click();
 
+        cy.getByTestingAttr("single-add-to-basket-button").click();
+        cy.get("#add-item-to-basket-overlay").should("contain", "Einfachauswahl +0€");
+        checkPrices(0);
     });
 
     it("should select multiple choice", () =>
     {
+        // TODO implement more reliable way to navigate?
+        cy.getByTestingAttr("order-property-next-slide").click();
+        cy.getByTestingAttr("order-property-input-checkbox").first().click();
 
+        cy.getByTestingAttr("single-add-to-basket-button").click();
+        cy.get("#add-item-to-basket-overlay").should("contain", "Mehrfachauswahl +0€");
     });
 
     it("should select label", () =>
     {
+        cy.getByTestingAttr("order-property-next-slide").click().click();
+        cy.getByTestingAttr("order-property-input-checkbox").click();
 
+        cy.getByTestingAttr("single-add-to-basket-button").click();
+        cy.get("#add-item-to-basket-overlay").should("contain", "Label ohne Aufpreis");
     });
 
     it("should fill file upload input", () =>
     {
-
+        // TODO
     });
 
     it("should fill selection input", () =>
     {
+        cy.getByTestingAttr("order-property-next-slide").click().click().click();
+        cy.getByTestingAttr("order-property-selection").click();
+        cy.getByTestingAttr("order-property-selection-option").click();
 
+        cy.getByTestingAttr("single-add-to-basket-button").click();
+        cy.get("#add-item-to-basket-overlay").should("contain", "Auswahl");
+        cy.get("#add-item-to-basket-overlay").should("contain", "Option 1");
     });
 
     it("should fill text input", () =>
     {
+        const value = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
 
+        cy.getByTestingAttr("order-property-next-slide").click().click().click();
+        cy.getByTestingAttr("order-property-input-text").type(value);
+
+        cy.getByTestingAttr("single-add-to-basket-button").click();
+        cy.get("#add-item-to-basket-overlay").should("contain", "Text");
+        cy.get("#add-item-to-basket-overlay").should("contain", value);
     });
 
     it("should fill float number input", () =>
     {
+        const value = 1.23;
 
+        cy.getByTestingAttr("order-property-next-slide").click().click().click();
+        cy.getByTestingAttr("order-property-input-float").type(value);
+
+        cy.getByTestingAttr("single-add-to-basket-button").click();
+        cy.get("#add-item-to-basket-overlay").should("contain", "Kommazahl");
+        cy.get("#add-item-to-basket-overlay").should("contain", value);
     });
 
     it("should fill int input", () =>
     {
+        const value = 123;
 
+        cy.getByTestingAttr("order-property-next-slide").click().click().click();
+        cy.getByTestingAttr("order-property-input-int").type(value);
+
+        cy.getByTestingAttr("single-add-to-basket-button").click();
+        cy.get("#add-item-to-basket-overlay").should("contain", "Ganze Zahl");
+        cy.get("#add-item-to-basket-overlay").should("contain", value);
     });
 
     it("should fill file upload input with extra costs", () =>
@@ -105,4 +147,11 @@ context("Single Item", () =>
     {
 
     });
+
+    // should i18n
+
+    function checkPrices(surcharge)
+    {
+        // TODO implement price check on single item and in basket
+    }
 });
