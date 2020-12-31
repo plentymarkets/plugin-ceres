8f418be9431e0ba26514058b820314e925ee43cd
Lukas Matzen <lukas.matzen@plentymarkets.com>
2020-12-31 12:07:07 +0100

    ADD order property - empty test cases

---
+++ testing/cypress/integration/single-item/order-property.spec.js
@@ -0,0 +1,88 @@
+// / <reference types="cypress" />
+context("Single Item", () =>
+{
+    beforeEach(() =>
+    {
+        cy.visit("/testartikel/merkmale_145_1134/");
+    });
+
+    it("should display order properties", () =>
+    {
+
+    });
+
+    it("should go to next order property page", () =>
+    {
+
+    });
+
+    it("should go to previous order property page", () =>
+    {
+
+    });
+
+    it("should select single choice", () =>
+    {
+
+    });
+
+    it("should select multiple choice", () =>
+    {
+
+    });
+
+    it("should select label", () =>
+    {
+
+    });
+
+    it("should fill file upload input", () =>
+    {
+
+    });
+
+    it("should fill selection input", () =>
+    {
+
+    });
+
+    it("should fill text input", () =>
+    {
+
+    });
+
+    it("should fill float number input", () =>
+    {
+
+    });
+
+    it("should fill int input", () =>
+    {
+
+    });
+
+    it("should fill file upload input with extra costs", () =>
+    {
+
+    });
+
+    it("should fill selection input with extra costs", () =>
+    {
+
+    });
+
+    it("should fill text input with extra costs", () =>
+    {
+
+    });
+
+    it("should fill float number input with extra costs", () =>
+    {
+
+    });
+
+    it("should fill int input with extra costs", () =>
+    {
+
+    });
+});
