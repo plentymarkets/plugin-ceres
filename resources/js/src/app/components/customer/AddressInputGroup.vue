1ca12569c30fc7d361d7c99538dcc466fe9c7bae
Lukas Matzen <lukas.matzen@plentymarkets.com>
2021-01-11 16:55:35 +0100

    UPDATE vat id component place in every address template

--- resources/js/src/app/components/customer/AddressInputGroup.vue
+++ resources/js/src/app/components/customer/AddressInputGroup.vue
@@ -390,15 +390,13 @@
                         class="col-12 col-sm-6"
                         v-if="(isInOptionalFields('gb', 'billing_address.salutation') && value.gender === 'company' &&  isInOptionalFields('gb', 'billing_address.vatNumber')) ||
                         (!isInOptionalFields('gb', 'billing_address.salutation') && isInOptionalFields('gb', 'billing_address.name1') && isInOptionalFields('gb', 'billing_address.vatNumber'))">
-                        <div
-                            class="input-unit"
-                            data-model="vatNumber"
-                            v-validate:text="isInRequiredFields('gb', 'billing_address.vatNumber')">
-                            <input type="text" name="vatNumber" :id="'txtVatNumber' + _uid" :value="value.vatNumber" @input="emitInputEvent('vatNumber', $event.target.value)" data-autofocus>
-                            <label :for="'txtVatNumber' + _uid">
-                                {{ transformTranslation("Ceres::Template.addressVatNumber", "gb", "billing_address.vatNumber") }}
-                            </label>
-                        </div>
+                        <vat-id
+                            v-validate:text="isInRequiredFields('gb', 'billing_address.vatNumber')"
+                            :is-required="isInRequiredFields('gb', 'billing_address.vatNumber')"
+                            :selected-country-id="value.countryId"
+                            @input="emitInputEvent('vatNumber', $event)"
+                            :initial-value="value.vatNumber">
+                        </vat-id>
                     </div>
                 </div>
             </div>
@@ -581,15 +579,13 @@
                         class="col-12 col-sm-6"
                         v-if="(isInOptionalFields('de', 'delivery_address.salutation') && value.gender === 'company' &&  isInOptionalFields('de', 'delivery_address.vatNumber')) ||
                         (!isInOptionalFields('de', 'delivery_address.salutation') && isInOptionalFields('de', 'delivery_address.name1') && isInOptionalFields('de', 'delivery_address.vatNumber'))">
-                        <div
-                            class="input-unit"
-                            data-model="vatNumber"
-                            v-validate:text="isInRequiredFields('de', 'delivery_address.vatNumber')">
-                            <input type="text" name="vatNumber" :id="'txtVatNumber' + _uid" :value="value.vatNumber" @input="emitInputEvent('vatNumber', $event.target.value)" data-autofocus>
-                            <label :for="'txtVatNumber' + _uid">
-                                {{ transformTranslation("Ceres::Template.addressVatNumber", "de", "delivery_address.vatNumber") }}
-                            </label>
-                        </div>
+                        <vat-id
+                            v-validate:text="isInRequiredFields('de', 'delivery_address.vatNumber')"
+                            :is-required="isInRequiredFields('de', 'delivery_address.vatNumber')"
+                            :selected-country-id="value.countryId"
+                            @input="emitInputEvent('vatNumber', $event)"
+                            :initial-value="value.vatNumber">
+                        </vat-id>
                     </div>
                 </div>
             </div>
@@ -763,15 +759,13 @@
                         class="col-12 col-sm-6"
                         v-if="(isInOptionalFields('gb', 'delivery_address.salutation') && value.gender === 'company' &&  isInOptionalFields('gb', 'delivery_address.vatNumber')) ||
                         (!isInOptionalFields('gb', 'delivery_address.salutation') && isInOptionalFields('gb', 'delivery_address.name1') && isInOptionalFields('gb', 'delivery_address.vatNumber'))">
-                        <div
-                            class="input-unit"
-                            data-model="vatNumber"
-                            v-validate:text="isInRequiredFields('gb', 'delivery_address.vatNumber')">
-                            <input type="text" name="vatNumber" :id="'txtVatNumber' + _uid" :value="value.vatNumber" @input="emitInputEvent('vatNumber', $event.target.value)" data-autofocus>
-                            <label :for="'txtVatNumber' + _uid">
-                                {{ transformTranslation("Ceres::Template.addressVatNumber", "gb", "delivery_address.vatNumber") }}
-                            </label>
-                        </div>
+                        <vat-id
+                            v-validate:text="isInRequiredFields('gb', 'delivery_address.vatNumber')"
+                            :is-required="isInRequiredFields('gb', 'delivery_address.vatNumber')"
+                            :selected-country-id="value.countryId"
+                            @input="emitInputEvent('vatNumber', $event)"
+                            :initial-value="value.vatNumber">
+                        </vat-id>
                     </div>
                 </div>
             </div>
