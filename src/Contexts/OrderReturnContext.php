53dd4e32188c3b045f74ee5e816741ee6c091862
cjohannsen <christoph.johannsen@plentymarkets.com>
2021-01-18 19:41:57 +0100

    CHANGE OrderReturnContext.php - add missing code comments

--- src/Contexts/OrderReturnContext.php
+++ src/Contexts/OrderReturnContext.php
@@ -6,9 +6,19 @@ use IO\Helper\ContextInterface;
 
 class OrderReturnContext extends CategoryContext implements ContextInterface
 {
+    /**
+     * @var array Data, including information about the current order.
+     */
     public $orderData;
+
+    /**
+     * @var string Key to authenticate the user and allow him to open the view.
+     */
     public $orderAccessKey;
 
+    /**
+     * @inheritdoc
+     */
     public function init($params)
     {
         parent::init($params);
