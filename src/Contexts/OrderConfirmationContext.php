b1cbc86562bc76837022d5e809ce5936a99b4cf8
cjohannsen <christoph.johannsen@plentymarkets.com>
2021-01-19 09:34:11 +0100

    CHANGE OrderConfirmationContext - add missing code comments

--- src/Contexts/OrderConfirmationContext.php
+++ src/Contexts/OrderConfirmationContext.php
@@ -6,12 +6,29 @@ use IO\Helper\ContextInterface;
 
 class OrderConfirmationContext extends CategoryContext implements ContextInterface
 {
-    
+    /**
+     * @var array Data, including information about the current order.
+     */
     public $data;
+
+    /**
+     * @var array Totals for the order.
+     */
     public $totals;
+
+    /**
+     * Defines if the user should be able to change the payment method later on.
+     */
     public $showAdditionalPaymentInformation;
+
+    /**
+     * @inheritdoc
+     */
     public $assetName = "ceres-checkout";
     
+    /**
+     * @inheritdoc
+     */
     public function init($params)
     {
         parent::init($params);
