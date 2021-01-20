33f6240db3b1296a8edd2532db58f230ee46947f
Felix Gehrmann <felix.gehrmann@plentymarkets.com>
2021-01-20 17:23:10 +0100

    docs for containers

--- src/Containers/CeresNewsletterInputContainer.php
+++ src/Containers/CeresNewsletterInputContainer.php
@@ -4,8 +4,23 @@ namespace Ceres\Containers;
 
 use Plenty\Plugin\Templates\Twig;
 
+/**
+ * Class CeresNewsletterInputContainer
+ * This class was previously a DataProvider. It is now a stub.
+ * @package Ceres\Containers
+ * @deprecated
+ */
 class CeresNewsletterInputContainer
 {
+    /**
+     * Does nothing, as the underlying template does no longer exist.
+     * @deprecated
+     * @param Twig $twig
+     * @return string
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
     public function call(Twig $twig)
     {
         return $twig->render('Ceres::Containers.Newsletter.ContainerNewsletterInput');
