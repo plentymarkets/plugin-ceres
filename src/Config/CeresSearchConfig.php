f2486a85ddd884e2d7201051b4aff48bf6b795b0
cjohannsen <christoph.johannsen@plentymarkets.com>
2021-01-28 16:38:46 +0100

    CHANGE CeresSearchConfig and CeresSortingConfig - add missing code comments

--- src/Config/CeresSearchConfig.php
+++ src/Config/CeresSearchConfig.php
@@ -6,16 +6,21 @@ use Plenty\Modules\Webshop\Helpers\PluginConfig;
 
 class CeresSearchConfig extends PluginConfig
 {
+    /**
+     * @var bool $forwardToSingleItem Search: Forward to single item view.
+     *
+     * @deprecated will be removed in 6.0.0.
+     */
     public $forwardToSingleItem;
-    
+
     /**
      * @inheritDoc
      */
-    protected function getPluginName()
+    protected function getPluginName() :string
     {
         return 'Ceres';
     }
-    
+
     /**
      * @inheritDoc
      */
