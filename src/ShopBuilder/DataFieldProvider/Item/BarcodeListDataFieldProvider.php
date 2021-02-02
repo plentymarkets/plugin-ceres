b866e6ff34684385b4c19f9f7a226f5c961725b7
Franz Mutschler <36404400+fmutschler@users.noreply.github.com>
2021-02-02 14:01:18 +0100

    Update BarcodeListDataFieldProvider.php

--- src/ShopBuilder/DataFieldProvider/Item/BarcodeListDataFieldProvider.php
+++ src/ShopBuilder/DataFieldProvider/Item/BarcodeListDataFieldProvider.php
@@ -9,8 +9,8 @@ use Plenty\Modules\ShopBuilder\Providers\DataFieldProvider;
 /**
  * Class BarcodeListDataFieldProvider
  *
- * This class is a data field provider centered about the topic of bar codes.
- * It is used to enable placeholders for dynamic data in the shop builders text widget.
+ * This class is a data field provider centered on the topic of barcodes.
+ * It is used to enable placeholders for dynamic data in the ShopBuilder's text widget.
  * Please refer to the parent class for more information about DataFieldProviders.
  * Please refer to https://developers.plentymarkets.com/dev-doc/result-fields-ceres for more information about
  * the data fields.
@@ -19,7 +19,7 @@ use Plenty\Modules\ShopBuilder\Providers\DataFieldProvider;
 class BarcodeListDataFieldProvider extends DataFieldProvider
 {
     /**
-     * Registers item data fields for use in the shop builder.
+     * Registers item data fields for use in the ShopBuilder.
      */
     function register()
     {
