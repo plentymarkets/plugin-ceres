16af83f4fe068f5007e03fc37b2dace8c31bf3b4
Franz Mutschler <36404400+fmutschler@users.noreply.github.com>
2021-02-02 10:49:28 +0100

    Update TextsDataFieldProvider.php

--- src/ShopBuilder/DataFieldProvider/Item/TextsDataFieldProvider.php
+++ src/ShopBuilder/DataFieldProvider/Item/TextsDataFieldProvider.php
@@ -7,8 +7,8 @@ use Plenty\Modules\ShopBuilder\Providers\DataFieldProvider;
 /**
  * Class TextsDataFieldProvider
  *
- * This class is a data field provider centered about the topic of texts.
- * It is used to enable placeholders for dynamic data in the shop builders text widget.
+ * This class is a data field provider centered on the topic of texts.
+ * It is used to enable placeholders for dynamic data in the ShopBuilder's text widget.
  * Please refer to the parent class for more information about DataFieldProviders.
  * Please refer to https://developers.plentymarkets.com/dev-doc/result-fields-ceres for more information about
  * the data fields.
@@ -17,7 +17,7 @@ use Plenty\Modules\ShopBuilder\Providers\DataFieldProvider;
 class TextsDataFieldProvider extends DataFieldProvider
 {
     /**
-     * Registers item data fields for use in the shop builder.
+     * Registers item data fields for use in the ShopBuilder.
      */
     function register()
     {
