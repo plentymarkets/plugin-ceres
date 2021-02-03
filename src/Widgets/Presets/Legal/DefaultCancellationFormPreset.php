79528922e47b5ec8585d3daa114723063d6b131d
Franz Mutschler <36404400+fmutschler@users.noreply.github.com>
2021-02-03 11:17:54 +0100

    Update DefaultCancellationFormPreset.php

--- src/Widgets/Presets/Legal/DefaultCancellationFormPreset.php
+++ src/Widgets/Presets/Legal/DefaultCancellationFormPreset.php
@@ -10,7 +10,7 @@ use Plenty\Modules\ShopBuilder\Contracts\ContentPreset;
  * Class DefaultCancellationFormPreset
  *
  * This is a preset for ShopBuilder contents. Presets can be applied during content creation to generate a default content with predefined and configured widgets.
- * This particular preset generates a page for viewing the shops cancellation form. It contains:
+ * This particular preset generates a page for viewing the shop's cancellation form. It contains:
  * - CodeWidget
  * - SeparatorWidget
  * - LegalTextsWidget
