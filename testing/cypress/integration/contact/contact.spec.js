e2c7b006ab7bd600ca033787a15d1e825c030605
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2021-02-16 16:44:41 +0100

    add additional test cases for error states

--- testing/cypress/integration/contact/contact.spec.js
+++ testing/cypress/integration/contact/contact.spec.js
@@ -29,7 +29,7 @@ context("Contact Page", () =>
         cy.get(".contact-form-message .input-unit").should("have.class", "error").and("have.class", "required");
         cy.get(".widget-accept-privacy-policy .select-unit").should("have.class", "required");
         cy.get(".widget-accept-privacy-policy .form-check").should("have.class", "error");
-        cy.get(".notification-wrapper").children().should("exist");
+        cy.get(".notification-wrapper").children().should("have.class", "show").and("have.class", "alert-danger");
         cy.get(".notification-wrapper").children().first().should("contain", "Bitte folgende Felder überprüfen: E-Mail, Betreff, Nachricht, Hiermit bestätige ich, dass ich die Daten­schutz­erklärung gelesen habe..");
     });
 
@@ -51,7 +51,70 @@ context("Contact Page", () =>
         cy.get(".widget-mail-attachment .input-unit-preview").should("not.have.class", "disabled").should('be.empty');
     });
 
+    it("check for error notification after sending form with file attachment that exceeds the upload size limit", () =>
+    {
+        cy.get(".widget-mail-input").type(`user${new Date().valueOf()}@plentye2etest.de`, { delay: 40 });
+        cy.get(".contact-form-subject").type("g", { delay: 40 });
+        cy.get(".contact-form-message").type("g", { delay: 40 });
+
+        cy.fixture("xl.jpg").then(fileContent =>
+        {
+            cy.get(".widget-mail-attachment input").attachFile({
+                fileContent: fileContent.toString(),
+                fileName: "xl.jpg",
+                mimeType: "image/jpg"
+            });
+
+            cy.get(".widget-accept-privacy-policy").click();
+            cy.intercept("POST", "/rest/io/customer/contact/mail/file/").as("uploadFile");
+            cy.get(".widget-contact-form .btn-primary").click().wait(10000);
+            cy.get(".notification-wrapper").children().should("have.class", "show").and("have.class", "alert-danger");
+            cy.get(".notification-wrapper").children().first().should("contain", "Die Datei konnte nicht hochgeladen werden, da sie die maximale Dateigröße von 10 MB übersteigt.");
+            cy.wait("@uploadFile").its("response.statusCode").should("eq", 400);
+        });
+    });
+
+    it("check for error notification after sending form with file attachment with forbidden MIME type", () =>
+    {
+        cy.get(".widget-mail-input").type(`user${new Date().valueOf()}@plentye2etest.de`, { delay: 40 });
+        cy.get(".contact-form-subject").type("g", { delay: 40 });
+        cy.get(".contact-form-message").type("g", { delay: 40 });
+
+        cy.fixture("test.php").then(fileContent =>
+        {
+            cy.get(".widget-mail-attachment input").attachFile({
+                fileContent: fileContent.toString(),
+                fileName: "test.php",
+                mimeType: "application/x-httpd-php"
+            });
+
+            cy.get(".widget-accept-privacy-policy").click();
+            cy.intercept("POST", "/rest/io/customer/contact/mail/file/").as("uploadFile");
+            cy.get(".widget-contact-form .btn-primary").click();
+            cy.get(".notification-wrapper").children().should("have.class", "show").and("have.class", "alert-danger");
+            cy.get(".notification-wrapper").children().first().should("contain", "Die Datei konnte nicht hochgeladen werden, da sie die maximale Dateigröße von 10 MB übersteigt.");
+            
+            cy.wait("@uploadFile").its("response.statusCode").should("eq", 400);
+        });
+    });
+
     it("check for notification after successful sending of form", () =>
+    {
+        cy.get(".widget-mail-input").type(`user${new Date().valueOf()}@plentye2etest.de`, { delay: 40 });
+        cy.get(".contact-form-subject").type("g", { delay: 40 });
+        cy.get(".contact-form-message").type("g", { delay: 40 });
+        cy.get(".widget-accept-privacy-policy").click();
+        
+        cy.intercept("POST", "/rest/io/customer/contact/mail/").as("sendForm");
+        cy.get(".widget-contact-form .btn-primary").click();
+
+        cy.get(".notification-wrapper").children().should("have.class", "show").and("have.class", "alert-success");
+        cy.get(".notification-wrapper").children().first().should("contain", "Deine Anfrage wurde erfolgreich gesendet.");
+
+        cy.wait("@sendForm").its("response.statusCode").should("eq", 201);
+    });
+
+    it("check for notification after successful sending of form with file attached", () =>
     {
         cy.get(".widget-mail-input").type(`user${new Date().valueOf()}@plentye2etest.de`, { delay: 40 });
         cy.get(".contact-form-subject").type("g", { delay: 40 });
@@ -65,9 +128,13 @@ context("Contact Page", () =>
             });
         });
         cy.get(".widget-accept-privacy-policy").click();
+        cy.intercept("POST", "/rest/io/customer/contact/mail/file/").as("uploadFile");
         cy.get(".widget-contact-form .btn-primary").click();
-        cy.get(".notification-wrapper").children().should("exist");
+
+        cy.get(".notification-wrapper").children().should("have.class", "show");
         cy.get(".notification-wrapper").children().first().should("contain", "Deine Anfrage wurde erfolgreich gesendet.");
+
+        cy.wait("@uploadFile").its("response.statusCode").should("eq", 201);
     });
 
     it("privacy policy page should be linked and can be opened", () =>
