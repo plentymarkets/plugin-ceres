bacfd09c297c5d7283ef3b34de84b9377b0c3336
Felix Dausch <felix.dausch@plentymarkets.com>
2021-03-01 16:00:35 +0100

    UPDATE vue renderer

--- resources/js/src/app/vue-server-renderer.js
+++ resources/js/src/app/vue-server-renderer.js
@@ -1,12 +1,14 @@
 const createRenderer = require("vue-server-renderer").createRenderer;
+const path = require("path");
 const {
     performance
-  } = require('perf_hooks');
+} = require('perf_hooks');
 
 process.stdin.setEncoding("utf8");
 process.stdout.setEncoding("utf8");
 let domInline = "";
 
+const t0 = performance.now();
 process.stdin.on("readable", () =>
 {
     let chunk;
@@ -20,87 +22,82 @@ process.stdin.on("readable", () =>
 
 process.stdin.on("end", () =>
 {
-    const t0 = performance.now();
-    let twigHtml = domInline.toString();
-    const vueAppMarker = "<!-- VUE_APP -->";
-    const vueAppStartIndex = twigHtml.indexOf(vueAppMarker);
-    const vueAppEndIndex = twigHtml.lastIndexOf(vueAppMarker);
-    const vueAppHtml = twigHtml.substring(vueAppStartIndex, vueAppEndIndex);
-
-    const ceresAppDateMarker = "// CERES_APP_DATA";
-    const ceresAppDateStart = twigHtml.indexOf(ceresAppDateMarker);
-    const ceresAppDateEnd = twigHtml.lastIndexOf(ceresAppDateMarker);
-
-    let ceresAppData = twigHtml.substring(ceresAppDateStart + ceresAppDateMarker.length, ceresAppDateEnd);
-
-    ceresAppData = ceresAppData.replace("App = ", "").replace(";", "").trim();
-
-    
-    // Translation extraction
-    const ceresTranslationRegex = /<script type="application\/json" data-translation="([^"]*)">(.*)<\/script>/g;
-    const ceresTranslationMatches = twigHtml.matchAll(ceresTranslationRegex);
-    const ceresTranslations = {};
-    
-    for(const match of ceresTranslationMatches)
-    {
-        const key = match[1].split('::');
-        const group = key[0], name = key[1];
-
-        if(ceresTranslations[group] === undefined)
-        {
-            ceresTranslations[group] = {};
-        }
-
-        ceresTranslations[group][name] = JSON.parse(match[2]);
-    }
-
-    const t1 = performance.now();
-    console.log("String processing took " + (t1 - t0) + " milliseconds.")
-
-    global.App = JSON.parse(ceresAppData);
-    global.translations = ceresTranslations;
-
     try
     {
-        const createApp = require("../../dist/ceres-server.js").default;
-        // const vueAppElement = document.getElementById("vue-app");
+        const t1 = performance.now();
+        let twigHtml = domInline.toString();
+        const vueAppMarker = "<!-- VUE_APP -->";
+        const vueAppStartIndex = twigHtml.indexOf(vueAppMarker);
+        const vueAppEndIndex = twigHtml.lastIndexOf(vueAppMarker);
+        const vueAppHtml = twigHtml.substring(vueAppStartIndex, vueAppEndIndex);
+
+        const ceresAppDateMarker = "// CERES_APP_DATA";
+        const ceresAppDateStart = twigHtml.indexOf(ceresAppDateMarker);
+        const ceresAppDateEnd = twigHtml.lastIndexOf(ceresAppDateMarker);
+
+        let ceresAppData = twigHtml.substring(ceresAppDateStart + ceresAppDateMarker.length, ceresAppDateEnd);
+
+        ceresAppData = ceresAppData.replace("App = ", "").replace(";", "").trim();
+
+
+        // Translation extraction
+        const ceresTranslationRegex = /<script type="application\/json" data-translation="([^"]*)">(.*)<\/script>/g;
+        const ceresTranslationMatches = twigHtml.matchAll(ceresTranslationRegex);
+        const ceresTranslations = {};
+
+        for(const match of ceresTranslationMatches)
+        {
+            const key = match[1].split('::');
+            const group = key[0], name = key[1];
+
+            if(ceresTranslations[group] === undefined)
+            {
+                ceresTranslations[group] = {};
+            }
+
+            ceresTranslations[group][name] = JSON.parse(match[2]);
+        }
+
+        global.App = JSON.parse(ceresAppData);
+        global.translations = ceresTranslations;
+        const t2 = performance.now();
+
+        const scriptPath = path.resolve(process.env.rootDir, '../plugins/sets/31/productive/Ceres/resources/js/dist/ceres-server.js');
+        const createApp = require(scriptPath).default;
+        const t3 = performance.now();
         const vueApp = createApp({
             template: vueAppHtml
         }).then((vueApp) =>
         {
-            // vueAppElement.parentElement.replaceChild(
-            //     document.createComment("vue-ssr-outlet"),
-            //     vueAppElement
-            // );
-            console.log("app created");
-
             twigHtml = twigHtml.replace(vueAppHtml, "<!--vue-ssr-outlet-->");
             // TODO cant this be placed inside the renderToString callback
             twigHtml = twigHtml.replace("<!-- SSR_SCRIPT_CONTAINER -->", `<script type="x-template" id="ssr-script-container">${vueAppHtml}</script>`);
 
-            // document.getElementById("ssr-script-container").innerHTML = vueAppElement.outerHTML;
-
+            const t4 = performance.now();
             createRenderer({ template: twigHtml })
                 .renderToString(vueApp, (err, renderedHTML) =>
                 {
                     if (err)
                     {
-                        console.log(err);
                         process.stdout.write(err);
-                        return 0;
+                        process.exit(-3);
+                        return;
                     }
 
                     renderedHTML = renderedHTML.replace(
                         "<!-- INITIAL_STATE -->",
                         `<script>window.__INITIAL_STATE__ = ${JSON.stringify(vueApp.$store.state)}</script>`);
 
-                    process.stdout.write(renderedHTML);
+                    const t5 = performance.now();
+                    process.stdout.write(`<!-- read input: ${t0 - t1}ms - processing string: ${t1 - t2}ms - require script: ${t2 - t3}ms - create app: ${t3 - t4}ms - render: ${t4 - t5}ms - total: ${t0 - t5}} -->` + renderedHTML);
                 });
         });
     }
     catch (e)
     {
-        console.log("error: ", e);
-        process.stdout.write("ERROR: ", e);
+        process.stdout.write(e);
+        process.exit(-1);
     }
 });
+
+process.exit(0);
