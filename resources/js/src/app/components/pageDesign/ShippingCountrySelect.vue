557e2627c5296433a2ff638d499bd3ceecc866f4
Lukas Matzen <lukas.matzen@plentymarkets.com>
2021-03-02 18:04:24 +0100

    ADD ShippingCountrySelect - lazy hydration

--- resources/js/src/app/components/pageDesign/ShippingCountrySelect.vue
+++ resources/js/src/app/components/pageDesign/ShippingCountrySelect.vue
@@ -65,6 +65,8 @@ export default {
 
     mounted()
     {
+        // TODO move out of here because of lazy hydration
+        console.log("mnt country select");
         removeUrlParam("openBasketPreview");
     },
 
