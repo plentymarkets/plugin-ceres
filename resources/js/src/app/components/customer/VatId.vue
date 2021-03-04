4b33c7e3be87ef06ec8a45bf910b53c0d6e43aa9
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2021-03-04 14:44:46 +0100

    remove debugging stuff

--- resources/js/src/app/components/customer/VatId.vue
+++ resources/js/src/app/components/customer/VatId.vue
@@ -86,7 +86,6 @@ export default
 
         isPrefixValid()
         {
-            console.log("isPrefixValid update");
             let isPrefixValid = false;
             const validPrefix = this.vatCodes.find(vatCode => this.value?.startsWith(vatCode));
 
@@ -109,16 +108,8 @@ export default
     {
         value(newValue)
         {
-            console.log("watch value");
             this.setValues(newValue);
-        },
-
-        selectedCountryId(countryId)
-        {
-            console.log("watch countryId");
-            // dont delete value after country change
-            // this.deleteValue();
-        },
+        }
     },
 
     created()
@@ -137,7 +128,6 @@ export default
 
         emitChange()
         {
-            console.log("emit: ", value);
             const value = !!this.vatNumber ? this.vatPrefix + this.vatNumber : "";
 
             this.$emit('input', value);
@@ -145,7 +135,6 @@ export default
 
         deleteValue()
         {
-            console.log("delete");
             this.vatNumber = "";
             this.vatPrefix = this.selectedCountry.vatCodes && this.selectedCountry.vatCodes[0] ? this.selectedCountry.vatCodes[0] : "";
 
@@ -154,7 +143,6 @@ export default
 
         setValues(value)
         {
-            console.log("setValues: ", value);
             const vatPrefix = this.vatCodes.find(vatCode => value?.startsWith(vatCode));
 
             if (!!vatPrefix)
