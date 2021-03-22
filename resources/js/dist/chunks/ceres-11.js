dbcf98a3b6b1afb6f261dfa526ac95b920260fd1
Steve Tentrop <steve.tentrop@plentymarkets.com>
2021-03-22 14:42:00 +0100

    add build files

--- resources/js/dist/chunks/ceres-11.js
+++ resources/js/dist/chunks/ceres-11.js
@@ -352,9 +352,11 @@ var NotificationService = __webpack_require__(/*! ../../services/NotificationSer
      */
     unsetInvalidSelection: function unsetInvalidSelection(attributeId, attributeValueId, unitId) {
       var qualifiedVariations = this.getQualifiedVariations(attributeId, attributeValueId, unitId);
-      var closestVariation = this.getClosestVariations(qualifiedVariations)[0];
+      var closestVariations = this.getClosestVariations(qualifiedVariations); // if the salable 'closestVariations' is undefined, take the not-salable one
 
-      if (!closestVariation) {
+      var closestVariation = closestVariations[0] || closestVariations[1];
+
+      if (!closestVariations || closestVariations.length <= 0) {
         return;
       }
 
