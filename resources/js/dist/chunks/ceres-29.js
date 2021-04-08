70be179e132f8530c9e5cbb487fb634de5f738a5
Luisa Haase <luisa.haase@plentymarkets.com>
2021-04-08 16:27:59 +0200

    Update build files

--- resources/js/dist/chunks/ceres-29.js
+++ resources/js/dist/chunks/ceres-29.js
@@ -84,6 +84,10 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 //
 //
 //
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "graduated-prices",
   props: {
@@ -110,6 +114,12 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
       return _toConsumableArray(prices).sort(function (priceA, priceB) {
         return priceA.minimumOrderQuantity - priceB.minimumOrderQuantity;
       });
+    },
+    showBasePrice: function showBasePrice() {
+      var currentVariation = this.$store.getters["".concat(this.itemId, "/currentItemVariation")];
+      var mayShowUnitPrice = currentVariation.variation.mayShowUnitPrice;
+      var isSinglePiece = currentVariation.unit && currentVariation.unit.content === 1 && currentVariation.unit.unitOfMeasurement === "C62";
+      return mayShowUnitPrice && !isSinglePiece;
     },
     activeGraduationIndex: function activeGraduationIndex() {
       var _this = this;
@@ -151,62 +161,102 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _vm.graduatedPrices[0]
-    ? _c("div", [
-        _c("b", [
-          _vm._v(
-            _vm._s(
-              _vm.$translate("Ceres::Template.singleItemGraduatedPrices")
-            ) + ":"
-          )
-        ]),
-        _vm._v(" "),
-        _c(
-          "table",
-          { staticClass: "graduated-prices-table text-muted" },
+    ? _c(
+        "div",
+        [
+          _c("b", [
+            _vm._v(
+              _vm._s(
+                _vm.$translate("Ceres::Template.singleItemGraduatedPrices")
+              ) + ":"
+            )
+          ]),
+          _vm._v(" "),
           _vm._l(_vm.graduatedPrices, function(price, index) {
-            return _c("tr", [
-              _c(
-                "td",
-                { class: _vm.paddingClasses, style: _vm.paddingInlineStyles },
-                [
-                  _vm._v(
-                    _vm._s(
-                      _vm.$translate(
-                        "Ceres::Template.singleItemMinimumQuantity"
+            return _c(
+              "table",
+              { staticClass: "graduated-prices-table text-muted" },
+              [
+                _c("tr", [
+                  _c(
+                    "td",
+                    {
+                      class: _vm.paddingClasses,
+                      style: _vm.paddingInlineStyles
+                    },
+                    [
+                      _vm._v(
+                        _vm._s(
+                          _vm.$translate(
+                            "Ceres::Template.singleItemMinimumQuantity"
+                          )
+                        ) +
+                          " " +
+                          _vm._s(price.minimumOrderQuantity)
                       )
-                    ) +
-                      " " +
-                      _vm._s(price.minimumOrderQuantity)
-                  )
-                ]
-              ),
-              _vm._v(" "),
-              _c(
-                "td",
-                { class: _vm.paddingClasses, style: _vm.paddingInlineStyles },
-                [
-                  _vm._v(
-                    "\n                " +
-                      _vm._s(price.unitPrice.formatted) +
-                      "\n                "
+                    ]
                   ),
-                  _c("transition", { attrs: { name: "fade" } }, [
-                    index === _vm.activeGraduationIndex
-                      ? _c("i", {
-                          staticClass:
-                            "fa fa-lg fa-check-circle-o ml-1 text-appearance",
-                          attrs: { "aria-hidden": "true" }
-                        })
-                      : _vm._e()
-                  ])
-                ],
-                1
-              )
-            ])
+                  _vm._v(" "),
+                  _c(
+                    "td",
+                    {
+                      staticClass: "graduated-price",
+                      class: _vm.paddingClasses,
+                      style: _vm.paddingInlineStyles
+                    },
+                    [
+                      _vm._v(
+                        "\n                " +
+                          _vm._s(price.unitPrice.formatted) +
+                          "\n                "
+                      ),
+                      _c("transition", { attrs: { name: "fade" } }, [
+                        index === _vm.activeGraduationIndex
+                          ? _c("i", {
+                              staticClass:
+                                "fa fa-lg fa-check-circle-o ml-1 text-appearance",
+                              attrs: { "aria-hidden": "true" }
+                            })
+                          : _vm._e()
+                      ])
+                    ],
+                    1
+                  )
+                ]),
+                _vm._v(" "),
+                _c("tr", [
+                  _vm.showBasePrice
+                    ? _c(
+                        "td",
+                        {
+                          staticClass: "graduated-base-price",
+                          class: _vm.paddingClasses,
+                          style: _vm.paddingInlineStyles,
+                          attrs: { colspan: "2" }
+                        },
+                        [
+                          _vm._v(
+                            "(" +
+                              _vm._s(
+                                _vm.$translate(
+                                  "Ceres::Template.singleItemGraduatedBasePrice",
+                                  { price: price.basePrice }
+                                )
+                              ) +
+                              ")"
+                          )
+                        ]
+                      )
+                    : _vm._e()
+                ])
+              ]
+            )
           }),
-          0
-        )
-      ])
+          _vm._v(" "),
+          _c("br")
+        ],
+        2
+      )
     : _vm._e()
 }
 var staticRenderFns = []
