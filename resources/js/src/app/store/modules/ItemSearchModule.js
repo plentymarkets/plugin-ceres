d139dec9091338fa22bfeb0d2ab1e1e11bc7e948
Maximilian Röll <maximilian.roell@plentymarkets.com>
2021-04-26 11:54:47 +0200

    Fix ssr autocomplete search

--- resources/js/src/app/store/modules/ItemSearchModule.js
+++ resources/js/src/app/store/modules/ItemSearchModule.js
@@ -6,7 +6,7 @@ const state = () => ({
     autocompleteRequest: null,
     autocompleteResult: { item: [], category: [], suggestion: [] },
     autocompleteSearchString: "",
-    autocompleteTypes: new Set()
+    autocompleteTypes: []
 });
 
 const mutations =
@@ -28,7 +28,7 @@ const mutations =
 
         addAutocompleteType(state, type)
         {
-            state.autocompleteTypes.add(type);
+            state.autocompleteTypes.push(type);
         }
     };
 
