7cf408984b2dbcefce3fb9cf0cba2f62fdf8a283
Lukas Matzen <lukas.matzen@plentymarkets.com>
2021-04-27 12:02:30 +0200

    ADD LazyImage slot to add additional image sources eg. for different media querys

--- resources/js/src/app/components/common/LazyImg.vue
+++ resources/js/src/app/components/common/LazyImg.vue
@@ -2,6 +2,7 @@
     <picture v-if="!isBackgroundImage" :data-iesrc="fallbackUrl || imageUrl" :data-picture-class="pictureClass" :data-alt="alt" :data-title="title">
         <source :srcset="imageUrl" :type="mimeType">
         <source v-if="fallbackUrl" :srcset="fallbackUrl">
+        <slot name="additionalImages"></slot>
     </picture>
     <div v-else :data-background-image="backgroundSource" :class="pictureClass">
         <slot></slot>
