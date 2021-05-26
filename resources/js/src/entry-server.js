67973b14e75e69a205e5f18fffacd74174d51cb6
Felix Dausch <felix.dausch@plentymarkets.com>
2021-05-26 22:43:49 +0200

    FIX linter errors

--- resources/js/src/entry-server.js
+++ resources/js/src/entry-server.js
@@ -13,22 +13,27 @@ function createApp(context)
 {
     return new Promise((resolve, reject) =>
     {
-        if(process.env.NODE_ENV === "production") {
+        if (process.env.NODE_ENV === "production")
+        {
             // for minified production bundle, only error handler is available
-            Vue.config.errorHandler = (err, vm, info) => {
+            Vue.config.errorHandler = (err, vm, info) =>
+            {
                 context.throwError({
                     message: `Error in ${ info }: "${ err.toString() }"`,
                     stack: err.stack
                 });
-            }
-        } else {
+            };
+        }
+        else
+        {
             // use more detailed warn handler for development bundles
-            Vue.config.warnHandler = (msg, vm, trace) => {
+            Vue.config.warnHandler = (msg, vm, trace) =>
+            {
                 context.throwError({
                     message: msg,
                     stack: trace.trim()
                 });
-            }
+            };
         }
         // defines if the render location is the server
         App.isSSR = true;
