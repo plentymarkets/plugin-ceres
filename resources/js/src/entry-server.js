485ea085f5b4ee5d6a1428b9f5fd76fbbdc8489c
Felix Dausch <felix.dausch@plentymarkets.com>
2021-05-26 22:33:21 +0200

    Pass vue errors to core renderer to be thrown during ssr

--- resources/js/src/entry-server.js
+++ resources/js/src/entry-server.js
@@ -13,6 +13,21 @@ function createApp(context)
 {
     return new Promise((resolve, reject) =>
     {
+        if(process.env.NODE_ENV === "production") {
+            Vue.config.errorHandler = (err, vm, info) => {
+                context.throwError({
+                    message: `Error in ${ info }: "${ err.toString() }"`,
+                    stack: err.stack
+                });
+            }
+        } else {
+            Vue.config.warnHandler = (msg, vm, trace) => {
+                context.throwError({
+                    message: msg,
+                    stack: trace.trim()
+                });
+            }
+        }
         // defines if the render location is the server
         App.isSSR = true;
         App.isSSREnabled = App.config.log.performanceSsr;
