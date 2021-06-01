54b7841ae33c26181d04f28820ebc461f0206cf0
Christoph Johannsen <christoph.johannsen@plentymarkets.com>
2021-06-01 10:48:28 +0200

    CHANGE HeaderScroller.js

--- resources/js/src/app/helper/HeaderScroller.js
+++ resources/js/src/app/helper/HeaderScroller.js
@@ -56,16 +56,21 @@ export default class HeaderScroller
         this.collectHeaderElementHeights();
         this.updateZIndexes();
 
-        // Initialize only, if the user has scrolled down from the top and is not in the shopbuilder.
         // Do not fixate, if the height of the fixed header elements is greater than 0.
-        if (!App.isShopBuilder && window.pageYOffset > 0 && this.headerHeight > 0)
+        if (this.headerHeight <= 0)
+        {
+            this.initialized = true;
+
+            return;
+        }
+
+        // Initialize only, if the user has scrolled down from the top and is not in the shopbuilder.
+        if (!App.isShopBuilder && window.pageYOffset > 0)
         {
             this.calculateBodyOffset();
             this.scrollHeaderElements();
             // If the header content gets active in the shopbuilder, the event listener for 'shopbuilder.after.activate-container' will fixate the header.
             this.fixateHeader();
-
-            this.initialized = true;
         }
     }
 
