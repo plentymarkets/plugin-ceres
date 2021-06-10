6bf833bf7ba3066e5585232dd57f1adf62d132f7
Luisa Haase <luisa.haase@plentymarkets.com>
2021-06-10 16:34:04 +0200

    Added property to set rel attribute in list widget

--- src/Widgets/Common/ListWidget.php
+++ src/Widgets/Common/ListWidget.php
@@ -7,6 +7,7 @@ use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
 use Ceres\Widgets\Helper\WidgetCategories;
 use Ceres\Widgets\Helper\Factories\WidgetDataFactory;
 use Ceres\Widgets\Helper\WidgetTypes;
+use Plenty\Modules\ShopBuilder\Factories\Settings\ValueListFactory;
 
 class ListWidget extends BaseWidget
 {
@@ -58,6 +59,19 @@ class ListWidget extends BaseWidget
         $container->children->createText("text")
             ->withName("Widget.listEntryNameLabel")
             ->withTooltip("Widget.listEntryNameTooltip");
+
+        $container->children->createSelect("rel")
+            ->withDefaultValue('none')
+            ->withName('Widget.listRel')
+            ->withTooltip('Widget.listRelTooltip')
+            ->withListBoxValues(
+                ValueListFactory::make()
+                    ->addEntry('none', 'Widget.listRelNone')
+                    ->addEntry('nofollow', 'Widget.listRelNofollow')
+                    ->addEntry('sponsored', 'Widget.listRelSponsored')
+                    ->addEntry('noopener', 'Widget.listRelNoopener')
+                    ->toArray()
+            );
 
         $container->children->createUrl("url")
             ->withName("Widget.listEntryUrlLabel")
