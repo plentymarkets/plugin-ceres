2ac6aceb0888acdca24c60be8e88571262c36641
Petrisor Frincu <petrisor.frincu@plentymarkets.com>
2021-06-11 16:49:53 +0300

    Added support for redirect functionality + added text into widget settings

--- resources/js/src/app/components/pageDesign/LanguageDetection.vue
+++ resources/js/src/app/components/pageDesign/LanguageDetection.vue
@@ -1,18 +1,73 @@
 <template>
-  <div>language selection placeholder from vue</div>
+  <div v-if="languageText" class="row py-2">
+    <div class="col-sm-8">{{ languageText }}</div>
+    <div class="col-sm-4 text-right">
+      <a :href="languageRedirect" :class="'btn btn-sm btn-' + appearance">{{ buttonText }}</a>
+      <a href="#" @click="deactivateRedirection" class="m-sm-1"><i class="fa fa-close"></i></a>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
+    data () {
+      return {
+        languageText: '',
+        buttonText: '',
+        languageRedirect: '',
+        appearance: '',
+        textLanguages: ''
+      }
+    },
+    props: ["redirect", "appearance", "langtext"],
     mounted() {
       this.initializeComponent()
     },
     methods: {
       initializeComponent() {
-        console.log('Default language:', App.defaultLanguage);
-        console.log('Current language:', App.language);
-        console.log('Language map:', App.languageMap);
-        console.log('Browser languages:', window.navigator.languages);
+
+        this.setAppearance();
+        this.setTextLanguages();
+
+        for (let i = 0; i < window.navigator.languages.length; i++) {
+          const langObject= window.navigator.languages[i].split('-')
+          const linkTag = document.querySelector('link[hreflang="' + langObject[0] +'"]')
+
+          if (App.language !== langObject[0] && linkTag) {
+            if (this.$props.redirect){
+              if (!window.localStorage.getItem('redirectActive')){
+                window.localStorage.setItem('redirectActive', true);
+                window.location.href = linkTag.getAttribute('href')
+              }
+            } else if (!window.localStorage.getItem('redirectDeactivated')) {
+              window.localStorage.removeItem('redirectActive');
+              this.setLanguage(langObject[0]);
+              this.languageRedirect = linkTag.getAttribute('href')
+              break;
+            }
+          }
+
+        }
+      },
+      deactivateRedirection() {
+        this.languageText = null;
+        window.localStorage.setItem('redirectDeactivated', true);
+      },
+      setAppearance() {
+        this.appearance = this.$props.appearance;
+      },
+      setTextLanguages() {
+        this.textLanguages = this.$props.langtext
+      },
+      setLanguage(lang) {
+        const langObj = JSON.parse(this.textLanguages);
+        for (let i=0; i < langObj.length; i++){
+          if (langObj[i].languageCountry === lang) {
+            this.languageText = langObj[i].languageText;
+            this.buttonText = langObj[i].languageButton;
+            break
+          }
+        }
       }
     }
   }
--- src/Widgets/Header/LanguageDetectionWidget.php
+++ src/Widgets/Header/LanguageDetectionWidget.php
@@ -3,6 +3,7 @@
 namespace Ceres\Widgets\Header;
 
 use Ceres\Widgets\Helper\BaseWidget;
+use Ceres\Widgets\Helper\Factories\Settings\ValueListFactory;
 use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
 use Ceres\Widgets\Helper\WidgetCategories;
 use Ceres\Widgets\Helper\Factories\WidgetDataFactory;
@@ -48,6 +49,32 @@ class LanguageDetectionWidget extends BaseWidget
 
         $settingsFactory->createCustomClass();
         $settingsFactory->createAppearance();
+        $settingsFactory->createCheckbox("redirect")
+            ->withDefaultValue(false)
+            ->withName("Widget.languageDetectionRedirect");
+
+        $languageDetectionContainer = $settingsFactory->createContainer('languageDetectionGroup');
+        $languageDetectionContainer->withType('vertical')
+            ->withName('Widget.languageDetectionLabel')
+            ->withList(1);
+
+        $languageDetectionContainer->children->createSelect("languageCountry")
+            ->withDefaultValue("de")
+            ->withName("Widget.languageDetectionCountry")
+            ->withListBoxValues(ValueListFactory::make()
+                ->addEntry('de', 'German')
+                ->addEntry('en', 'English')
+                ->addEntry('fr', 'French')
+                ->addEntry('nl', 'Netherlands')
+                ->addEntry('pl', 'Poland')
+                ->toArray()
+            );
+        $languageDetectionContainer->children->createText("languageText")
+            ->withName("Widget.languageDetectionText")
+            ->withDefaultValue("Language Text");
+        $languageDetectionContainer->children->createText("languageButton")
+            ->withName("Widget.languageDetectionButton")
+            ->withDefaultValue("Language Button");
 
         return $settingsFactory->toArray();
     }
