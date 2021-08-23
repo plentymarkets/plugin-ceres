058b593c8f40387c51ff7a10cd0f849bcc6362fc
Christoph Johannsen <christoph.johannsen@plentymarkets.com>
2021-08-23 17:09:19 +0200

    Mark properties of type text and the item technical data field as block elements

--- src/ShopBuilder/DataFieldProvider/Item/TextsDataFieldProvider.php
+++ src/ShopBuilder/DataFieldProvider/Item/TextsDataFieldProvider.php
@@ -26,7 +26,7 @@ class TextsDataFieldProvider extends DataFieldProvider
         $this->addField("name3", "Ceres::Widget.dataFieldTextsName3", "item_data_field('texts.name3')");
         $this->addField("shortDescription", "Ceres::Widget.dataFieldTextsShortDescription", "item_data_field('texts.shortDescription', null, null)");
         $this->addField("description", "Ceres::Widget.dataFieldTextsDescription", "item_data_field('texts.description', null, null)", true);
-        $this->addField("technicalData", "Ceres::Widget.dataFieldTextsTechnicalData", "item_data_field('texts.technicalData', null, null)");
+        $this->addField("technicalData", "Ceres::Widget.dataFieldTextsTechnicalData", "item_data_field('texts.technicalData', null, null)", true);
         $this->addField("urlPath", "Ceres::Widget.dataFieldTextsUrlPath", "item_data_field('texts.urlPath')");
         $this->addField("metaDescription", "Ceres::Widget.dataFieldTextsMetaDescription", "item_data_field('texts.metaDescription')");
         $this->addField("metaKeywords", "Ceres::Widget.dataFieldTextsMetaKeywords", "item_data_field('texts.keywords')");
