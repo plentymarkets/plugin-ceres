61446230e374e207e6537af85284558f2bfef9bb
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2021-09-08 09:46:31 +0200

    add html type to block datafields

--- src/ShopBuilder/DataFieldProvider/Item/PropertyDataFieldProvider.php
+++ src/ShopBuilder/DataFieldProvider/Item/PropertyDataFieldProvider.php
@@ -44,7 +44,7 @@ class PropertyDataFieldProvider extends DataFieldProvider
         {
             $this->addField("value_$propertyId", "Ceres::Widget.dataFieldPropertyValue", "item_data_field('variationProperties.{id, $this->propertyGroupId}.properties.{id, $propertyId}.values.value', null, 'href', 'a', 'file')");
         }
-        else if ($this->property->cast === 'text')
+        else if ($this->property->cast === 'text' || $this->property->cast === 'html')
         {
             $this->addField("value_$propertyId", "Ceres::Widget.dataFieldPropertyValue", "item_data_field_html('variationProperties.{id, $this->propertyGroupId}.properties.{id, $propertyId}.values.value', 'escape')", true);
         }
