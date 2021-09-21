540b7fa8914d07ac998e03198da61e146e2f5503
Tim-M-S <tim.mueller-spaeth@plentymarkets.com>
2021-09-21 17:18:49 +0200

    show widget placeholder if needed setting is not active

--- src/Widgets/Category/Filter/FilterBaseWidget.php
+++ src/Widgets/Category/Filter/FilterBaseWidget.php
@@ -2,6 +2,7 @@
 
 namespace Ceres\Widgets\Category\Filter;
 
+use Ceres\Config\CeresConfig;
 use Ceres\Widgets\Helper\BaseWidget;
 use Ceres\Widgets\Helper\Factories\WidgetSettingsFactory;
 use IO\Services\ItemSearch\Factories\Faker\FacetFaker;
@@ -82,7 +83,13 @@ class FilterBaseWidget extends BaseWidget
         /** @var FacetFaker $facetFaker */
         $facetFaker  = pluginApp(FacetFaker::class);
         $facetResult = $facetFaker->fill([]);
-        
-        return ['facets' => $facetResult];
+
+        /** @var CeresConfig $ceresConfig */
+        $ceresConfig = pluginApp(CeresConfig::class);
+
+        return [
+            'facets' => $facetResult,
+            'hasCategoryFilter' => $ceresConfig->item->showCategoryFilter
+        ];
     }
 }
