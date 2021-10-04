58b692ee428e56754d1ee043810812a13ab778c4
Christoph Johannsen <Christoph.johannsen@plentymarkets.com>
2021-10-04 16:52:48 +0200

    Update ItemGridWidget.php

--- src/Widgets/Category/ItemGridWidget.php
+++ src/Widgets/Category/ItemGridWidget.php
@@ -101,8 +101,8 @@ class ItemGridWidget extends BaseWidget
 
         $itemListOptions = [];
         $itemListOptions = SearchOptions::validateItemListOptions($itemListOptions, SearchOptions::SCOPE_CATEGORY);
-        // limit the maximum of items to 20, to prevent memory exhaustion errors
-        $itemListOptions['itemsPerPage'] = min(20, $itemListOptions['itemsPerPage']);
+        // limit the maximum of items to 50, to prevent memory exhaustion errors
+        $itemListOptions['itemsPerPage'] = min(50, $itemListOptions['itemsPerPage']);
 
         $itemList        = $itemListService->getItemList(
             ItemListService::TYPE_RANDOM,
