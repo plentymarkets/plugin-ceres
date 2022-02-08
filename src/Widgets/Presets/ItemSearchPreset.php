7c551b219fea77119074dc934bf99d734a0e6a7a
Christoph Johannsen <christoph.johannsen@plentymarkets.com>
2022-02-08 16:51:10 +0100

    CHANGE ItemSearchPreset.php - add | e("js") for each searchString usage

--- src/Widgets/Presets/ItemSearchPreset.php
+++ src/Widgets/Presets/ItemSearchPreset.php
@@ -82,19 +82,19 @@ class ItemSearchPreset implements ContentPreset
     <div class="col-12">
         <h1 class="h2">
             {% if isTag %}
-                {{ trans("Ceres::Template.tagSearchResults", {"searchString": searchString}) }}
+                {{ trans("Ceres::Template.tagSearchResults", {"searchString": searchString}) | e("js") }}
             {% elseif itemCountTotal > 0 and suggestionString | length > 0 %}
-                <p class="text-muted">{{ trans("Ceres::Template.itemSearchNoResults", {"searchString": searchString}) }}</p>
+                <p class="text-muted">{{ trans("Ceres::Template.itemSearchNoResults", {"searchString": searchString}) | e("js") }}</p>
                 <p>
                     {% autoescape false %}
                         {% set suggestionStringHtml -%}
-                            <a href="{{ queryString({query: suggestionString }) }}">{{ suggestionString }}</a>
+                            <a href="{{ queryString({query: suggestionString }) }}">{{ suggestionString | e("js") }}</a>
                         {%- endset %}
                         {{ trans("Ceres::Template.itemSearchDidYouMean", {"suggestionString": suggestionStringHtml }) }}
                     {% endautoescape %}
                 </p>
             {% elseif itemCountTotal > 0 %}
-                {{ trans("Ceres::Template.itemSearchResults") }} {{ searchString }}
+                {{ trans("Ceres::Template.itemSearchResults") }} {{ searchString | e("js") }}
             {% endif %}
         </h1>
     </div>
@@ -109,7 +109,7 @@ class ItemSearchPreset implements ContentPreset
                      ->withSetting('text',
 '{% if itemCountTotal <= 0 %}
     {% if category is empty and searchString is empty %}{% set searchString = trans("Ceres::Template.itemSearchSearchTerm") %}{% endif %}
-    <p class="h3 text-muted mb-5 text-center">{{ trans("Ceres::Template.itemSearchNoResults", {"searchString": searchString}) }}</p>
+    <p class="h3 text-muted mb-5 text-center">{{ trans("Ceres::Template.itemSearchNoResults", {"searchString": searchString}) | e("js") }}</p>
 {% endif%}');
     }
 
