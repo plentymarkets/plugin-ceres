3e5bab1645dcaee48fe9c8869595cd65bc97b152
Lukas Matzen <lukas.matzen@plentymarkets.com>
2022-03-16 12:17:55 +0100

    ADD OrderPropertyValueList component

---
+++ resources/js/src/app/components/item/OrderPropertyValueListItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <li>
+        <span class="d-block">
+            <strong :class="{ 'colon': property.property.valueType.length > 0 }">{{ property.property.names.name }} <template v-if="surcharge > 0">({{ $translate("Ceres::Template.basketIncludeAbbr") }} {{ surcharge | currency }})</template></strong>
+            <span>
+                <span v-if="property.property.valueType === 'file'">
+                    <a :href="property.property.value | fileUploadPath" target="_blank">
+                        <i class="fa fa-external-link" aria-hidden="true"></i>
+                        {{ property.property.value | fileName }}
+                    </a>
+                </span>
+                <span v-else-if="property.property.valueType !== 'empty'">{{ property.property.value }}</span>
+            </span>
+        </span>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "order-property-value-list-item",
+
+    props:
+    {
+        property:
+        {
+            required: true,
+            type: Object
+        }
+    },
+
+    computed:
+    {
+        surcharge()
+        {
+            return this.$options.filters.propertySurcharge([property], property.propertyId);
+        }
+    }
+}
+</script>
