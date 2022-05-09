2d25f443ed808edb8a20baac7dedb1d8f9eccc94
Christoph Johannsen <christoph.johannsen@plentymarkets.com>
2022-05-09 18:04:43 +0200

    add first try

---
+++ resources/js/src/app/components/checkout/BasketItemConsent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p>filteredData:</p>
+
+        <div>{{ filteredData }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        propertyIdToConsent: Number
+    },
+
+    computed: {
+        filteredData() {
+            return this.basketItems.filter(item => {
+                const variationPropertyGroups = item.variation.data.variationProperties;
+
+                for (const variationPropertyGroup of variationPropertyGroups)
+                {
+                    for (const variationProperty of variationPropertyGroup.properties)
+                    {
+                        if (variationProperty.id === this.propertyIdToConsent)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            });
+        },
+
+        basketItems() {
+            return this.$store.state.basket.items;
+        }
+    }    
+}
+</script>
