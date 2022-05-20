31562770d6993f16d958eadb27192d9f1ab4629f
Christoph Johannsen <christoph.johannsen@plentymarkets.com>
2022-05-20 16:15:40 +0200

    no message

--- testing/cypress/integration/category/category-item.spec.js
+++ testing/cypress/integration/category/category-item.spec.js
@@ -10,6 +10,6 @@ context("category-item", () =>
     it("should not display the lowest price, because item has no rrp", () =>
     {
         cy.visit("/suche/?query=202");
-        cy.get("article[data-testing='1143'] .category-lowest-price").should("not.exist");
+        cy.get("article[data-testing='1144'] .category-lowest-price").should("not.exist");
     });
 });
