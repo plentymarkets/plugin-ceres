02f1247c9e38a172fcf12e9c0718c4a6df6b9408
Dominik Meyer <dominik.meyer@plentymarkets.com>
2022-11-17 08:44:48 +0100

    ADD payment method AlreadyPaid

---
+++ src/Methods/AlreadyPaidPaymentMethod.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace Ceres\Methods;
+
+use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
+use Plenty\Modules\Frontend\Contracts\Checkout;
+use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
+use Plenty\Modules\Payment\Method\Services\PaymentMethodBaseService;
+use Plenty\Plugin\Application;
+use Plenty\Plugin\Translation\Translator;
+
+class AlreadyPaidPaymentMethod extends PaymentMethodBaseService
+{
+    /** @var BasketRepositoryContract */
+    private $basketRepository;
+
+    /** @var  Checkout */
+    private $checkout;
+
+    /**
+     * CashInAdvancePaymentMethod constructor.
+     * @param BasketRepositoryContract $basketRepository
+     * @param Checkout $checkout
+     */
+    public function __construct(
+        BasketRepositoryContract $basketRepository,
+        Checkout $checkout
+    ) {
+        $this->basketRepository = $basketRepository;
+        $this->checkout = $checkout;
+    }
+
+    /**
+     * Check whether AlreadyPaid is active or not
+     *
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->basketRepository->load()->basketAmount <= 0;
+    }
+
+    /**
+     * Get shown name
+     *
+     * @param $lang
+     * @return string
+     */
+    public function getName(string $lang = 'de'): string
+    {
+        return 'Already paid';
+
+        //TODO trans
+        /** @var Translator $translator */
+        //$translator = pluginApp(Translator::class);
+        //return $translator->trans('PrePayment::PaymentMethod.paymentMethodName',[],$lang);
+    }
+
+    /**
+     * Get AlreadyPaid Fee
+     *
+     * @return float
+     */
+    public function getFee(): float
+    {
+        return 0.00;
+    }
+
+    /**
+     * Get AlreadyPaid Icon
+     *
+     * @param string $lang
+     * @return string
+     */
+    public function getIcon(string $lang = 'de'): string
+    {
+        return '';
+    }
+
+    /**
+     * Get AlreadyPaid source URL
+     *
+     * @param string $lang
+     * @return string
+     */
+    public function getSourceUrl(string $lang = 'de'): string
+    {
+        return '';
+    }
+
+    /**
+     * Get AlreadyPaid description
+     *
+     * @param string $lang
+     * @return string
+     */
+    public function getDescription(string $lang = 'de'): string
+    {
+        //TODO trans
+        return 'No payment needed';
+    }
+
+    /**
+     * Check if it is allowed to switch to this payment method
+     *
+     * @return bool
+     */
+    public function isSwitchableTo(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Check if it is allowed to switch from this payment method
+     *
+     * @return bool
+     */
+    public function isSwitchableFrom(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Check if this payment method should be searchable in the backend
+     *
+     * @return bool
+     */
+    public function isBackendSearchable(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Check if this payment method should be active in the backend
+     *
+     * @return bool
+     */
+    public function isBackendActive(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the name for the backend
+     *
+     * @param string $lang
+     * @return string
+     */
+    public function getBackendName(string $lang = 'de'): string
+    {
+        return $this->getName($lang);
+    }
+
+    /**
+     * Check if this payment method can handle subscriptions
+     *
+     * @return bool
+     */
+    public function canHandleSubscriptions(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the url for the backend icon
+     *
+     * @return string
+     */
+    public function getBackendIcon(): string
+    {
+        //TODO real icon
+        return '';
+        //$app = pluginApp(Application::class);
+        //$icon = $app->getUrlPath('prepayment').'/images/logos/prepayment_plus_backend_icon.svg';
+        //return $icon;
+    }
+}
