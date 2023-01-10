62114c9f7d1b38c074c1b90f097ca281d8111e8c
Lukas Matzen <lukas.matzen@plentymarkets.com>
2023-01-10 16:29:06 +0100

    UPDATE remove console log

--- tools/webpack/common.config.js
+++ tools/webpack/common.config.js
@@ -4,7 +4,6 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (mode) =>
 {
-    console.log("common.config.js", mode);
     return {
         mode: mode,
         output: {
