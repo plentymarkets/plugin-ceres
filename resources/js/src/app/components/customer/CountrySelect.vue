69d4f25a86edb1d1d4a3a0393fbc29d1fbd5b6f3
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-01-19 13:22:06 +0100

    test

--- resources/js/src/app/components/customer/CountrySelect.vue
+++ resources/js/src/app/components/customer/CountrySelect.vue
@@ -15,7 +15,7 @@
                 v-if="stateList && stateList.length > 0"
                 v-validate="isInRequiredFields('stateId')"
                 data-model="stateId"
-                data-validate="please-select">
+                data-validate="'please-select'">
                 <select :id="'state-id-select' + _uid" :value="selectedStateId" class="custom-select" @change="stateChanged($event.target.value)">
                     <option :selected="selectedStateId === null">{{ $translate("Ceres::Template.addressPleaseSelect") }}</option>
                     <option :value="state.id" :selected="state.id === selectedStateId" v-for="state in stateList" :key="state.id">
