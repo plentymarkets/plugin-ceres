a9d35f0d4590fab97e1cc04896820b84e3da57e1
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-08-14 10:32:02 +0200

    fix: recaptcha loading without consent

--- src/Providers/TemplateServiceProvider.php
+++ src/Providers/TemplateServiceProvider.php
@@ -387,7 +387,9 @@ class TemplateServiceProvider extends ServiceProvider
                     'provider' => 'Ceres::Template.consentReCaptchaProvider',
                     'lifespan' => $webstoreConfig->sessionLifetime > 0 ? 'Ceres::Template.consentLifespan100Days' : 'Ceres::Template.consentLifespanSession',
                     'policyUrl' => 'Ceres::Template.consentReCaptchaPolicyUrl',
-                    'group' => 'media'
+                    'group' => 'media',
+                    'necessary' => false,
+                    'isOptOut' => false
                 ]
             );
         }
