d99ce448578b051c3b0c10ac32a3214294d78e8c
oivan-plenty <oana.ivan@plentymarkets.com>
2023-12-13 12:18:08 +0200

    preset header

--- src/Widgets/WidgetCollection.php
+++ src/Widgets/WidgetCollection.php
@@ -66,7 +66,7 @@ use Ceres\Widgets\Grid\TabWidget;
 use Ceres\Widgets\Grid\ThreeColumnWidget;
 use Ceres\Widgets\Grid\TwoColumnWidget;
 use Ceres\Widgets\Header\BreadcrumbWidget;
-use Ceres\Widgets\Header\PresetHeaderWidget;
+// use Ceres\Widgets\Header\PresetHeaderWidget;
 use Ceres\Widgets\Header\LanguageDetectionWidget;
 use Ceres\Widgets\Header\NavigationWidget;
 use Ceres\Widgets\Header\Search\SearchSuggestionCategoryWidget;
@@ -191,8 +191,8 @@ class WidgetCollection
         SearchSuggestionItemWidget::class,
         SearchSuggestionSuggestionWidget::class,
         TopBarWidget::class,
-        LanguageDetectionWidget::class,
-        PresetHeaderWidget::class
+        LanguageDetectionWidget::class
+//         PresetHeaderWidget::class
     ];
 
     const ITEM_WIDGETS = [
