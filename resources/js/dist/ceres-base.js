fb0fa5ee3e3951a47f7315f2aca9ef4166a1d078
aoltea-plenty <alexandru.oltean@plentymarkets.com>
2023-12-18 13:54:42 +0200

    Remove current HeaderScroller

--- resources/js/dist/ceres-base.js
+++ resources/js/dist/ceres-base.js
@@ -60288,10 +60288,10 @@ var HeaderScroller = /*#__PURE__*/function () {
       this.updateZIndexes(); // Initialize only, if the user has scrolled down from the top and is not in the shopbuilder.
 
       if (!App.isShopBuilder && window.pageYOffset > 0) {
-        this.calculateBodyOffset();
-        this.scrollHeaderElements(); // If the header content gets active in the shopbuilder, the event listener for 'shopbuilder.after.activate-container' will fixate the header.
-
-        this.fixateHeader();
+        // this.calculateBodyOffset();
+        // this.scrollHeaderElements();
+        // // If the header content gets active in the shopbuilder, the event listener for 'shopbuilder.after.activate-container' will fixate the header.
+        // this.fixateHeader();
         this.initialized = true;
       }
     } // Collect heights of header elements for later use
