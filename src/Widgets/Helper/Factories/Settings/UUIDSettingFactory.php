7e2dfb584c2b44f5bbc438470043659f0dc7bfe3
Cristian Lincu <cristian.lincu@plentymarkets.com>
2023-12-28 16:33:42 +0200

    Debug.

--- src/Widgets/Helper/Factories/Settings/UUIDSettingFactory.php
+++ src/Widgets/Helper/Factories/Settings/UUIDSettingFactory.php
@@ -3,14 +3,15 @@
 namespace Ceres\Widgets\Helper\Factories\Settings;
 
 use Plenty\Modules\ShopBuilder\Factories\Settings\BaseSettingFactory;
-use IO\Extensions\Functions\UniqueId;
+//use IO\Extensions\Functions\UniqueId;
 
 class UUIDSettingFactory extends BaseSettingFactory
 {
     public function __construct()
     {
-        $uuidGenerator = pluginApp(UniqueId::class);
-        $uuid = $uuidGenerator->generateUniqueId();
-        $this->withType('uuid')->withDefaultValue($uuid);
+//        $uuidGenerator = pluginApp(UniqueId::class);
+//        $uuid = $uuidGenerator->generateUniqueId();
+        $this->withType('uuid')
+            ->withDefaultValue('4af67288-b1e4-4a83-9777-fbbfe0295e95');
     }
 }
