b200a16cad26971850720f994bd929de63172f76
cristian sandru <cristian.sandru@plentysystems.com>
2024-02-20 16:43:00 +0200

    feat: webp item pages

--- resources/js/dist/ceres-server.js
+++ resources/js/dist/ceres-server.js
@@ -7291,21 +7291,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "item-image-carousel",
--- resources/js/dist/chunks/ceres-36.js
+++ resources/js/dist/chunks/ceres-36.js
@@ -54,21 +54,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "item-image-carousel",
