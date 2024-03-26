8ca854b5af8ebab245541ec3a75efcdb69610379
Steve Tentrop <steve.tentrop@plentymarkets.com>
2024-03-26 20:59:48 +0100

    change tests

--- testing/cypress/integration/single-item/lightbox.spec.js
+++ testing/cypress/integration/single-item/lightbox.spec.js
@@ -4,7 +4,7 @@ context("Lightbox js", () =>
     it("should open lightbox image on click", () =>
     {
         cy.visit("/wohnzimmer/sessel-hocker/barhockerl-fury-gepolstert_107_1005/");
-        cy.get(".widget-item-image").find("img").click();
+        cy.get(".widget-item-image").find("img").first().click();
         cy.get("#lightbox").should("not.have.attr", "style", "display: none;");
     });
 });
