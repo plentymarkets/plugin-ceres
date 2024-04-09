db9301cf264372d7021f103095ae2d94ec189b46
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2024-04-09 14:09:11 +0200

    Revert "add: build files"
    This reverts commit 603f8e9bc5fcbc46af3e380e5c236ee7e93ef848.

--- resources/js/dist/chunks/ceres-client-14.js
+++ resources/js/dist/chunks/ceres-client-14.js
@@ -117,7 +117,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
       return state.navigation.categoryChildren;
     }
   })),
-  beforeMount: function beforeMount() {
+  created: function created() {
     this.loadChunk();
   },
   methods: {
