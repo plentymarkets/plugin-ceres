55e17e865fa976974eb4236b55e856d8783db316
cristian sandru <cristian.sandru@plentysystems.com>
2024-08-12 12:34:46 +0300

    revert forced conversion

--- resources/js/dist/ceres-checkout.js
+++ resources/js/dist/ceres-checkout.js
@@ -812,7 +812,7 @@ var mime = __webpack_require__(/*! mime-types */ "./node_modules/mime-types/inde
   },
   data: function data() {
     return {
-      imageConversionEnabled: true,
+      imageConversionEnabled: App.config.log.modernImagesConversion,
       receivedImageExtension: null,
       browserSupportedImgExtension: null,
       defaultImageUrl: this.imageUrl,
--- resources/js/dist/ceres-server.js
+++ resources/js/dist/ceres-server.js
@@ -2949,7 +2949,7 @@ var mime = __webpack_require__(/*! mime-types */ "./node_modules/mime-types/inde
   },
   data: function data() {
     return {
-      imageConversionEnabled: true,
+      imageConversionEnabled: App.config.log.modernImagesConversion,
       receivedImageExtension: null,
       browserSupportedImgExtension: null,
       defaultImageUrl: this.imageUrl,
