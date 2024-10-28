6a83fc2ef09e88c8ec77e3a2f175b1154b47490a
Teodor Rosu <teodor.rosu@plentymarkets.com>
2024-10-28 15:54:46 +0200

    Update DefaultSingleItemPreset.php

--- src/Widgets/Presets/DefaultSingleItemPreset.php
+++ src/Widgets/Presets/DefaultSingleItemPreset.php
@@ -330,49 +330,7 @@ class DefaultSingleItemPreset implements ContentPreset
             ->withSetting('spacing.padding.bottom.unit', null)
             ->withSetting('text',$this->getShopBuilderDataFieldProvider('TextsDataFieldProvider::technicalData',array('texts.technicalData', null, null)));
 
-        $this->tabWidget->createChild($uuidTabMoreDetails, 'Ceres::ItemDataTableWidget')
-            ->withSetting('itemInformation',
-                            array("item.id",
-                                "item.condition.names.name",
-                                "item.ageRestriction",
-                                "variation.externalId",
-                                "variation.model",
-                                "item.manufacturer.externalName",
-                                "item.producingCountry.names.name",
-                                "unit.names.name",
-                                "variation.weightG",
-                                "variation.weightNetG",
-                                "item.variationDimensions",
-                                "variation.customsTariffNumber"));
-
-        $euResponsiblePersonTab = $this->tabWidget->createChild($uuidEuResponsiblePerson, 'Ceres::ManufacturerDataWidget')
-            ->withSetting('appearance','none')
-            ->withSetting('spacing.customPadding', true)
-            ->withSetting('spacing.padding.left.value', 0)
-            ->withSetting('spacing.padding.left.unit', null)
-            ->withSetting('spacing.padding.right.value', 0)
-            ->withSetting('spacing.padding.right.unit', null)
-            ->withSetting('spacing.padding.top.value', 0)
-            ->withSetting('spacing.padding.top.unit', null)
-            ->withSetting('spacing.padding.bottom.value', 0)
-            ->withSetting('spacing.padding.bottom.unit', null);
-
-        $uuidOne    = $uuidGenerator->generateUniqueId();
-        $euResponsiblePersonTab->createChild($uuidOne, 'Ceres::InlineTextWidget')
-            ->withSetting('appearance','none')
-            ->withSetting('spacing.customPadding', true)
-            ->withSetting('spacing.padding.left.value', 0)
-            ->withSetting('spacing.padding.left.unit', null)
-            ->withSetting('spacing.padding.right.value', 0)
-            ->withSetting('spacing.padding.right.unit', null)
-            ->withSetting('spacing.padding.top.value', 0)
-            ->withSetting('spacing.padding.top.unit', null)
-            ->withSetting('spacing.padding.bottom.value', 0)
-            ->withSetting('spacing.padding.bottom.unit', null)
-            ->withSetting('text',$this->getShopBuilderDataFieldProvider('ManufacturerDataFieldProvider::responsibleCountry',array('manufacturer.responsibleCountry', null, null)));
-
-        $uuidTwo    = $uuidGenerator->generateUniqueId();
-        $euResponsiblePersonTab->createChild($uuidTwo, 'Ceres::InlineTextWidget')
+        $this->tabWidget->createChild($uuidTabTechData, 'Ceres::InlineTextWidget')
             ->withSetting('appearance','none')
             ->withSetting('spacing.customPadding', true)
             ->withSetting('spacing.padding.left.value', 0)
@@ -384,6 +342,18 @@ class DefaultSingleItemPreset implements ContentPreset
             ->withSetting('spacing.padding.bottom.value', 0)
             ->withSetting('spacing.padding.bottom.unit', null)
             ->withSetting('text',$this->getShopBuilderDataFieldProvider('ManufacturerDataFieldProvider::responsibleEmail',array('manufacturer.responsibleEmail', null, null)));
+
+        $this->tabWidget->createChild($uuidEuResponsiblePerson, 'Ceres::ManufacturerDataWidget')
+            ->withSetting('appearance','none')
+            ->withSetting('spacing.customPadding', true)
+            ->withSetting('spacing.padding.left.value', 0)
+            ->withSetting('spacing.padding.left.unit', null)
+            ->withSetting('spacing.padding.right.value', 0)
+            ->withSetting('spacing.padding.right.unit', null)
+            ->withSetting('spacing.padding.top.value', 0)
+            ->withSetting('spacing.padding.top.unit', null)
+            ->withSetting('spacing.padding.bottom.value', 0)
+            ->withSetting('spacing.padding.bottom.unit', null);
 
 //        $this->generateEUManufacturerField($euResponsiblePersonTab, ManufacturerDataFieldProvider::RESPONSIBLE_NAME, $uuidEuResponsiblePerson);
 //        $this->generateEUManufacturerField($euResponsiblePersonTab, ManufacturerDataFieldProvider::RESPONSIBLE_STREET, $uuidEuResponsiblePerson);
