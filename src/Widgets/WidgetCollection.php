b8e4e1940f201dfeeb3556fb189f4d23ecb8d8b8
plentydev <iulian.vladu@plentymarkets.com>
2024-10-29 14:02:41 +0200

    fix error

--- src/Widgets/WidgetCollection.php
+++ src/Widgets/WidgetCollection.php
@@ -39,7 +39,6 @@ use Ceres\Widgets\Common\LinkListWidget;
 use Ceres\Widgets\Common\LinkWidget;
 use Ceres\Widgets\Common\ListWidget;
 use Ceres\Widgets\Common\LiveShoppingWidget;
-use Ceres\Widgets\Common\ManufacturerDataWidget;
 use Ceres\Widgets\Common\NewsletterUnsubscribeWidget;
 use Ceres\Widgets\Common\NewsletterWidget;
 use Ceres\Widgets\Common\PrintButtonWidget;
@@ -147,8 +146,7 @@ class WidgetCollection
         PrivacySettingsWidget::class,
         SeparatorWidget::class,
         TextWidget::class,
-        TitleBarWidget::class,
-        ManufacturerDataWidget::class
+        TitleBarWidget::class
     ];
 
     const CONTACT_WIDGETS = [
