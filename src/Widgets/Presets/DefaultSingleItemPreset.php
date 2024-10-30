7ac7b877b34588ebaf686aaf780fe84d1e12e4bd
plentydev <iulian.vladu@plentymarkets.com>
2024-10-30 15:47:58 +0200

    remove title and body

--- src/Widgets/Presets/DefaultSingleItemPreset.php
+++ src/Widgets/Presets/DefaultSingleItemPreset.php
@@ -337,22 +337,6 @@ class DefaultSingleItemPreset implements ContentPreset
                 "variation.customsTariffNumber"
             ]);
 
-        $euResponsiblePersonDescription = '<h3>'.$this->translator->trans("Ceres::Template.singleItemEuResponsiblePersonTitle").'</h3>';
-        $euResponsiblePersonDescription .= '<p>'.$this->translator->trans("Ceres::Template.singleItemEuResponsiblePersonBody").'</p><br>';
-
-        $this->tabWidget->createChild($uuidEuResponsiblePerson, 'Ceres::InlineTextWidget')
-            ->withSetting('appearance','none')
-            ->withSetting('spacing.customPadding', true)
-            ->withSetting('spacing.padding.left.value', 0)
-            ->withSetting('spacing.padding.left.unit', null)
-            ->withSetting('spacing.padding.right.value', 0)
-            ->withSetting('spacing.padding.right.unit', null)
-            ->withSetting('spacing.padding.top.value', 0)
-            ->withSetting('spacing.padding.top.unit', null)
-            ->withSetting('spacing.padding.bottom.value', 0)
-            ->withSetting('spacing.padding.bottom.unit', null)
-            ->withSetting('text', $euResponsiblePersonDescription);
-
         foreach (ManufacturerDataFieldProvider::getFields() as $field) {
             $this->tabWidget->createChild($uuidEuResponsiblePerson, 'Ceres::InlineTextWidget')
                 ->withSetting('appearance','none')
