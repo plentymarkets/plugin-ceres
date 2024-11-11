32324b487445a607bff96456bea34629314ede19
plentydev <iulian.vladu@plentymarkets.com>
2024-11-11 10:53:45 +0200

    add country to condition

--- resources/js/dist/ceres-checkout.js
+++ resources/js/dist/ceres-checkout.js
@@ -3789,7 +3789,7 @@ __webpack_require__.r(__webpack_exports__);
       return (App.config.item.itemData.includes("item.description") || App.config.item.itemData.includes("all")) && !!this.currentVariation.texts.description.length;
     },
     isEuResponsibleTabShown: function isEuResponsibleTabShown() {
-      return this.currentVariation.item.manufacturer.responsibleEmail !== "" && this.currentVariation.item.manufacturer.responsibleHouseNo && this.currentVariation.item.manufacturer.responsibleName && this.currentVariation.item.manufacturer.responsiblePhoneNo && this.currentVariation.item.manufacturer.responsiblePostCode && this.currentVariation.item.manufacturer.responsibleStreet && this.currentVariation.item.manufacturer.responsibleTown;
+      return this.currentVariation.item.manufacturer.responsibleEmail !== "" && this.currentVariation.item.manufacturer.responsibleHouseNo !== "" && this.currentVariation.item.manufacturer.responsibleName !== "" && this.currentVariation.item.manufacturer.responsiblePhoneNo !== "" && this.currentVariation.item.manufacturer.responsiblePostCode !== "" && this.currentVariation.item.manufacturer.responsibleStreet !== "" && this.currentVariation.item.manufacturer.responsibleTown !== "" && this.currentVariation.item.manufacturer.responsibleCountry !== 0;
     },
     isRecommendedPriceActive: function isRecommendedPriceActive() {
       return App.config.item.itemData.includes("item.recommendedPrice") || App.config.item.itemData.includes("all");
--- resources/js/src/app/components/item/SingleItem.vue
+++ resources/js/src/app/components/item/SingleItem.vue
@@ -325,12 +325,13 @@ export default {
         isEuResponsibleTabShown()
         {
             return (this.currentVariation.item.manufacturer.responsibleEmail !== "") &&
-                (this.currentVariation.item.manufacturer.responsibleHouseNo) &&
-                (this.currentVariation.item.manufacturer.responsibleName) &&
-                (this.currentVariation.item.manufacturer.responsiblePhoneNo) &&
-                (this.currentVariation.item.manufacturer.responsiblePostCode) &&
-                (this.currentVariation.item.manufacturer.responsibleStreet) &&
-                (this.currentVariation.item.manufacturer.responsibleTown);
+                (this.currentVariation.item.manufacturer.responsibleHouseNo !== "") &&
+                (this.currentVariation.item.manufacturer.responsibleName !== "") &&
+                (this.currentVariation.item.manufacturer.responsiblePhoneNo !== "") &&
+                (this.currentVariation.item.manufacturer.responsiblePostCode !== "") &&
+                (this.currentVariation.item.manufacturer.responsibleStreet !== "") &&
+                (this.currentVariation.item.manufacturer.responsibleTown !== "") &&
+                (this.currentVariation.item.manufacturer.responsibleCountry !== 0);
         },
 
         isRecommendedPriceActive()
