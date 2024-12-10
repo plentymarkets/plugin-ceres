2bd578dde468f2c9082dd0e0e5de5705f6a2a877
plentydev <iulian.vladu@plentymarkets.com>
2024-12-10 13:32:27 +0200

    add to setComponentMeta data

--- resources/js/dist/ceres-base.js
+++ resources/js/dist/ceres-base.js
@@ -69173,9 +69173,9 @@ var actions = {
 
           component.data.variation.minimumOrderQuantity = setComponentMeta.minimumOrderQuantity;
           component.data.variation.maximumOrderQuantity = setComponentMeta.maximumOrderQuantity;
-          component.data.variation.manufacturer = component.data.item.manufacturer;
-          component.data.variation.manufacturerId = component.data.item.manufacturerId;
-          component.data.variation.texts = component.data.item.texts; // register a module for every set item
+          setComponentMeta.manufacturer = component.data.item.manufacturer;
+          setComponentMeta.manufacturerId = component.data.item.manufacturerId;
+          setComponentMeta.texts = component.data.item.texts; // register a module for every set item
 
           dispatch("registerItem", component);
           commit("".concat(itemId, "/setPleaseSelectVariationId"), variationId);
