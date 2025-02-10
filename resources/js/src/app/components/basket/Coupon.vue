05bbfd022080a48085f765728dca4bd98d78a5f7
Teodor Rosu <teodor.rosu@plentymarkets.com>
2025-02-10 11:13:42 +0200

    Fixed issues after testing
    - Fixed issues after testing

--- resources/js/src/app/components/basket/Coupon.vue
+++ resources/js/src/app/components/basket/Coupon.vue
@@ -6,6 +6,7 @@
         <div :class="{'input-group':true, 'component-loading':isCheckoutReadonly, 'is-loading':isCheckoutReadonly}">
             <input
                 type="text"
+                :aria-label="$translate('Ceres::Template.couponEnterCoupon')"
                 :id="'coupon-code-identifier' + _uid"
                 class="form-control"
                 v-model="couponCode"
@@ -14,9 +15,6 @@
                 :disabled="disabled || isCheckoutReadonly"
                 data-testing="coupon-input"
             >
-            <label :for="'coupon-code-identifier' + _uid">
-              <span class="d-none">{{ $translate('Ceres::Template.couponEnterCoupon') }}</span>
-            </label>
             <span class="input-group-btn">
                 <button
                     class="btn btn-medium btn-primary btn-appearance"
