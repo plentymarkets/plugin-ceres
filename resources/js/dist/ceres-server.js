69f5c017b1b0dc57a1a57b55bc80049d608d6941
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-04-08 12:29:18 +0300

    fix: build

--- resources/js/dist/ceres-server.js
+++ resources/js/dist/ceres-server.js
@@ -13851,7 +13851,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "order-return-item",
@@ -56161,7 +56160,6 @@ var render = function() {
                   attrs: {
                     "image-url": _vm.orderItemImage,
                     alt: _vm._f("itemBundleName")(_vm.orderItem),
-                    title: _vm._f("itemBundleName")(_vm.orderItem),
                     "picture-class": "d-block mw-100 mh-100"
                   }
                 })
