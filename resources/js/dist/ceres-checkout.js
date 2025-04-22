0eee43d57556ab3014786e65ef1faa4a92e01585
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-04-22 14:47:53 +0300

    fix: color override

--- resources/js/dist/ceres-checkout.js
+++ resources/js/dist/ceres-checkout.js
@@ -46057,7 +46057,8 @@ var render = function() {
                         ? _c(
                             "div",
                             {
-                              staticClass: "producertag h6 producer text-muted"
+                              staticClass:
+                                "producertag h6 producer text-muted color-gray-700"
                             },
                             [
                               _vm._v(
