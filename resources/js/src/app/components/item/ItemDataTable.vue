07689c92c0d635aeca06f042b89116d306eb6d3f
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-05-22 11:38:46 +0300

    fix: accessibility fix

--- resources/js/src/app/components/item/ItemDataTable.vue
+++ resources/js/src/app/components/item/ItemDataTable.vue
@@ -1,17 +1,35 @@
 <template>
-    <table class="table table-striped table-hover table-sm">
-        <tbody>
-        <template v-for="itemDataAccessor in itemInformation">
-            <tr v-if="isCheckedAndNotEmpty(itemDataAccessor)">
-                <td :class="paddingClasses" :style="paddingInlineStyles">
-                    {{ getTranslation(itemDataAccessor) }}
-                </td>
-
-                <td :class="paddingClasses" :style="paddingInlineStyles">
-                    {{ getFieldValue(itemDataAccessor) }}
-                </td>
+    <table class="table table-striped table-hover table-sm" role="table">
+        <thead>
+            <tr>
+                <th 
+                    scope="col" 
+                    class="visually-hidden" 
+                    aria-hidden="false"
+                >
+                    Product property type
+                </th>
+                <th 
+                    scope="col" 
+                    class="visually-hidden" 
+                    aria-hidden="false"
+                >
+                    Product property value
+                </th>
             </tr>
-        </template>
+        </thead>
+        <tbody>
+            <template v-for="itemDataAccessor in itemInformation">
+                <tr v-if="isCheckedAndNotEmpty(itemDataAccessor)" role="row">
+                    <td :class="paddingClasses" :style="paddingInlineStyles" role="rowheader">
+                        {{ getTranslation(itemDataAccessor) }}
+                    </td>
+
+                    <td :class="paddingClasses" :style="paddingInlineStyles" role="cell">
+                        {{ getFieldValue(itemDataAccessor) }}
+                    </td>
+                </tr>
+            </template>
         </tbody>
     </table>
 </template>
