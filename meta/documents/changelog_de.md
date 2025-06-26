a4f1cae243b8a23d6722e52c7f78c30ad83b4f44
Tim Müller-Späth <tim.mueller.spaeth@gmail.com>
2025-06-26 10:59:05 +0200

    add changelog and missing config entry

--- meta/documents/changelog_de.md
+++ meta/documents/changelog_de.md
@@ -1,6 +1,11 @@
 # Release Notes für plentyShop LTS
 
-## v5.0.74 (2025-06-25) <a href="https://github.com/plentymarkets/plugin-ceres/compare/5.0.73...5.0.74" target="_blank" rel="noopener"><b>Übersicht aller Änderungen</b></a>
+## v5.0.74 (2025-06-26) <a href="https://github.com/plentymarkets/plugin-ceres/compare/5.0.73...5.0.74" target="_blank" rel="noopener"><b>Übersicht aller Änderungen</b></a>
+
+### Hinzugefügt
+
+- Es können Meta-Daten für die Barrierefreiheitserklärung editiert werden.
+- Die Robots Einstellungen der Barrierefreiheitserklärung können eingestellt werden.
 
 ### Behoben
 
--- src/Config/CeresMetaConfig.php
+++ src/Config/CeresMetaConfig.php
@@ -75,6 +75,7 @@ class CeresMetaConfig extends PluginConfig
         $this->robotsContact                        = $this->getTextValue( 'meta.robots_contact' , 'all' );
         $this->robotsCancellationRights             = $this->getTextValue( 'meta.robots_cancel_rights' , 'all' );
         $this->robotsCancellationForm               = $this->getTextValue( 'meta.robots_cancel_form' , 'all' );
+        $this->robotsDeclarationOfAccessibility     = $this->getTextValue( 'meta.robots_declaration_of_accessibility' , 'all' );
         $this->robotsLegalDisclosure                = $this->getTextValue( 'meta.robots_legal_disclosure' , 'all' );
         $this->robotsPrivacyPolicy                  = $this->getTextValue( 'meta.robots_privacy_policy' , 'all' );
         $this->robotsTermsAndConditions             = $this->getTextValue( 'meta.robots_terms_and_conditions' , 'all' );
